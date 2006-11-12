8e04d130cc4e3fce54fc0b91b51619e8de499d79
d-marc <d-marc@private.net>
2006-11-12 09:46:19 +0000

    ADDED: missing include for Gui::Painter

--- src/Pt-Gui/Pixmap.cpp
+++ src/Pt-Gui/Pixmap.cpp
@@ -16,14 +16,10 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
-#include "Pt/Gui/Pixmap.h"
-
 #include "PixmapImpl.h"
 
-#include <string>
-#include <iostream>
-using namespace std;
+#include "Pt/Gui/Pixmap.h"
+#include "Pt/Gui/Painter.h"
 
 
 namespace Pt {
