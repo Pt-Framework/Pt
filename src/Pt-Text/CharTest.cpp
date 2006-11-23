6009e451f89c90b662ecc114675216959ac8e9b0
slyh_ <slyh_@private.net>
2006-11-23 13:21:18 +0000

    INITIAL_VERSION: Tests for Char and TextStream/Codecs.

---
+++ src/Pt-Text/CharTest.cpp
@@ -0,0 +1,297 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Tobias Müller                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ **************************************************************************/
+
+#include "Pt/Text/Char.h"
+#include "Pt/Exception.h"
+#include "Pt/Main.h"
+
+#include <cassert>
+
+#include "Pt/Types.h"
+
+using namespace std;
+using namespace Pt::Text;
+
+class CharTest
+{
+public:
+
+	void testConstructor();
+	void testAssign();
+	void testCategory();
+	void testCompare();
+	void testOperators();
+};
+
+int main(int argc, char* argv[])
+{
+	CharTest ct;
+	ct.testConstructor();
+	ct.testAssign();
+	ct.testCategory();
+	ct.testCompare();
+	ct.testOperators();
+}
+
+void CharTest::testConstructor()
+{
+	Char p1;
+	assert(p1.value() == 0);
+
+	Char p2((char)'a');
+	assert(p2.value() == 97);
+	
+	Char p3((wchar_t)'a');
+	assert(p3.value() == 97);
+
+	Char p4((unsigned char)'a');
+	assert(p4.value() == 97);
+
+	Char p5((Pt::int16_t)'a');
+	assert(p5.value() == 97);
+
+	Char p6((Pt::uint16_t)'a');
+	assert(p6.value() == 97);
+
+	Char p7((Pt::int32_t)'a');
+	assert(p7.value() == 97);
+
+	Char p8((Pt::uint32_t)'a');
+	assert(p8.value() == 97);
+
+	Char p9((long)'a');
+	assert(p9.value() == 97);
+
+	Char p10((unsigned long)'a');
+	assert(p10.value() == 97);
+}
+
+void CharTest::testAssign()
+{
+	Char p;
+	assert(p.value() == 0);
+
+	p = (char)'a';
+	assert(p.value() == 97);
+	
+	p = (wchar_t)('b');
+	assert(p.value() == 98);
+
+	p = (unsigned char)'c';
+	assert(p.value() == 99);
+
+	p = (Pt::int16_t)'d';
+	assert(p.value() == 100);
+
+	p = (Pt::uint16_t)'e';
+	assert(p.value() == 101);
+
+	p = (Pt::int32_t)'f';
+	assert(p.value() == 102);
+
+	p = (Pt::uint32_t)'g';
+	assert(p.value() == 103);
+
+	p = (long)'h';
+	assert(p.value() == 104);
+
+	p = (unsigned long)'i';
+	assert(p.value() == 105);
+}
+
+void CharTest::testCategory()
+{
+	Char p;
+
+	p = 'a';
+	assert( p.isAlpha());
+	assert( p.isAlnum());
+	assert(!p.isDigit());
+	assert( p.isLower());
+	assert(!p.isUpper());
+	assert(!p.isControl());
+	assert(!p.isGraph());
+	assert( p.isPrint());
+	assert(!p.isPunctuation());
+	assert(!p.isSpace());
+
+	p = p.toUpper();  // A
+	assert( p.isAlpha());
+	assert( p.isAlnum());
+	assert(!p.isDigit());
+	assert(!p.isLower());
+	assert( p.isUpper());
+	assert(!p.isControl());
+	assert(!p.isGraph());
+	assert( p.isPrint());
+	assert(!p.isPunctuation());
+	assert(!p.isSpace());
+
+	p = L'ö';
+	assert( p.isAlpha());
+	assert( p.isAlnum());
+	assert(!p.isDigit());
+	assert( p.isLower());
+	assert(!p.isUpper());
+	assert(!p.isControl());
+	assert(!p.isGraph());
+	assert( p.isPrint());
+	assert(!p.isPunctuation());
+	assert(!p.isSpace());
+
+	
+	p = p.toUpper();  // Ö
+	assert( p.isAlpha());
+	assert( p.isAlnum());
+	assert(!p.isDigit());
+	assert(!p.isLower());
+	assert( p.isUpper());
+	assert(!p.isControl());
+	assert(!p.isGraph());
+	assert( p.isPrint());
+	assert(!p.isPunctuation());
+	assert(!p.isSpace());
+
+	p = '1';
+	assert(!p.isAlpha());
+	assert( p.isAlnum());
+	assert( p.isDigit());
+	assert(!p.isLower());
+	assert(!p.isUpper());
+	assert(!p.isControl());
+	assert(!p.isGraph());
+	assert( p.isPrint());
+	assert(!p.isPunctuation());
+	assert(!p.isSpace());
+
+	assert(p.toUpper() == p);
+
+	p = '\t';
+	assert(!p.isAlpha());
+	assert(!p.isAlnum());
+	assert(!p.isDigit());
+	assert(!p.isLower());
+	assert(!p.isUpper());
+	assert( p.isControl());
+	assert(!p.isGraph());
+	assert(!p.isPrint());
+	assert(!p.isPunctuation());
+	assert(!p.isSpace());
+
+	assert(p.toUpper() == p);
+
+	p = ' ';
+	assert(!p.isAlpha());
+	assert(!p.isAlnum());
+	assert(!p.isDigit());
+	assert(!p.isLower());
+	assert(!p.isUpper());
+	assert(!p.isControl());
+	assert(!p.isGraph());
+	assert( p.isPrint());
+	assert(!p.isPunctuation());
+	assert( p.isSpace());
+
+	assert(p.toUpper() == p);
+
+	p = ':';
+	assert(!p.isAlpha());
+	assert(!p.isAlnum());
+	assert(!p.isDigit());
+	assert(!p.isLower());
+	assert(!p.isUpper());
+	assert(!p.isControl());
+	assert(!p.isGraph());
+	assert( p.isPrint());
+	assert( p.isPunctuation());
+	assert(!p.isSpace());
+
+	assert(p.toUpper() == p);
+
+	p = '+';
+	assert(!p.isAlpha());
+	assert(!p.isAlnum());
+	assert(!p.isDigit());
+	assert(!p.isLower());
+	assert(!p.isUpper());
+	assert(!p.isControl());
+	assert( p.isGraph());
+	assert( p.isPrint());
+	assert(!p.isPunctuation());
+	assert(!p.isSpace());
+
+	assert(p.toUpper() == p);
+}
+
+void CharTest::testCompare()
+{
+	Char a1('a');
+	Char a2('a');
+	Char b('b');
+
+	assert(a1 == a2);
+	assert(a1 == 'a');
+	assert(a1 == 97);
+
+	assert(b != a1);
+	assert(a1 < b);
+	assert(b > a1);
+}
+
+
+void CharTest::testOperators()
+{
+	Char a('a'); // 97
+	Char z('z'); // 122
+	Char r;
+	
+	r = z - a;
+	assert(r == 25);
+
+	r = z - 'a';
+	assert(r == 25);
+
+	r = z + a;
+	assert(r == 219);
+
+	r = z + 'a';
+	assert(r == 219);
+
+	r = z;
+	r -= a;
+	assert(r == 25);
+
+	r = a;
+	r += z;
+	assert(r == 219);
+
+
+	Char v1(0x1E0); // 111100000
+	Char v2(0x10F); // 100001111
+
+	r = v1 | v2;
+	assert(r == 0x1EF); // 111101111
+
+
+	Char v3(0x3C); // 000111100
+	Char v4(0x0F); // 000001111
+
+	r = v3 & v4;
+	assert(r == 0x0C); // 000001100
+}
---
+++ src/Pt-Text/TextStreamTest.cpp
@@ -0,0 +1,148 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Tobias Müller                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ **************************************************************************/
+
+#include <cassert>
+#include <sstream>
+#include "Pt/Text/Utf8Codec.h"
+#include "Pt/Text/Utf32Codec.h"
+#include "Pt/Text/TextStream.h"
+#include "Pt/Main.h"
+
+
+
+using namespace std;
+using namespace Pt::Text;
+
+class TextStreamTest
+{
+
+public:
+	static char _textUTF8[];
+	static Char _textUnicode[];
+
+	void testTextStreamDirectFromUTF8ToUnicode();
+	void testTextStreamGetLineFromUTF8ToUnicode();
+	void testTextBufferFromUnicodeToUTF8();
+	void testTextStreamFromUnicodeToUTF8();
+	void testTextStreamFromUTF32ToUnicode();
+};
+
+
+char TextStreamTest::_textUTF8[]    = { (char)0xce, (char)0xba, (char)0xe1, (char)0xbd, (char)0xb9, (char)0xcf, (char)0x83,
+                                        (char)0xce, (char)0xbc, (char)0xce, (char)0xb5, (char)0x0 };
+
+Char TextStreamTest::_textUnicode[] = { 954, 8057, 963, 956, 949, 0 };
+
+
+int main(int argc, char* argv[])
+{
+	TextStreamTest tst;
+
+	tst.testTextStreamDirectFromUTF8ToUnicode();
+	tst.testTextStreamGetLineFromUTF8ToUnicode();
+	tst.testTextBufferFromUnicodeToUTF8();
+	tst.testTextStreamFromUnicodeToUTF8();
+	tst.testTextStreamFromUTF32ToUnicode();
+}
+
+
+void TextStreamTest::testTextStreamDirectFromUTF8ToUnicode()
+{
+	stringstream ss(_textUTF8);
+
+	Pt::Text::TextStream textStream(ss, new Pt::Utf8Codec());
+
+	assert(textStream.get() == _textUnicode[0].value());
+	assert(textStream.get() == _textUnicode[1].value());
+	assert(textStream.get() == _textUnicode[2].value());
+	assert(textStream.get() == _textUnicode[3].value());
+	assert(textStream.get() == _textUnicode[4].value());
+}
+
+
+void TextStreamTest::testTextStreamGetLineFromUTF8ToUnicode()
+{
+	stringstream ss;
+	ss << _textUTF8;
+	
+	Pt::Text::TextStream textStream(ss, new Pt::Utf8Codec());
+
+	Char c[6];
+	textStream.getline(c, 6);
+	
+	assert(c[0] == _textUnicode[0]);
+	assert(c[1] == _textUnicode[1]);
+	assert(c[2] == _textUnicode[2]);
+	assert(c[3] == _textUnicode[3]);
+	assert(c[4] == _textUnicode[4]);
+	assert(c[5] == _textUnicode[5]);
+}
+
+
+
+void TextStreamTest::testTextBufferFromUnicodeToUTF8()
+{
+	stringstream ss;
+
+	Pt::Text::TextBuffer textBuffer(ss.rdbuf(), new Pt::Utf8Codec());
+	textBuffer.sputn(_textUnicode, 5);
+	textBuffer.pubsync();
+
+	string str = ss.str();
+	for (unsigned int i = 0; i < str.size(); i++) {
+		assert(str[i] == _textUTF8[i]);
+	}
+}
+
+
+void TextStreamTest::testTextStreamFromUnicodeToUTF8()
+{
+	stringstream ss;
+
+	Pt::Text::TextStream textStream(ss, new Pt::Utf8Codec());
+	textStream << _textUnicode;
+
+	string str = ss.str();
+	for (unsigned int i = 0; i < str.size(); i++) {
+		assert(str[i] == _textUTF8[i]);
+	}
+}
+
+
+void TextStreamTest::testTextStreamFromUTF32ToUnicode()
+{
+	stringstream ss;
+
+	Pt::Text::TextStream textStream(ss, new Pt::Utf32Codec());
+	textStream << _textUnicode;
+
+	Char c[6];
+	textStream.getline(c, 6);
+
+	cerr << c[0] << endl;
+
+	assert(c[0] == _textUnicode[0]);
+	assert(c[1] == _textUnicode[1]);
+	assert(c[2] == _textUnicode[2]);
+	assert(c[3] == _textUnicode[3]);
+	assert(c[4] == _textUnicode[4]);
+	assert(c[5] == _textUnicode[5]);
+}
+
+
