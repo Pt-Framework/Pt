0cff76c87735932e490018e05624c06a2be074c3
d-marc <d-marc@private.net>
2006-11-26 15:28:14 +0000

    CHANGED: whitespace

--- src/Pt-Text/CharTest.cpp
+++ src/Pt-Text/CharTest.cpp
@@ -156,7 +156,7 @@ void CharTest::testCategory()
 	assert(!p.isSpace());
 
 
-	p = p.toUpper();  // �
+	p = p.toUpper();  // ?
 	assert( p.isAlpha());
 	assert( p.isAlnum());
 	assert(!p.isDigit());
