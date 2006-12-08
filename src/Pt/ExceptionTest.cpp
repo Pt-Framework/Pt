411318fd2c3b1e43edebde33ec094b6e7425a193
blue_wind_25 <blue_wind_25@private.net>
2006-12-08 14:20:57 +0000

    STARTED: The writing of ExceptionTest

---
+++ src/Pt/ExceptionTest.cpp
@@ -0,0 +1,46 @@
+/***************************************************************************
+ *   Copyright (C) 2006 Aloysius Indrayanto                                *
+ *   Copyright (C) 2006 Marc Boris Duerner                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+#include "Pt/Exception.h"
+using namespace Pt;
+
+#include "Pt/Unit/Assertion.h"
+#include "Pt/Unit/TestFixture.h"
+#include "Pt/Unit/TestCase.h"
+#include "Pt/Unit/TestMain.h"
+#include "Pt/Unit/Application.h"
+
+class ExceptionTest : public Pt::Unit::TestCase
+{
+	public:
+		ExceptionTest()
+		: TestCase("ExceptionTest")
+		{}
+
+		void test()
+		{
+		}
+};
+
+Pt::Unit::RegisterTest<ExceptionTest> register_ExceptionTest;
+
