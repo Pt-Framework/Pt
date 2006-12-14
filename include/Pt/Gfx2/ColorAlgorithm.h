c6b0a46202151bf30d705cb532f5f4128dc58ec4
blue_wind_25 <blue_wind_25@private.net>
2006-12-14 13:25:06 +0000

    Working on Gfx2

--- include/Pt/Gfx2/ColorAlgorithm.h
+++ include/Pt/Gfx2/ColorAlgorithm.h
@@ -17,59 +17,19 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+#ifndef Pt_Gfx2_ColorAlgorithm_h
+#define Pt_Gfx2_ColorAlgorithm_h
 
-#include <Pt/Gfx2/ARgbColor.h>
-#include <Pt/Gfx2/ARgb8888Color.h>
-#include <Pt/Gfx2/FloatedColor.h>
-#include <Pt/Gfx2/ColorAlgorithm.h>
+#include <Pt/Gfx2/BasicColorAlgorithm.h>
 
 
 namespace Pt {
 
 	namespace Gfx {
 
-		// Explicit instantiation of the image classes
-		// Explicit instantiation of the subimage classes
-
 	} // namespace Gfx
 
 } // namespace Pt
 
+#endif
 
-namespace Pt {
-namespace Gfx {
-
-void dummyTest()
-{
-	ARgbColor        argb_1, argb_2;
-	ARgb8888Color    argb8888_1, argb8888_2;
-	FloatedARgbColor float_1, float_2;
-
-	uint16_t a, r, g, b;
-
-	toARgb(a, r, g, b, argb_1);
-	fromARgb(argb_2, a, r, g, b);
-
-	toARgb_fast(a, r, g, b, argb_1);
-	fromARgb_fast(argb_2, a, r, g, b);
-
-	toARgb(a, r, g, b, argb8888_1);
-	fromARgb(argb8888_2, a, r, g, b);
-
-	toARgb_fast(a, r, g, b, argb8888_1);
-	fromARgb_fast(argb8888_2, a, r, g, b);
-
-	assign(argb_1, argb_2);
-	assign(argb_1, argb8888_2);
-	assign(argb8888_1, argb_2);
-
-	assign_fast(argb_1, argb_2);
-	assign_fast(argb_1, argb8888_2);
-	assign_fast(argb8888_1, argb_2);
-
-	assign(float_1, argb_1);
-	assign(argb_2, float_1);
-}
-
-} // namespace Gfx
-} // namespace Pt
