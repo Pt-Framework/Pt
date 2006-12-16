3b61f0f6131784c9e105b2b70bf42e73a053dca9
d-marc <d-marc@private.net>
2006-12-16 17:17:06 +0000

    added Unit.h for docs

---
+++ include/Pt/Unit/Unit.h
@@ -0,0 +1,21 @@
+#ifndef PT_UNIT_UNIT_H
+#define PT_UNIT_UNIT_H
+
+/** \dir
+This module is the base module for all other modules. It provides
+basic types, support for byte-order handling and macros for conditional
+compilation. Support for object-modelling is included as well as the
+root of the exception hierachy.
+The core module introduces the namespace ptv and all extention modules
+use their own module-wide namespace i.e.The ptv-io module uses the
+ptv::io namespace.
+*/
+
+namespace Pt {
+
+namespace Unit {
+}
+
+}
+
+#endif
