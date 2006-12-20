5a04da118bd64af6e7d952e79327a093feed54bb
d-marc <d-marc@private.net>
2006-12-20 22:45:45 +0000

    CHANGED: more forward decls

--- include/Pt/Pt.h
+++ include/Pt/Pt.h
@@ -33,7 +33,114 @@ and the exception hierachy root. The core module introduces the namespace Pt.
 namespace Pt {
 
 	class Any;
+
+    template <typename T>
+	struct AnyTraits;
+
+	template < typename R,
+	            typename A1,
+	            typename A2,
+	            typename A3,
+	            typename A4,
+	            typename A5 >
 	class Callable;
+
+	class Connectable;
+	class Connection;
+	class ICallable;
+
+    // NOTE: change when API is ready
+	//class PropertyProxy
+	//class Property;
+
+	class Time;
+
+	template < typename R,
+                typename C,
+	            typename A1,
+	            typename A2,
+	            typename A3,
+	            typename A4,
+	            typename A5 >
+	class ConstMethod;
+
+    template <typename T>
+	class TypeInfo;
+	
+    class Date;
+
+	template < typename A1,
+	            typename A2,
+	            typename A3,
+	            typename A4,
+	            typename A5 >
+	class Invokable;
+
+    class RefCounted;
+	class Application;
+	class DateTime;
+	class Reflectable;
+
+    template <typename T>
+	class TypeTraits;
+
+    template <typename T>
+	class Arg;
+
+	template < typename R,
+	            typename A1,
+	            typename A2,
+	            typename A3 >
+	class Delegate;
+
+	template < typename A1,
+	            typename A2,
+	            typename A3 >
+	class Signal;
+
+	class Args;
+	class Event;
+
+	template <typename T, typename AllocatorT >
+	class Singleton;
+
+	class Variant;
+	class AtomicInt;
+	class Exception;
+	class Slot;
+
+	template < typename R,
+	            typename A1,
+	            typename A2,
+	            typename A3,
+	            typename A4,
+	            typename A5 >
+	class BasicSlot;
+
+    template <typename T>
+	class VariantTraits;
+
+	template < typename R,
+	            typename A1,
+	            typename A2,
+	            typename A3 >
+	class Function;
+
+	template < typename R,
+                typename C,
+	            typename A1,
+	            typename A2,
+	            typename A3,
+	            typename A4,
+	            typename A5 >
+	class Method;
+
+	template <typename T, typename ModelT >
+	class SmartPtr;
+
+	class Void;
+	class SourceInfo;
+	class NonCopyable;
 
 }
 
