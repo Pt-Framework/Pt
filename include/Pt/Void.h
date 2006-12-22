99f48423eda90987cd3f09dce9eb572d7bb56a14
blue_wind_25 <blue_wind_25@private.net>
2006-12-22 14:54:27 +0000

    Fixing some typo (hopefully)

--- include/Pt/Void.h
+++ include/Pt/Void.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Marc Boris Dürner                               *
+ *   Copyright (C) 2005 by Marc Boris DÃ¼rner                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
