42eb5d47ba58233d4d5d964dd961ae5df10419b3
d-marc <d-marc@private.net>
2007-01-07 20:43:55 +0000

    Added StreamServerSocket and impl

---
+++ include/Pt/Net/StreamServerSocket.h
@@ -0,0 +1,55 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Marc Boris Dürner, Tommi Maekitalo              *
+ *                                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef Pt_Net_StreamServerSocket_h
+#define Pt_Net_StreamServerSocket_h
+
+#include <Pt/Api.h>
+#include <string>
+
+
+namespace Pt {
+
+namespace Net {
+
+	//class StreamServerSocketImpl
+	
+    class PT_API StreamServerSocket
+    {
+        private:
+            class StreamServerSocketImpl* _impl;
+        
+        public:
+            StreamServerSocket()
+            : _impl(0)
+            { }
+            
+            ~StreamServerSocket();
+            
+            void bind(const std::string& ipaddr, unsigned short int port);
+            
+            void listen();
+    };
+
+} // !namespace Net
+
+} // !namespace Pt
+
+#endif
---
+++ src/Pt-Net/StreamServerSocket.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Marc Boris Duerner, Tommi Maekitalo             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "Pt/Net/StreamServerSocket.h"
+#include "StreamServerSocketImpl.h"
+
+
+namespace Pt {
+
+namespace Net {
+
+StreamServerSocket::~StreamServerSocket()
+{
+	delete _impl;
+}
+
+
+void StreamServerSocket::bind(const std::string& ipaddr, unsigned short int port)
+{
+
+}
+
+
+void StreamServerSocket::listen()
+{
+
+}
+
+} // namespace Net
+
+} // namespace Pt
