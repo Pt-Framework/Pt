c3254b22505ceb43389a602541a86627e6a8e348
d-marc <d-marc@private.net>
2007-01-09 19:05:23 +0000

    fixed use NO_ERRNO_H for wince builds of freetype

--- src/Pt-Gfx/freetype/src/gzip/zconf.h
+++ src/Pt-Gfx/freetype/src/gzip/zconf.h
@@ -60,6 +60,10 @@
 #  define MSDOS
 #endif
 
+#if defined(_WIN32_WCE)
+#  define NO_ERRNO_H
+#endif
+
 /*
  * Compile with -DMAXSEG_64K if the alloc function cannot allocate more
  * than 64k bytes at a time (needed on systems with 16-bit int).
