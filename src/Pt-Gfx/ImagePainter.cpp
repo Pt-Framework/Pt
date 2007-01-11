48abbdaed360e4aace888e65cb2229aaefc6d6dc
blue_wind_25 <blue_wind_25@private.net>
2007-01-11 04:07:35 +0000

    Testing Font

--- src/Pt-Gfx/ImagePainter.cpp
+++ src/Pt-Gfx/ImagePainter.cpp
@@ -41,8 +41,8 @@
 
 namespace Pt{
 namespace Gfx{
-        
-        
+
+
 ImagePainter::ImagePainter( ARgbImage& image )
 : _image( image )
 , _drawLine( 0 )
@@ -54,7 +54,7 @@ ImagePainter::ImagePainter( ARgbImage& image )
 , _fillConvexPolygon( new FillConvexPolygon() )
 {
      _drawLine      = _drawThinLine.get();
-     _drawPolyline  = _drawThinPolyline.get();         
+     _drawPolyline  = _drawThinPolyline.get();
 }
 
 ImagePainter::~ImagePainter()
@@ -140,7 +140,7 @@ void ImagePainter::drawText(const  Math::Point& to, const Text::String& text)
 
     FT_Init_FreeType( &ft );
 
-#ifdef _WIN32_WCE    
+#ifdef _WIN32_WCE
     if( FT_New_Face( ft, "\\WINDOWS\\tahoma.ttf", 0, &face ) )
         throw IllegalArgument( "FT_New_Face  error", PT_SOURCEINFO );
 
@@ -151,155 +151,155 @@ void ImagePainter::drawText(const  Math::Point& to, const Text::String& text)
 
     FT_Attach_File( face, "c:\\WINDOWS\\fonts\\tahoma" );
 #else
-    if( FT_New_Face( ft, "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf", 0, &face ) )
+    //if( FT_New_Face( ft, "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf", 0, &face ) )
+    if( FT_New_Face( ft, "/usr/share/fonts/bitstream-vera/Vera.ttf", 0, &face ) )
         throw IllegalArgument( "FT_New_Face  error", PT_SOURCEINFO );
 
-    FT_Attach_File( face, "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans" );
+    //FT_Attach_File( face, "/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans" );
 #endif
-
     if( FT_Select_Charmap( face, FT_ENCODING_UNICODE ) )
         FT_Select_Charmap( face, FT_ENCODING_NONE );
-            
+
     FT_Set_Char_Size( face, (size+2) << 6, (size+2) << 6, 72, 72 );
-        
+
     FT_GlyphSlot slot     = face->glyph;
     FT_UInt      previous = 0;
     int          pen_x    = to.x();
     int          pen_y    = to.y();
     size_t border = 1;
-           
 
-    
+
+
     for( Text::String::const_iterator it = text.begin(); it != text.end(); ++it )
     {
         {
             _pen = Pen(1,ARgbColor(0xffff,0xffff,0xffff));
             FT_UInt glyph_index = FT_Get_Char_Index( face, it->value() );
-            
+
             if( !glyph_index )
                 continue;
-            
+
             if( FT_HAS_KERNING( face ) && previous )
             {
                 FT_Vector delta;
-                
-                FT_Get_Kerning( face, previous, glyph_index, FT_KERNING_DEFAULT, &delta );       
-                pen_x += delta.x >> 6;    
+
+                FT_Get_Kerning( face, previous, glyph_index, FT_KERNING_DEFAULT, &delta );
+                pen_x += delta.x >> 6;
                 pen_y -= delta.y >> 6;
-            }                  
-            
+            }
+
             if( FT_Load_Glyph( face, glyph_index, FT_LOAD_RENDER ) )
                 continue;
-                
+
             int incX = slot->advance.x >> 6;
             int incY = slot->advance.y >> 6;
-                
-            FT_Bitmap& bm = slot->bitmap;        
-            
-            drawGlyph(pen_x + slot->bitmap_left, pen_y - slot->bitmap_top, bm.buffer, bm.width, bm.rows, bm.pitch);               
-            
-            drawGlyph(pen_x + slot->bitmap_left + border, pen_y - slot->bitmap_top, bm.buffer, bm.width, bm.rows, bm.pitch);                
-            drawGlyph(pen_x + slot->bitmap_left - border, pen_y - slot->bitmap_top, bm.buffer, bm.width, bm.rows, bm.pitch);                
-            
-            drawGlyph(pen_x + slot->bitmap_left , pen_y - slot->bitmap_top + border, bm.buffer, bm.width, bm.rows, bm.pitch);                
-            drawGlyph(pen_x + slot->bitmap_left , pen_y - slot->bitmap_top - border, bm.buffer, bm.width, bm.rows, bm.pitch);                
-            
-            drawGlyph(pen_x + slot->bitmap_left + border , pen_y - slot->bitmap_top - border, bm.buffer, bm.width, bm.rows, bm.pitch);                
-            drawGlyph(pen_x + slot->bitmap_left + border, pen_y - slot->bitmap_top + border, bm.buffer, bm.width, bm.rows, bm.pitch);                
-            
-            drawGlyph(pen_x + slot->bitmap_left  - border, pen_y - slot->bitmap_top - border, bm.buffer, bm.width, bm.rows, bm.pitch);                
-            drawGlyph(pen_x + slot->bitmap_left  - border, pen_y - slot->bitmap_top + border, bm.buffer, bm.width, bm.rows, bm.pitch);                
+
+            FT_Bitmap& bm = slot->bitmap;
+
+            drawGlyph(pen_x + slot->bitmap_left, pen_y - slot->bitmap_top, bm.buffer, bm.width, bm.rows, bm.pitch);
+
+            drawGlyph(pen_x + slot->bitmap_left + border, pen_y - slot->bitmap_top, bm.buffer, bm.width, bm.rows, bm.pitch);
+            drawGlyph(pen_x + slot->bitmap_left - border, pen_y - slot->bitmap_top, bm.buffer, bm.width, bm.rows, bm.pitch);
+
+            drawGlyph(pen_x + slot->bitmap_left , pen_y - slot->bitmap_top + border, bm.buffer, bm.width, bm.rows, bm.pitch);
+            drawGlyph(pen_x + slot->bitmap_left , pen_y - slot->bitmap_top - border, bm.buffer, bm.width, bm.rows, bm.pitch);
+
+            drawGlyph(pen_x + slot->bitmap_left + border , pen_y - slot->bitmap_top - border, bm.buffer, bm.width, bm.rows, bm.pitch);
+            drawGlyph(pen_x + slot->bitmap_left + border, pen_y - slot->bitmap_top + border, bm.buffer, bm.width, bm.rows, bm.pitch);
+
+            drawGlyph(pen_x + slot->bitmap_left  - border, pen_y - slot->bitmap_top - border, bm.buffer, bm.width, bm.rows, bm.pitch);
+            drawGlyph(pen_x + slot->bitmap_left  - border, pen_y - slot->bitmap_top + border, bm.buffer, bm.width, bm.rows, bm.pitch);
         }
-        
-        
+
+
         {
             _pen = Pen(1,ARgbColor(0,0,0));
-            
+
             FT_Set_Char_Size( face, size<< 6, size << 6, 72, 72 );
-            
+
             FT_UInt glyph_index = FT_Get_Char_Index( face, it->value() );
-            
+
             if( !glyph_index )
                 continue;
-            
+
             if( FT_HAS_KERNING( face ) && previous )
             {
                 FT_Vector delta;
-                
-                FT_Get_Kerning( face, previous, glyph_index, FT_KERNING_DEFAULT, &delta );       
-                pen_x += delta.x >> 6;    
+
+                FT_Get_Kerning( face, previous, glyph_index, FT_KERNING_DEFAULT, &delta );
+                pen_x += delta.x >> 6;
                 pen_y -= delta.y >> 6;
-            }                  
-            
+            }
+
             if( FT_Load_Glyph( face, glyph_index, FT_LOAD_RENDER ) )
                 continue;
-                
+
             int incX = slot->advance.x >> 6;
             int incY = slot->advance.y >> 6;
-                
-            FT_Bitmap& bm = slot->bitmap;        
-            
-            drawGlyph(pen_x + slot->bitmap_left, pen_y - slot->bitmap_top , bm.buffer, bm.width, bm.rows, bm.pitch);            
-            
+
+            FT_Bitmap& bm = slot->bitmap;
+
+            drawGlyph(pen_x + slot->bitmap_left, pen_y - slot->bitmap_top , bm.buffer, bm.width, bm.rows, bm.pitch);
+
             pen_x += incX;
             pen_y -= incY;
             previous = glyph_index;
-            
+
         }
-            
-        
+
+
     }
-    
+
     FT_Done_Face( face );
-    FT_Done_FreeType( ft );    
+    FT_Done_FreeType( ft );
 }
 
 void ImagePainter::drawGlyph( int penX, int penY, const Pt::uint8_t* bitmap, Pt::uint32_t bmWidth, Pt::uint32_t bmHeight, Pt::uint32_t bmPitch )
 {
     //_image.pixel(
-    
+
     Pt::ssize_t x1 = 0;
     Pt::ssize_t x2 = _image.width() - 1;
     Pt::ssize_t y1 = 0;
     Pt::ssize_t y2 = _image.height() - 1;
-    
+
     if( bmPitch < bmWidth)
-        bmPitch += bmWidth; 
-    
+        bmPitch += bmWidth;
+
     int dsy = penY;
-    
+
     for( Pt::uint32_t y = 0; y < bmHeight; y++, dsy++ )
     {
         if( dsy < y1 )
             continue;
-        
+
         if( dsy > y2 )
             break;
-        
+
         Pt::uint32_t yOffset = y * bmPitch;
-        
+
         int dsx = penX;
 
         for( Pt::uint32_t x = 0; x < bmWidth; x++, dsx++ )
         {
             if( dsx < x1 )
                 continue;
-            
+
             if( dsx > x2 )
                 break;
-            
+
             Pt::uint8_t col = bitmap[ yOffset + x ];
-            
+
             if( !col )
                 continue;
-            
+
             float af = col / 255.0f;
-            
+
            //_image.pixel(dsx, dsy)  = ARgbColor( 0, 0 ,0 );
-           
+
             mixColor( _image.pixel(dsx, dsy), _pen.color(), af );
         }
-    }                 
+    }
 }
 
 
