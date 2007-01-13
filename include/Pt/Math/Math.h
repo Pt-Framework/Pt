c392541d2cbab6d6538d8af4dbe0dd55514a6ea6
d-marc <d-marc@private.net>
2007-01-13 19:48:34 +0000

    added typedefs to Math.h

--- include/Pt/Math/Math.h
+++ include/Pt/Math/Math.h
@@ -29,24 +29,24 @@ namespace Pt {
         template <typename T>
         class BasicVector3d;
 
-		typedef BasicSize<ptv::size_t>      Size;
+		typedef BasicSize<Pt::size_t>      Size;
         typedef BasicSize<double>           SizeF;
 
-        typedef BasicPoint<ptv::ssize_t>    Point;
+        typedef BasicPoint<Pt::ssize_t>    Point;
         typedef BasicPoint<double>          PointF;
 
-        typedef BasicRect<ptv::ssize_t, ptv::size_t> Rect;
+        typedef BasicRect<Pt::ssize_t, Pt::size_t> Rect;
         typedef BasicRect<double, double>            RectF;
 
-        typedef BasicVector2d<ptv::ssize_t> Vector2d;
+        typedef BasicVector2d<Pt::ssize_t> Vector2d;
         typedef BasicVector2d<double>       Vector2dF;
 
-        typedef BasicVector3d<ptv::ssize_t> Vector3d;
+        typedef BasicVector3d<Pt::ssize_t> Vector3d;
         typedef BasicVector3d<double>       Vector3dF;
 
-        typedef BasicMatrix<ptv::ssize_t, 3, 3>   Matrix3x3;
+        typedef BasicMatrix<Pt::ssize_t, 3, 3>   Matrix3x3;
 		typedef BasicMatrix<double, 3, 3>		  Matrix3x3F;
-		typedef BasicMatrix<ptv::ssize_t, 4, 4>   Matrix4x4;
+		typedef BasicMatrix<Pt::ssize_t, 4, 4>   Matrix4x4;
         typedef BasicMatrix<double, 4, 4>         Matrix4x4F;
 
 	} // namespace Math
