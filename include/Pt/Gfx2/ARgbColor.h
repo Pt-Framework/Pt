f593402ef79699f9aff6c4d44ca79ca2eafd6dc9
blue_wind_25 <blue_wind_25@private.net>
2007-01-19 06:12:58 +0000

    Cleanups

--- include/Pt/Gfx2/ARgbColor.h
+++ include/Pt/Gfx2/ARgbColor.h
@@ -48,9 +48,6 @@ namespace Pt {
 		template <>
 		class PT_API PT_PACKED Color<ARgb> {
 			public:
-				typedef uint16_t ComponentT;
-
-			public:
 				/** @brief The default constructor, will generate the default color (black).
 				 */
 				inline Color()
@@ -207,45 +204,41 @@ namespace Pt {
 		};
 
 
-        // Pure conceptional traits class
-        // it may contain much more than just a typedef for temporary color
-        // values
-        template <typename ColorT>
-        struct ColorTraits;
+		/** @brief Choose the type which has greater size (from the two given types).
+		 */
+		template <typename A, typename B>
+		struct LargestSizeOf {
+			typedef typename IfElse< (sizeof(A) >= sizeof(B)), A, B >::ResultT Result;
+		};
 
 
-        // Specialisation for ARgbColor
-        template <>
-        struct ColorTraits<ARgbColor>
-        {
-            typedef uint32_t TmpValue;
-        };
+		// Pure conceptional traits class. Later it may contain much more than just
+		// a typedef for temporary color values
+		template <typename ColorT>
+		struct ColorTraits;
 
-
-        // Aloy, this is a pretty cool idea. I think I will move it to Pt core :)
-        template <typename A, typename B>
-        struct LargestSizeOf {
-            typedef typename IfElse< (sizeof(A) >= sizeof(B)), A, B >::ResultT Result;
-        };
+		// Full specialisation for ARgbColor
+		template <>
+		struct ColorTraits<ARgbColor> {
+			typedef uint32_t TmpValue;
+		};
 
 
 		/** @brief Mix two Color<ARgb>s using the given mixing factor
-
-            This could be the implementation detail for overloaded functions.
-            This would give us the ability to react to defect compilers that
-            are not very good with template specialisations. However we should
-            be pretty safe as long as we avoid partial specialisation.
+		 *
+		 *  This could be the implementation detail for overloaded functions.
+		 *  This would give us the ability to react to defect compilers that
+		 *  are not very good with template specialisations. However we should
+		 *  be pretty safe as long as we avoid partial specialisation.
 		 */
 		template <typename FactorT>
 		inline void mixColor(Color<ARgb>& dst, const Color<ARgb>& src, const FactorT& factor)
 		{
-			//typedef typename ResultType< Color<ARgb>::ComponentT, FactorT >::ValueT ValueT;
-
-            typedef ColorTraits<ARgbColor> Traits;
-            typedef typename LargestSizeOf< Traits::TmpValue, FactorT >::Result ValueT;
-
 			assert(  std::numeric_limits<FactorT>::is_integer() );
 			assert( !std::numeric_limits<FactorT>::is_signed () );
+
+			typedef ColorTraits<ARgbColor> Traits;
+			typedef typename LargestSizeOf< Traits::TmpValue, FactorT >::Result ValueT;
 
 			const ValueT oF = factor;
 			const ValueT rF = std::numeric_limits<FactorT>::max() - oF;
