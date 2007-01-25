dd79b87fa2f0f1a23d284a83996097141466f0b8
d-marc <d-marc@private.net>
2007-01-25 22:18:05 +0000

    whitespace

--- src/Pt-System/win32/RWLockImpl.h
+++ src/Pt-System/win32/RWLockImpl.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Marc Boris Duerner                               *
+ *   Copyright (C) 2005 by Marc Boris Duerner                              *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
