577cb5cb95561632f3cec19206abb541128411f5
avogt <avogt@private.net>
2007-01-31 13:25:44 +0000

    CHANGED: exception when accessing nonexisting properties

--- src/Pt/Reflectable.cpp
+++ src/Pt/Reflectable.cpp
@@ -23,67 +23,66 @@
 namespace Pt {
 
 Reflectable::Reflectable()
-{
-
-}
+{}
 
 
 Reflectable::~Reflectable()
 {
-	for(PropertyMap::iterator it = _properties.begin(); it != _properties.end(); ++it) {
-		delete it->second;
-	}
-	_properties.clear();
+    for(PropertyMap::iterator it = _properties.begin(); it != _properties.end(); ++it) {
+        delete it->second;
+    }
+    _properties.clear();
 
-	MethodMap::iterator it;
-	for( it = _methods.begin(); it != _methods.end(); ++it)
-	{
-		delete it->second;
-	}
+    MethodMap::iterator it;
+    for( it = _methods.begin(); it != _methods.end(); ++it)
+    {
+        delete it->second;
+    }
 
-	_methods.clear();
+    _methods.clear();
 }
 
 
 Pt::Any Reflectable::property(const std::string& name)
 {
-	PropertyMap::iterator it = _properties.find(name);
-	if(it == _properties.end())
-		return Any();
+    PropertyMap::iterator it = _properties.find(name);
+    if(it == _properties.end())
+        throw std::invalid_argument("No such property." + PT_SOURCEINFO);
 
-	return it->second->value();
+    return it->second->value();
 }
 
 
 void Reflectable::setProperty(const std::string& name, const Pt::Any& value)
 {
-	PropertyMap::iterator it = _properties.find(name);
-	if( it == _properties.end() ) {
-		std::cerr << "Reflectable: Could not set '" << name << "' = " << value << std::endl;
-		return;
-	}
+    PropertyMap::iterator it = _properties.find(name);
+    if( it == _properties.end() ) {
+        std::cerr << "Reflectable: Could not set '" << name << "' = " << value << std::endl;
+        throw std::invalid_argument("No such property." + PT_SOURCEINFO);
+        return;
+    }
 
-	it->second->setValue(value);
+    it->second->setValue(value);
 }
 
 
 const ICallable& Reflectable::method(const std::string& name) const
 {
-	MethodMap::const_iterator it = _methods.find(name);
-	if( it == _methods.end() )
-		throw std::invalid_argument("No such method." + PT_SOURCEINFO);
+    MethodMap::const_iterator it = _methods.find(name);
+    if( it == _methods.end() )
+        throw std::invalid_argument("No such method." + PT_SOURCEINFO);
 
-	return *(it->second);
+    return *(it->second);
 }
 
 
 void Reflectable::call(const std::string& name, const Args& args)
 {
-	MethodMap::iterator it = _methods.find(name);
-	if( it == _methods.end() )
-		throw std::invalid_argument("No such method." + PT_SOURCEINFO);
+    MethodMap::iterator it = _methods.find(name);
+    if( it == _methods.end() )
+        throw std::invalid_argument("No such method." + PT_SOURCEINFO);
 
-	it->second->call(args);
+    it->second->call(args);
 }
 
 } // namespace Pt
