a581efd2156f8e5e952695abe70c4d93c95239b4
d-marc <d-marc@private.net>
2007-02-06 10:26:58 +0000

    extended demo

--- src/Pt-Gfx/DrawLineDemo.cpp
+++ src/Pt-Gfx/DrawLineDemo.cpp
@@ -37,141 +37,137 @@
 
 class DrawLineDemo : public Pt::Gui::Widget
 {
-	public:
-	DrawLineDemo()
-	: _image( )
-	, _imagePainter( _image )
-	, _angle(0 )
-	{
-		this->setTitle(L"DrawLineDemo");
-	}
+    public:
+        DrawLineDemo()
+        : _image( )
+        , _imagePainter( _image )
+        , _angle(0 )
+        {
+            this->setTitle(L"DrawLineDemo");
+        }
 
-	virtual ~DrawLineDemo()
-	{}
+        void drawStar(Gfx::Painter& painter, const Pt::Math::Point& center, size_t lineWidth)
+        {
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x(), center.y()-120 ),
+                                    Pt::Math::Point( center.x(), center.y()-50 ));
 
-	virtual void _paintEvent(const Pt::Gui::PaintEvent& event)
-	{
-        _imagePainter.setPen( Pt::Gfx::Pen( 3, Pt::Gfx::ARgbColor(0, 0xdddd, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 20, 10 ), Pt::Math::Point( 160, 30 ));
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x(), center.y()-120 ),
+                                    Pt::Math::Point( center.x(), center.y()-50 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 20, 10 ), Pt::Math::Point( 160, 30 ));
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()-120 ),
+                                    Pt::Math::Point( center.x(), center.y()-30 ));
 
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()-120 ),
+                                    Pt::Math::Point( center.x(), center.y()-30 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 4, Pt::Gfx::ARgbColor(0, 0xdddd, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 20, 20 ), Pt::Math::Point( 160, 40 ));
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()-70 ),
+                                    Pt::Math::Point( center.x(), center.y()-20 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 20, 20 ), Pt::Math::Point( 160, 40 ));
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()-70 ),
+                                    Pt::Math::Point( center.x(), center.y()-20 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 5, Pt::Gfx::ARgbColor(0, 0xdddd, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 20, 30 ), Pt::Math::Point( 160, 50 ));
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()-20 ),
+                                    Pt::Math::Point( center.x(), center.y()-10 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 20, 30 ), Pt::Math::Point( 160, 50 ));
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()-20 ),
+                                    Pt::Math::Point( center.x(), center.y()-10 ));
 
-        painter().drawImage( Pt::Math::Point( 0, 0 ), _image );
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y() ), center);
 
-/*
-		//
-		// draw wide lines of varying widths using the ImagePainter
-		//
-		_imagePainter.setPen( Pt::Gfx::Pen( 2, Pt::Gfx::ARgbColor(0, 0xffff,0 ) ));
-        _imagePainter.drawLine( Pt::Math::Point( 10, 10 ), Pt::Math::Point( 55, -155 ));
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y() ), center);
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 3, Pt::Gfx::ARgbColor(0, 0xffff, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 20, 10 ), Pt::Math::Point( 160, 150 ));
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()+20 ),
+                                    Pt::Math::Point( center.x(), center.y()+10 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 4, Pt::Gfx::ARgbColor(0, 0xffff, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 30, 10 ), Pt::Math::Point( 70, 50 ));
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()+20 ),
+                                    Pt::Math::Point( center.x(), center.y()+10 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 5, Pt::Gfx::ARgbColor(0, 0xffff, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 40, 10 ), Pt::Math::Point( 80, 50 ));
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()+70 ),
+                                    Pt::Math::Point( center.x(), center.y()+20 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 6, Pt::Gfx::ARgbColor(0, 0xffff, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 52, 10 ), Pt::Math::Point( 92, 50 ));
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()+70 ),
+                                    Pt::Math::Point( center.x(), center.y()+20 ));
 
-        _imagePainter.setPen( Pt::Gfx::Pen( 7, Pt::Gfx::ARgbColor(0, 0xffff, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 65, 10 ), Pt::Math::Point( 105, 50 ));
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()+120 ),
+                                    Pt::Math::Point( center.x(), center.y()+30 ));
 
-		//
-		// draw thin lines over the wide lines to mark center using the ImagePainter
-		//
-        _imagePainter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
-        _imagePainter.drawLine( Pt::Math::Point( 10, 10 ), Pt::Math::Point( 150, 150 ));
-        _imagePainter.drawLine( Pt::Math::Point( 20, 10 ), Pt::Math::Point( 60, 50 ));
-        _imagePainter.drawLine( Pt::Math::Point( 30, 10 ), Pt::Math::Point( 70, 50 ));
-        _imagePainter.drawLine( Pt::Math::Point( 40, 10 ), Pt::Math::Point( 80, 50 ));
-        _imagePainter.drawLine( Pt::Math::Point( 52, 10 ), Pt::Math::Point( 92, 50 ));
-        _imagePainter.drawLine( Pt::Math::Point( 65, 10 ), Pt::Math::Point( 105, 50 ));
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x() - 50, center.y()+120 ),
+                                    Pt::Math::Point( center.x(), center.y()+30 ));
 
-        painter().drawImage( Pt::Math::Point( 0, 0 ), _image );
+            painter.setPen( Pt::Gfx::Pen( lineWidth, Pt::Gfx::ARgbColor(0, 0xcccc, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x(), center.y()+120 ),
+                                    Pt::Math::Point( center.x(), center.y()+50 ));
 
+            painter.setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
+            painter.drawLine( Pt::Math::Point( center.x(), center.y()+120 ),
+                                    Pt::Math::Point( center.x(), center.y()+50 ));
+        }
 
-		//
-		// draw wide lines of varying widths using the Gui::Painter
-		//
-        painter().setPen( Pt::Gfx::Pen( 2, Pt::Gfx::ARgbColor(0, 0, 0xffff) ));
-        painter().drawLine( Pt::Math::Point( 55, 55 ), Pt::Math::Point( 100, 100 ));
+        virtual void _paintEvent(const Pt::Gui::PaintEvent& event)
+        {
+            this->drawStar( _imagePainter, Pt::Math::Point(60, 150), 2);
+            this->drawStar( _imagePainter, Pt::Math::Point(120, 150), 3);
+            this->drawStar( _imagePainter, Pt::Math::Point(180, 150), 4);
+            this->drawStar( _imagePainter, Pt::Math::Point(240, 150), 5);
+            this->drawStar( _imagePainter, Pt::Math::Point(300, 150), 6);
+            this->drawStar( _imagePainter, Pt::Math::Point(360, 150), 8);
+            painter().drawImage( Pt::Math::Point( 0, 0 ), _image );
 
-        painter().setPen( Pt::Gfx::Pen( 3, Pt::Gfx::ARgbColor(0, 0, 0xffff) ));
-        painter().drawLine( Pt::Math::Point( 65, 55 ), Pt::Math::Point( 110, 100 ));
+            Gui::Painter widgetPainter = painter();
+            this->drawStar( widgetPainter, Pt::Math::Point(60, 450), 2);
+            this->drawStar( widgetPainter,Pt::Math::Point(120, 450), 3);
+            this->drawStar( widgetPainter,Pt::Math::Point(180, 450), 4);
+            this->drawStar( widgetPainter,Pt::Math::Point(240, 450), 5);
+            this->drawStar( widgetPainter,Pt::Math::Point(300, 450), 6);
+            this->drawStar( widgetPainter,Pt::Math::Point(360, 450), 8);
+        }
 
-        painter().setPen( Pt::Gfx::Pen( 4, Pt::Gfx::ARgbColor(0, 0, 0xffff) ));
-        painter().drawLine( Pt::Math::Point( 75, 55 ), Pt::Math::Point( 120, 100 ));
+        virtual void _resizeEvent(const Pt::Gui::ResizeEvent& event)
+        {
+            _image.resize(  event.width(), event.height(), Pt::Gfx::ARgbColor( 0xffff, 0, 0 ) );
+        }
 
-        painter().setPen( Pt::Gfx::Pen( 5, Pt::Gfx::ARgbColor(0, 0, 0xffff) ));
-        painter().drawLine( Pt::Math::Point( 85, 55 ), Pt::Math::Point( 130, 100 ));
-
-        painter().setPen( Pt::Gfx::Pen( 6, Pt::Gfx::ARgbColor(0, 0, 0xffff) ));
-        painter().drawLine( Pt::Math::Point( 97, 55 ), Pt::Math::Point( 142, 100 ));
-
-        painter().setPen( Pt::Gfx::Pen( 7, Pt::Gfx::ARgbColor(0, 0, 0xffff) ));
-        painter().drawLine( Pt::Math::Point( 110, 55 ), Pt::Math::Point( 155, 100 ));
-
-		//
-		// draw thin lines over the wide lines to mark center using the Gui::Painter
-		//
-        painter().setPen( Pt::Gfx::Pen( 1, Pt::Gfx::ARgbColor(0, 0, 0) ));
-        painter().drawLine( Pt::Math::Point( 55, 55 ), Pt::Math::Point( 100, 100 ));
-        painter().drawLine( Pt::Math::Point( 65, 55 ), Pt::Math::Point( 110, 100 ));
-        painter().drawLine( Pt::Math::Point( 75, 55 ), Pt::Math::Point( 120, 100 ));
-        painter().drawLine( Pt::Math::Point( 85, 55 ), Pt::Math::Point( 130, 100 ));
-        painter().drawLine( Pt::Math::Point( 97, 55 ), Pt::Math::Point( 142, 100 ));
-        painter().drawLine( Pt::Math::Point( 110, 55 ), Pt::Math::Point( 155, 100 ));
-*/
-    }
-
-	virtual void _resizeEvent(const Pt::Gui::ResizeEvent& event)
-	{
-		_image.resize(  event.width(), event.height(), Pt::Gfx::ARgbColor( 0xffff, 0, 0 ) );
-	}
-
-private:
-	Pt::Gfx::ARgbImage		_image;
-	Pt::Gfx::ImagePainter	_imagePainter;
-	Pt::ssize_t				_angle;
+    private:
+        Pt::Gfx::ARgbImage _image;
+        Pt::Gfx::ImagePainter _imagePainter;
+        Pt::ssize_t _angle;
 };
 
 
 int main( int argc, char* argv[] )
 {
-	try
-	{
-		Pt::Gui::Application app;
+    try
+    {
+        Pt::Gui::Application app;
 
-		DrawLineDemo demo;
-		connect( demo.closed, app, &Pt::Gui::Application::exit );
+        DrawLineDemo demo;
+        connect( demo.closed, app, &Pt::Gui::Application::exit );
 
-		demo.resize(400, 300);
-		demo.show();
-		return app.run();
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
-		return 1;
-	}
+        demo.resize(400, 600);
+        demo.show();
+        return app.run();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return 1;
+    }
 
-	return 0;
+    return 0;
 }
