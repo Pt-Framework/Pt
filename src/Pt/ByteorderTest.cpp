2ebc130e8d3d1523ef3c898af943f7255d42b258
blue_wind_25 <blue_wind_25@private.net>
2007-02-10 12:03:02 +0000

    Small cleanups

--- src/Pt/ByteorderTest.cpp
+++ src/Pt/ByteorderTest.cpp
@@ -17,8 +17,6 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-#if 1
-
 #include <iostream>
 using namespace std;
 
@@ -31,314 +29,57 @@ using namespace std;
 
 class ByteorderTest : public Pt::Unit::TestCase
 {
-	public:
-	ByteorderTest()
-	: TestCase("ByteorderTest")
-		{ }
+    public:
+    ByteorderTest()
+    : TestCase("ByteorderTest")
+        { }
 
-		virtual void test()
-		{
-			#ifdef PT_LE
-				Pt::uint16_t a = 0xaabb;
-				PT_UNIT_ASSERT( Pt::uint16_t(0xbbaa) == Pt::hostToBe(a) );
+        virtual void test()
+        {
+            #ifdef PT_LE
+                Pt::uint16_t a = 0xaabb;
+                PT_UNIT_ASSERT( Pt::uint16_t(0xbbaa) == Pt::hostToBe(a) );
 
-				Pt::int16_t b = (Pt::int16_t)(0xaabb);
-				PT_UNIT_ASSERT( Pt::int16_t(0xbbaa) == Pt::hostToBe(b) );
+                Pt::int16_t b = (Pt::int16_t)(0xaabb);
+                PT_UNIT_ASSERT( Pt::int16_t(0xbbaa) == Pt::hostToBe(b) );
 
-				Pt::uint32_t c = 0xaabbccdd;
-				PT_UNIT_ASSERT( Pt::uint32_t(0xddccbbaa) == Pt::hostToBe(c) );
+                Pt::uint32_t c = 0xaabbccdd;
+                PT_UNIT_ASSERT( Pt::uint32_t(0xddccbbaa) == Pt::hostToBe(c) );
 
-				Pt::int32_t d = 0xaabbccdd;
-				PT_UNIT_ASSERT( Pt::int32_t(0xddccbbaa) == Pt::hostToBe(d) );
+                Pt::int32_t d = 0xaabbccdd;
+                PT_UNIT_ASSERT( Pt::int32_t(0xddccbbaa) == Pt::hostToBe(d) );
 
-			   #ifdef PTV_64BIT
-				Pt::uint64_t e = 0x1122334455667788ULL;
-				PT_UNIT_ASSERT( Pt::uint64_t(0x8877665544332211ULL) == Pt::hostToBe(e) );
+               #ifdef PTV_64BIT
+                Pt::uint64_t e = 0x1122334455667788ULL;
+                PT_UNIT_ASSERT( Pt::uint64_t(0x8877665544332211ULL) == Pt::hostToBe(e) );
 
-				Pt::int64_t f = 0x1122334455667788LL;
-				PT_UNIT_ASSERT( Pt::int64_t(0x8877665544332211LL) == Pt::hostToBe(f) );
-			   #endif
-			#elif PT_BE
-				Pt::uint16_t a = 0xaabb;
-				PT_UNIT_ASSERT( Pt::uint16_t(0xaabb) == Pt::hostToBe(a) );
+                Pt::int64_t f = 0x1122334455667788LL;
+                PT_UNIT_ASSERT( Pt::int64_t(0x8877665544332211LL) == Pt::hostToBe(f) );
+               #endif
+            #elif PT_BE
+                Pt::uint16_t a = 0xaabb;
+                PT_UNIT_ASSERT( Pt::uint16_t(0xaabb) == Pt::hostToBe(a) );
 
-				Pt::int16_t b = (Pt::int16_t)(0xaabb);
-				PT_UNIT_ASSERT( Pt::int16_t(0xaabb) == Pt::hostToBe(b) );
+                Pt::int16_t b = (Pt::int16_t)(0xaabb);
+                PT_UNIT_ASSERT( Pt::int16_t(0xaabb) == Pt::hostToBe(b) );
 
-				Pt::uint32_t c = 0xaabbccdd;
-				PT_UNIT_ASSERT( Pt::uint32_t(0xaabbccdd) == Pt::hostToBe(c) );
+                Pt::uint32_t c = 0xaabbccdd;
+                PT_UNIT_ASSERT( Pt::uint32_t(0xaabbccdd) == Pt::hostToBe(c) );
 
-				Pt::int32_t d = 0xaabbccdd;
-				PT_UNIT_ASSERT( Pt::int32_t(0xaabbccdd) == Pt::hostToBe(d) );
+                Pt::int32_t d = 0xaabbccdd;
+                PT_UNIT_ASSERT( Pt::int32_t(0xaabbccdd) == Pt::hostToBe(d) );
 
-			   #ifdef PTV_64BIT
-				Pt::uint64_t e = 0x1122334455667788ULL;
-				PT_UNIT_ASSERT( Pt::uint64_t(0x1122334455667788ULL) == Pt::hostToBe(e) );
+               #ifdef PTV_64BIT
+                Pt::uint64_t e = 0x1122334455667788ULL;
+                PT_UNIT_ASSERT( Pt::uint64_t(0x1122334455667788ULL) == Pt::hostToBe(e) );
 
-				Pt::int64_t f = 0x1122334455667788LL;
-				PT_UNIT_ASSERT( Pt::int64_t(0x1122334455667788LL) == Pt::hostToBe(f) );
-			   #endif
-			#else
+                Pt::int64_t f = 0x1122334455667788LL;
+                PT_UNIT_ASSERT( Pt::int64_t(0x1122334455667788LL) == Pt::hostToBe(f) );
+               #endif
+            #else
                 #error "Neither PT_BE not PT_LE is defined"
-			#endif
-		}
-	};
+            #endif
+        }
+    };
 
 Pt::Unit::RegisterTest<ByteorderTest> register_ByteorderTest;
-
-#else
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-
-extern uint16_t swap16(uint16_t value)
-{
-#if 0
-	value = ( (value & 0x00FF) << 8 ) |
-				  ( (value & 0xFF00) >> 8 );
-  return(value);
-	/*
-	movzwl	8(%ebp), %edx
-	movl	%edx, %eax
-	sall	$8, %eax
-	shrl	$8, %edx
-	orl	%edx, %eax
-	movzwl	%ax, %eax
-	leave
-	ret
-	*/
-#else
-	union {
-		uint16_t v;
-		uint8_t  b[2];
-	} u;
-
-	u.v = value;
-
-	const uint8_t b0 = u.b[0];
-	const uint8_t b1 = u.b[1];
-	u.b[0] = b1;
-	u.b[1] = b0;
-
-	return(u.v);
-	/*
-	movl	8(%ebp), %edx
-	movl	%edx, %eax
-	movzbl	%dh, %ecx
-	movb	%cl, %al
-	movb	%dl, %ah
-	movzwl	%ax, %eax
-	leave
-	ret
-	*/
-#endif
-}
-
-extern uint32_t swap32(uint32_t value)
-{
-#if 0
-	value = ( (value & 0x000000FF) << 24 ) |
-				  ( (value & 0x0000FF00) <<  8 ) |
-				  ( (value & 0x00FF0000) >>  8 ) |
-				  ( (value & 0xFF000000) >> 24 );
-	return(value);
-	/*
-	movl	8(%ebp), %ecx
-	movl	%ecx, %eax
-	sall	$24, %eax
-	movl	%ecx, %edx
-	andl	$65280, %edx
-	sall	$8, %edx
-	orl	%edx, %eax
-	movl	%ecx, %edx
-	andl	$16711680, %edx
-	shrl	$8, %edx
-	shrl	$24, %ecx
-	orl	%ecx, %edx
-	orl	%edx, %eax
-	leave
-	ret
-	*/
-#else
-	uint8_t *w = reinterpret_cast<uint8_t*>(&value);
-
-	const uint8_t w0 = w[0];
-	const uint8_t w1 = w[1];
-	const uint8_t w2 = w[2];
-	const uint8_t w3 = w[3];
-	w[0] = w3;
-	w[1] = w2;
-	w[2] = w1;
-	w[3] = w0;
-	return(value);
-	/*
-	movb	8(%ebp), %cl
-	movb	9(%ebp), %dl
-	movb	11(%ebp), %al
-	movb	%al, 8(%ebp)
-	movb	10(%ebp), %al
-	movb	%al, 9(%ebp)
-	movb	%dl, 10(%ebp)
-	movb	%cl, 11(%ebp)
-	movl	8(%ebp), %eax
-	leave
-	ret
-	*/
-#endif
-}
-
-extern uint64_t swap64(uint64_t value)
-{
-#if 0
-	value = ( (value & 0x00000000000000FFULL) << 56 ) |
-					( (value & 0x000000000000FF00ULL) << 40 ) |
-					( (value & 0x0000000000FF0000ULL) << 24 ) |
-					( (value & 0x00000000FF000000ULL) <<  8 ) |
-					( (value & 0x000000FF00000000ULL) >>  8 ) |
-					( (value & 0x0000FF0000000000ULL) >> 24 ) |
-					( (value & 0x00FF000000000000ULL) >> 40 ) |
-					( (value & 0xFF00000000000000ULL) >> 56 );
-	return(value);
-	/*
-	movzbl	8(%ebp),%eax
-	xorl	%edx, %edx
-	movl	%eax, %edx
-	movl	$0, %eax
-	sall	$24, %edx
-	movl	8(%ebp), %ecx
-	andl	$65280, %ecx
-	xorl	%ebx, %ebx
-	movl	%ecx, %ebx
-	movl	$0, %ecx
-	sall	$8, %ebx
-	orl	%ecx, %eax
-	orl	%ebx, %edx
-	xorl	%ecx, %ecx
-	movzbl	12(%ebp),%ebx
-	shrdl	$8, %ebx, %ecx
-	shrl	$8, %ebx
-	orl	%ecx, %eax
-	orl	%ebx, %edx
-	xorl	%ecx, %ecx
-	movl	12(%ebp), %ebx
-	andl	$16711680, %ebx
-	movl	%ebx, %ecx
-	xorl	%ebx, %ebx
-	shrl	$8, %ecx
-	orl	%ecx, %eax
-	orl	%ebx, %edx
-	movl	8(%ebp), %ecx
-	andl	$16711680, %ecx
-	xorl	%ebx, %ebx
-	shldl	$24, %ecx, %ebx
-	sall	$24, %ecx
-	movl	8(%ebp), %esi
-	andl	$-16777216, %esi
-	xorl	%edi, %edi
-	shldl	$8, %esi, %edi
-	sall	$8, %esi
-	orl	%esi, %ecx
-	orl	%edi, %ebx
-	xorl	%esi, %esi
-	movl	12(%ebp), %edi
-	andl	$65280, %edi
-	shrdl	$24, %edi, %esi
-	shrl	$24, %edi
-	orl	%esi, %ecx
-	orl	%edi, %ebx
-	xorl	%esi, %esi
-	movl	12(%ebp), %edi
-	andl	$-16777216, %edi
-	movl	%edi, %esi
-	xorl	%edi, %edi
-	shrl	$24, %esi
-	orl	%esi, %ecx
-	orl	%edi, %ebx
-	orl	%ecx, %eax
-	orl	%ebx, %edx
-	popl	%ebx
-	popl	%esi
-	popl	%edi
-	leave
-	ret
-	*/
-#else
-	uint8_t *w = reinterpret_cast<uint8_t*>(&value);
-
-	const uint8_t w0 = w[0];
-	const uint8_t w1 = w[1];
-	const uint8_t w2 = w[2];
-	const uint8_t w3 = w[3];
-	const uint8_t w4 = w[4];
-	const uint8_t w5 = w[5];
-	const uint8_t w6 = w[6];
-	const uint8_t w7 = w[7];
-	w[0] = w7;
-	w[1] = w6;
-	w[2] = w5;
-	w[3] = w4;
-	w[4] = w3;
-	w[5] = w2;
-	w[6] = w1;
-	w[7] = w0;
-	return(value);
-	/*
-	movl	8(%ebp), %eax
-	movl	%eax, -16(%ebp)
-	movl	12(%ebp), %eax
-	movl	%eax, -12(%ebp)
-	movb	-16(%ebp), %al
-	movb	%al, -17(%ebp)
-	movb	-15(%ebp), %bl
-	movb	-14(%ebp), %cl
-	movb	-13(%ebp), %dl
-	movb	-9(%ebp), %al
-	movb	%al, -16(%ebp)
-	movb	-10(%ebp), %al
-	movb	%al, -15(%ebp)
-	movb	-11(%ebp), %al
-	movb	%al, -14(%ebp)
-	movb	-12(%ebp), %al
-	movb	%al, -13(%ebp)
-	movb	%dl, -12(%ebp)
-	movb	%cl, -11(%ebp)
-	movb	%bl, -10(%ebp)
-	movb	-17(%ebp), %al
-	movb	%al, -9(%ebp)
-	movl	-16(%ebp), %eax
-	movl	-12(%ebp), %edx
-	addl	$20, %esp
-	popl	%ebx
-	leave
-	ret
-	*/
-#endif
-}
-
-int main(int argc, void *argv[])
-{
-	uint16_t X = 0x2010;
-	uint32_t Y = 0x40302010;
-	uint64_t Z = 0x8070605040302010ULL;
-
-	printf("%04X\n", X);
-	X = swap16(X);
-	printf("%04X\n", X);
-
-	printf("\n");
-
-	printf("%08X\n", Y);
-	Y = swap32(Y);
-	printf("%08X\n", Y);
-
-	printf("\n");
-
-	printf("%08LX\n", Z);
-	Z = swap64(Z);
-	printf("%08LX\n", Z);
-
-	return(0);
-}
-
-#endif
