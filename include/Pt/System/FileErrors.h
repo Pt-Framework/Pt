4aca6330dbe2a3b8a4c0f1a056a9df298b9e7624
slyh_ <slyh_@private.net>
2007-03-15 15:30:23 +0000

    INITIAL_VERSION: FileErrors, containing exception for File operations.

---
+++ include/Pt/System/FileErrors.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   Copyright (C) 2007 Tobias Mueller                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef PT_SYSTEM_FILEERRORS_H
+#define PT_SYSTEM_FILEERRORS_H
+
+#include <Pt/System/Api.h>
+#include <Pt/Exception.h>
+#include <Pt/System/File.h>
+
+namespace Pt {
+
+namespace System {
+
+    class PT_SYSTEM_API FileNotFound : public std::runtime_error
+    {
+        public:
+            FileNotFound(const File& file, const SourceInfo& si);
+
+            ~FileNotFound() throw();
+    };
+
+} // namespace System
+
+} // namespace Pt
+
+#endif
