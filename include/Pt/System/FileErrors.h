b65f48d1536f464b26d85aff897e4d2c936cca3e
slyh_ <slyh_@private.net>
2007-03-28 16:25:03 +0000

    FIXED: Header commentary.

--- include/Pt/System/FileErrors.h
+++ include/Pt/System/FileErrors.h
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2007 Tobias Mueller                                     *
+ *   Copyright (C) 2007 PTV AG                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
