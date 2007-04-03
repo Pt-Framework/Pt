47c6a4086fc26dd330573075ee4f8a3fadb86712
d-marc <d-marc@private.net>
2007-04-03 09:06:28 +0000

    fixed IOMonitorDemo to use std::cerr

--- src/Pt-System/IOMonitorDemo.cpp
+++ src/Pt-System/IOMonitorDemo.cpp
@@ -36,45 +36,45 @@ class Multiplexer : public Pt::Connectable
 {
     public:
         Multiplexer()
-        : _device("/dev/ttyS0", std::ios_base::in)
-//        , _device2("COM1:", std::ios_base::in)
-        ,_out("ser.txt")
+        : _device("/dev/ttyUSB0", std::ios_base::in)
+        //, _device2("COM1:", std::ios_base::in)
         {
             _device.setBaudRate(Pt::System::SerialDevice::BaudRate4800);
-            _device.setCharSize(7);
+            _device.setCharSize(8);
             _device.setStopBits(Pt::System::SerialDevice::OneStopBit);
             _device.setParity(Pt::System::SerialDevice::ParityEven);
             _device.setTimeout( 10 );
-
-/*            _device2.setBaudRate(Pt::System::SerialDevice::BaudRate1200);
+/*
+            _device2.setBaudRate(Pt::System::SerialDevice::BaudRate1200);
             _device2.setCharSize(7);
             _device2.setStopBits(Pt::System::SerialDevice::OneStopBit);
             _device2.setParity(Pt::System::SerialDevice::ParityEven);
-            _device2.setTimeout( 10 );*/
+            _device2.setTimeout( 10 );
+*/
 
-            Pt::Signal<const Pt::System::IOEvent&>& signal = _monitor.addDevice( _device, Pt::System::IODevice::WaitInput | Pt::System::IODevice::WaitOutput );
+            Pt::Signal<const Pt::System::IOEvent&>& signal = _monitor.addDevice( _device, Pt::System::IODevice::WaitInput);
             Pt::connect( signal, *this, &Multiplexer::onIOEvent );
 
-//            Pt::Signal<const Pt::System::IOEvent&>& signal2 = _monitor.addDevice( _device2, Pt::System::IODevice::WaitOutput );
-//            Pt::connect( signal2, *this, &Multiplexer::onIOEvent2 );
+            //Pt::Signal<const Pt::System::IOEvent&>& signal2 = _monitor.addDevice( _device2, Pt::System::IODevice::WaitOutput );
+            //Pt::connect( signal2, *this, &Multiplexer::onIOEvent2 );
 
             Pt::connect( _monitor.timeout, *this, &Multiplexer::onTimeout );
         }
 
         void run()
         {
-            for(int i = 0; i < 200; ++i)
+            for(int i = 0; i < 5000; ++i)
             {
-                _monitor.wait(200);
+                _monitor.wait(100);
             }
 
             _monitor.removeDevice( _device );
-//            _monitor.removeDevice( _device2 );
+            //_monitor.removeDevice( _device2 );
         }
 
         void onTimeout( )
         {
-            _out << "--- TIMEOUT ---" << std::endl;
+            std::cerr << "--- TIMEOUT ---" << std::endl;
         }
 
         void onIOEvent( const Pt::System::IOEvent& ev )
@@ -86,7 +86,7 @@ class Multiplexer : public Pt::Connectable
                 char buffer[201];
                 memset( buffer, 0, 201);
                 size_t size = _device.read( buffer, 200);
-                _out.write(buffer, size);
+                std::cerr.write(buffer, size);
             }
         }
 
@@ -98,16 +98,16 @@ class Multiplexer : public Pt::Connectable
             {
                 char buffer[201];
                 memset( buffer, 0, 201);
-//                size_t size = _device2.read( buffer, 200);
-                _out << "MOUSE IO:";
-                //_out.write(buffer, size);
-                _out << std::endl;
+                //size_t size = _device2.read( buffer, 200);
+                std::cerr << "MOUSE IO:";
+                //std::cerr.write(buffer, size);
+                std::cerr << std::endl;
             }
         }
 
     private:
         Pt::System::SerialDevice _device;
-//        Pt::System::SerialDevice _device2;
+        //Pt::System::SerialDevice _device2;
         Pt::System::IOMonitor    _monitor;
         std::ofstream            _out;
 };
@@ -119,11 +119,11 @@ int main( int argc, char* argv[] )
     {
         Multiplexer m;
         m.run();
-        //_out << "\n\nSUCCESS\n";
+        std::cerr << "\n\nSUCCESS\n";
     }
     catch( const std::exception& e )
     {
-        //_out<<e.what()<<std::endl;
+        std::cerr << e.what() << std::endl;
     }
 
     return 0;
