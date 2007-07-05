e447275df98e31ae6d1c0b50c1d174ba0d09efd4
b_buedi <b_buedi@private.net>
2007-07-05 14:48:52 +0000

    INITIAL-VERSION: polygon class

---
+++ include/Pt/Math/Polygon.h
@@ -0,0 +1,126 @@
+/***************************************************************************
+ *   Copyright (C) 2007 PTV AG                                             *
+ ***************************************************************************/
+
+#ifndef PT_MATH_POLYGON_H
+#define PT_MATH_POLYGON_H
+
+#include <Pt/Math/Api.h>
+#include <Pt/Math/Point.h>
+#include <Pt/Math/Math.h>
+#include <list>
+
+namespace Pt {
+namespace Math {
+
+//! \brief A generic line segment class.
+template<typename PointT>
+class BasicPolygon
+{
+public:
+    //! A basic class for polygons
+    BasicPolygon(const std::list< BasicPoint<PointT> >& pointList)
+    : _points(pointList)
+    {
+    }
+
+    BasicPolygon()
+    {
+    }
+    
+    void addPoint(BasicPoint<PointT> point)
+    {
+        _points.push_back(point);
+    }
+
+    template <typename T>
+    bool operator==(const BasicPolygon<T>& other) const
+    {
+        if (other.size() != this->size())
+        {
+            return false;
+        }
+        
+        typename std::list< BasicPoint<T> >::const_iterator      otherIt = other.points().begin();
+        typename std::list< BasicPoint<PointT> >::const_iterator thisIt  = this->_points.begin();
+        
+        while (thisIt != this->_points.end())
+        {
+            if (otherIt->x() != thisIt->x() || otherIt->y() != thisIt->y())
+            {
+                return false;
+            }
+            
+            ++otherIt;
+            ++thisIt;
+        }
+        
+        return true;
+    }
+
+
+    template <typename T>
+    bool operator!=(const BasicPolygon<T>& other) const
+    {
+        return !(*this == other);
+    }
+    
+    
+    Pt::size_t size() const
+    {
+        return _points.size();
+    }
+    
+    
+    const std::list< BasicPoint<PointT> >& points() const
+    {
+        return _points;
+    }
+    
+    
+    template <typename T>
+    inline bool contains(const BasicPoint<T>& point) const
+    {
+        if (this->size() < 3)
+        {
+            return false;
+        }
+    
+        bool result = false;
+        
+        BasicPoint<PointT> last = _points.back();
+        typename std::list< BasicPoint<PointT> >::const_iterator it;
+        
+        for (it = _points.begin(); it != _points.end(); ++it)
+        {
+            BasicPoint<PointT> current = *it;
+            
+            if ((current.y() <= point.y() && point.y() < last.y()) ||
+                (last.y()    <= point.y() && point.y() < current.y()))
+            {
+                // y-value of point lies between y-value of last and y-value of current point.
+                // Now check if the x-value of the point is left of the line (between last point
+                // and current point). If so, we passed a line and thus have to invert our result.
+                double xPositionOnLine = (last.x() - current.x()) * (point.y() - current.y())
+                                       / double(last.y() - current.y()) + current.x();
+                
+                if (double(point.x()) <= xPositionOnLine)
+                {
+                    result = !result;
+                }
+            }
+            
+            last = current;
+        }
+        
+        return result;
+    }
+
+protected:
+    std::list< BasicPoint<PointT> > _points;
+};
+
+} // namespace Math
+} // namespace Pt
+
+#endif
