9453c7d8cc786d2f8f2e4212b07beeeb56502193
d-marc <d-marc@private.net>
2007-07-05 08:33:56 +0000

    undefine PT_API for unit test

--- src/Pt/SettingsTest.cpp
+++ src/Pt/SettingsTest.cpp
@@ -16,7 +16,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-#undef PT_API_LOG_EXPORT
+#undef PT_API_EXPORT
 
 #include "Pt/Settings.h"
 #include "Pt/Text/TextStream.h"
