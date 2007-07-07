0afc4552c1e73264e6ef3fc44f01122c8307f8d4
d-marc <d-marc@private.net>
2007-07-07 10:29:14 +0000

    ported SqliteTest to Pt::Unit

--- src/Pt-Db/sqlite/SqliteTest.cpp
+++ src/Pt-Db/sqlite/SqliteTest.cpp
@@ -1,51 +1,51 @@
-/***************************************************************************
- *   Copyright (C) 2006 PTV AG                                             *
- ***************************************************************************/
-#include "cppunit/extensions/HelperMacros.h"
-#include "cppunit/TestMain.h"
+#include "Pt/Unit/Assertion.h"
+#include "Pt/Unit/TestSuite.h"
+#include "Pt/Unit/TestMain.h"
+#include "Pt/Unit/RegisterTest.h"
 
-#include <ptv/db/Connection.h>
-#include <ptv/db/Transaction.h>
-#include <ptv/db/Result.h>
-#include <ptv/system/File.h>
+#include <Pt/Db/Connection.h>
+#include <Pt/Db/Transaction.h>
+#include <Pt/Db/Result.h>
+#include <Pt/System/File.h>
 
 #include <fstream>
 #include <cassert>
 #include <stddef.h>
 
-using namespace ptv;
-using namespace std;
 
-
-class SqliteTest : public CPPUNIT_NS::TestFixture
+class SqliteTest : public Pt::Unit::TestSuite
 {
-    CPPUNIT_TEST_SUITE( SqliteTest );
-    CPPUNIT_TEST( testConnection );
-    CPPUNIT_TEST( testCreateTable );
-    CPPUNIT_TEST( testInsert );
-    CPPUNIT_TEST( testSelect );
-    CPPUNIT_TEST( testDelete );
-    CPPUNIT_TEST( testPragma );
-    CPPUNIT_TEST_SUITE_END();
 
-public:
-    void setUp();
-    void tearDown();
+    public:
+        SqliteTest()
+        : Pt::Unit::TestSuite("SqliteTest")
+        {
+            this->registerMethod( "Connection", *this, &SqliteTest::testConnection );
+            this->registerMethod( "CreateTable", *this, &SqliteTest::testCreateTable );
+            this->registerMethod( "Insert", *this, &SqliteTest::testInsert );
+            this->registerMethod( "Select", *this, &SqliteTest:: testSelect );
+            this->registerMethod( "Delete", *this, &SqliteTest::testDelete );
+            this->registerMethod( "Pragma", *this, &SqliteTest::testPragma );
+        }
 
-protected:    
-    void testConnection();
-    void testCreateTable();
-    void testInsert();
-    void testSelect();
-    void testDelete();
-    void testPragma();
-    
-private:
-    void fillTable(ptv::db::Connection& con);
+    public:
+        void setUp();
+        void tearDown();
+
+    protected:
+        void testConnection();
+        void testCreateTable();
+        void testInsert();
+        void testSelect();
+        void testDelete();
+        void testPragma();
+
+    private:
+        void fillTable(Pt::Db::Connection& con);
 };
 
+Pt::Unit::RegisterTest<SqliteTest> register_SqliteTest;
 
-CPPUNIT_TEST_SUITE_REGISTRATION( SqliteTest );
 
 
 void SqliteTest::setUp()
@@ -57,135 +57,135 @@ void SqliteTest::tearDown()
 {
     try
     {
-        ptv::db::Connection con = ptv::db::connect("sqlite:SqliteTest.db");
+        Pt::Db::Connection con = Pt::Db::connect("sqlite:SqliteTest.db");
         con.execute("DROP TABLE TestTable");
     }
     catch(...){}
-    
+
     try
     {
-        ptv::system::File("SqliteTest.db").remove();
+        Pt::System::File("SqliteTest.db").remove();
     }
     catch(...){}
-    
+
     try
     {
-        ptv::system::File("SqliteTestPragma.db").remove();
+        Pt::System::File("SqliteTestPragma.db").remove();
     }
     catch(...){}
 }
 
 
-void SqliteTest::fillTable(ptv::db::Connection& con)
+void SqliteTest::fillTable(Pt::Db::Connection& con)
 {
     con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Hans',50,40000)");
-    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Fritz',26,20000)");    
-    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Karl',18,39500)");    
-    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Ines',63,25800)");    
-    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Albert',48,8000)");    
-    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Jennifer',35,15800)");    
-    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Angelique',32,68400)");    
+    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Fritz',26,20000)");
+    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Karl',18,39500)");
+    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Ines',63,25800)");
+    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Albert',48,8000)");
+    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Jennifer',35,15800)");
+    con.execute("INSERT INTO TestTable (name,age,salary) VALUES ('Angelique',32,68400)");
 }
 
 
 void SqliteTest::testConnection()
 {
-    ptv::db::Connection con = ptv::db::connect("sqlite:SqliteTest.db");
+    Pt::Db::Connection con = Pt::Db::connect("sqlite:SqliteTest.db");
 }
 
 
 void SqliteTest::testCreateTable()
 {
-    ptv::db::Connection con = ptv::db::connect("sqlite:SqliteTest.db");
-    ptv::db::Transaction tact(con);
-    
+    Pt::Db::Connection con = Pt::Db::connect("sqlite:SqliteTest.db");
+    Pt::Db::Transaction tact(con);
+
     con.execute("CREATE TABLE TestTable (name,age,salary);");
-    
-    ptv::db::Result result = con.select("SELECT * FROM TestTable");
-    CPPUNIT_ASSERT( result.size() == 0);
-    
+
+    Pt::Db::Result result = con.select("SELECT * FROM TestTable");
+    PT_UNIT_ASSERT( result.size() == 0);
+
     tact.commit();
 }
 
 void SqliteTest::testInsert()
 {
-    ptv::db::Connection con = ptv::db::connect("sqlite:SqliteTest.db");
-    ptv::db::Transaction tact(con);
-    
+    Pt::Db::Connection con = Pt::Db::connect("sqlite:SqliteTest.db");
+    Pt::Db::Transaction tact(con);
+
     con.execute("CREATE TABLE TestTable (name,age,salary);");
-    
+
     fillTable(con);
 
-    ptv::db::Result result = con.select("SELECT * FROM TestTable");
-    CPPUNIT_ASSERT( result.size() == 7);
-    
+    Pt::Db::Result result = con.select("SELECT * FROM TestTable");
+    PT_UNIT_ASSERT( result.size() == 7);
+
     tact.commit();
 }
 
 void SqliteTest::testSelect()
 {
-    ptv::db::Connection con = ptv::db::connect("sqlite:SqliteTest.db");
-    ptv::db::Transaction tact(con);
-    
+    Pt::Db::Connection con = Pt::Db::connect("sqlite:SqliteTest.db");
+    Pt::Db::Transaction tact(con);
+
     con.execute("CREATE TABLE TestTable (name,age,salary);");
-    
+
     fillTable(con);
-    
-    ptv::db::Result result = con.select("SELECT * FROM TestTable WHERE age > 30 AND age < 60");
-    CPPUNIT_ASSERT( result.size() == 4 );
-    
-    ptv::db::Row row = con.selectRow("SELECT * FROM TestTable WHERE name = 'Angelique'");
-    CPPUNIT_ASSERT( row.size() == 3 );
-    
-    ptv::db::Value val = con.selectValue("SELECT salary FROM TestTable WHERE age = 48");
-    CPPUNIT_ASSERT( val.getUnsigned() == 8000 );
+
+    Pt::Db::Result result = con.select("SELECT * FROM TestTable WHERE age > 30 AND age < 60");
+    PT_UNIT_ASSERT( result.size() == 4 );
+
+    Pt::Db::Row row = con.selectRow("SELECT * FROM TestTable WHERE name = 'Angelique'");
+    PT_UNIT_ASSERT( row.size() == 3 );
+
+    Pt::Db::Value val = con.selectValue("SELECT salary FROM TestTable WHERE age = 48");
+    PT_UNIT_ASSERT( val.getUnsigned() == 8000 );
 
     tact.commit();
 }
 
 void SqliteTest::testDelete()
 {
-    ptv::db::Connection con = ptv::db::connect("sqlite:SqliteTest.db");
-    ptv::db::Transaction tact(con);
-    
+    Pt::Db::Connection con = Pt::Db::connect("sqlite:SqliteTest.db");
+    Pt::Db::Transaction tact(con);
+
     con.execute("CREATE TABLE TestTable (name,age,salary);");
-    
+
     fillTable(con);
-    
+
     //delet one data table entry
     con.execute("DELETE FROM TestTable WHERE salary > 50000");    
-    ptv::db::Result result = con.select("SELECT * FROM TestTable");
-    CPPUNIT_ASSERT( result.size() == 6 );
-    
+    Pt::Db::Result result = con.select("SELECT * FROM TestTable");
+    PT_UNIT_ASSERT( result.size() == 6 );
+
     //delete whole table
     con.execute("DELETE FROM TestTable");    
     result = con.select("SELECT * FROM TestTable");
-    CPPUNIT_ASSERT( result.size() == 0 );
-    
+    PT_UNIT_ASSERT( result.size() == 0 );
+
     tact.commit();
 }
 
 void SqliteTest::testPragma()
 {
-    ptv::db::Connection con = ptv::db::connect("sqlite:SqliteTestPragma.db");
-    ptv::db::Transaction tact(con);
-    ptv::db::Value result;
-    
+    Pt::Db::Connection con = Pt::Db::connect("sqlite:SqliteTestPragma.db");
+    Pt::Db::Transaction tact(con);
+    Pt::Db::Value result;
+
     con.execute("PRAGMA auto_vacuum = 1"); // 0 | 1 - reclaim unused space or not
     result = con.selectValue("PRAGMA auto_vacuum");
-    CPPUNIT_ASSERT( result.getInt() == 1 );
-        
+    PT_UNIT_ASSERT( result.getInt() == 1 );
+
     con.execute("PRAGMA cache_size = 3500"); // No. of cached pages
     result = con.selectValue("PRAGMA cache_size");
-    CPPUNIT_ASSERT( result.getInt() == 3500 );
+    PT_UNIT_ASSERT( result.getInt() == 3500 );
 
     con.execute("PRAGMA count_changes = 1"); // 0 | 1 - if set INSERT, UPDATE, DELETE return No. of changes
     result = con.selectValue("PRAGMA count_changes");
-    CPPUNIT_ASSERT( result.getInt() == 1 );
+    PT_UNIT_ASSERT( result.getInt() == 1 );
 
     con.execute("PRAGMA page_size = 4096"); // size of one page in bytes
     result = con.selectValue("PRAGMA page_size");
-    CPPUNIT_ASSERT( result.getInt() == 4096 );
+    PT_UNIT_ASSERT( result.getInt() == 4096 );
 
     tact.commit();
 }
