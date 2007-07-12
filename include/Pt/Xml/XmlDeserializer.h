0fe0c35ad2623e31317585ed496bbd70f68417a2
d-marc <d-marc@private.net>
2007-07-12 13:01:30 +0000

    docs

--- include/Pt/Xml/XmlDeserializer.h
+++ include/Pt/Xml/XmlDeserializer.h
@@ -14,6 +14,12 @@ namespace Xml {
     class XmlReader;
     class Node;
 
+
+    /** @brief Deserialize objects or object data to XML
+
+        Thic class performs XML deserialization of a single object or
+        object data.
+    */
     class PT_XML_API XmlDeserializer
     {
         public:
@@ -21,32 +27,65 @@ namespace Xml {
 
             XmlDeserializer(std::istream& is);
 
+            //! @brief Destructor
             ~XmlDeserializer();
 
+            /** @brief Deserialize object data from XML
+
+                This method will append the object data generated
+                from an XML format to \a data.
+            */
             void getData(SerializationData& data);
 
+            /** @brief Deserialize an object to XML
+
+                This method will deserialize the object \a type from an
+                XML format. The type \a type must be serializable.
+            */
             template <typename T>
-            void deserialize(T& t)
+            void deserialize(T& type)
             {
                 SerializationData data;
                 this->getData( data );
-                data >> t;
+                data >> type;
             }
 
         protected:
+            //! @internal
             void beginDocument(const Node& node);
+
+            //! @internal
             void onRootElement(const Node& node);
+
+            //! @internal
             void onStartElement(const Node& node);
+
+            //! @internal
             void onWhitespace(const Node& node);
+
+            //! @internal
             void onContent(const Node& node);
+
+            //! @internal
             void onEndElement(const Node& node);
 
         private:
+            //! @internal
             XmlReader* _reader;
+
+            //! @internal
             std::auto_ptr<XmlReader> _deleter;
+
+            //! @internal
             typedef void (XmlDeserializer::*ProcessNode)(const Node&);
+
+            //! @internal
             ProcessNode _processNode;
+
+            //! @internal
             SerializationData* _current;
+
+            //! @internal
             String _nodeName;
     };
 
--- include/Pt/Xml/XmlSerializer.h
+++ include/Pt/Xml/XmlSerializer.h
@@ -12,30 +12,61 @@ namespace Pt {
 namespace Xml {
     class XmlWriter;
 
+    /** @brief Serialize objects or object data to XML
+
+        Thic class performs XML serialization of a single object or
+        object data.
+    */
     class PT_XML_API XmlSerializer
     {
         public:
+            /** @brief Construct a serializer writing to a byte stream
+
+                The serializer will write the objects as XML with
+                UTF-8 encoding to the output stream.
+            */
             XmlSerializer(std::ostream& os);
 
+            //! @brief Destructor
             ~XmlSerializer();
 
+            /** @brief Serialize object data to XML
+
+                The serializer will serialize the object data \a data
+                as a complete XML document to the stream.
+            */
             void putData(const SerializationData& data);
 
+            /** @brief Serialize an object to XML
+
+                The serializer will serialize the object \a type as a complete
+                XML document to the stream. The string \a name will be used
+                as the instance name of \a type and appear as the name of the
+                root element. The type must be serializable.
+            */
             template <typename T>
-            void serialize(const T& t, const Pt::String& name)
+            void serialize(const T& type, const Pt::String& name)
             {
                 SerializationData data(0, name);
-                data << t;
+                data << type;
                 this->putData( data );
             }
 
+            /** @brief Serialize object to XML
+
+                Writes all pending data to the used stream.
+            */
             void flush();
 
         protected:
+            //! @internal
             void writeData(const SerializationData& data);
 
         private:
+            //! @internal
             XmlWriter* _writer;
+
+            //! @internal
             std::auto_ptr<XmlWriter> _deleter;
     };
 
