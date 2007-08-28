604ac6cc9fdbfefd66ddb246e2f91cf6d0a11064
b_buedi <b_buedi@private.net>
2007-08-28 06:21:14 +0000

    CHANGED: separating the test method declaration from the implementation RENAMED: perf() -> SerializeDeserializeTest(), StdVector() -> StdVectorTest(), Data() -> DateTest(), Time() -> TimeTest(), DateTime() -> DateTimeTest() ADDED: BuiltInTypesTest() CHANGED: SerializeDeserializeTest() -> added test assertions

--- src/Pt/SerializationTest.cpp
+++ src/Pt/SerializationTest.cpp
@@ -159,156 +159,301 @@ void operator <<=(Pt::SerializationInfo& si, const DateRef& dr)
 
 
 
+
+
 class SerializationTest : public Pt::Unit::TestSuite
 {
     public:
         SerializationTest()
         : Pt::Unit::TestSuite("SerializationTest")
         {
-            Pt::Unit::TestSuite::registerMethod( "perf", *this, &SerializationTest::perf );
-            Pt::Unit::TestSuite::registerMethod( "StdVector", *this, &SerializationTest::StdVector );
-            Pt::Unit::TestSuite::registerMethod( "Date", *this, &SerializationTest::Date );
-            Pt::Unit::TestSuite::registerMethod( "Time", *this, &SerializationTest::Time );
-            Pt::Unit::TestSuite::registerMethod( "DateTime", *this, &SerializationTest::DateTime );
+            Pt::Unit::TestSuite::registerMethod( "BuiltInTypesTest", *this, &SerializationTest::BuiltInTypesTest );
+            Pt::Unit::TestSuite::registerMethod( "StdVectorTest", *this, &SerializationTest::StdVectorTest );
+            Pt::Unit::TestSuite::registerMethod( "DateTest", *this, &SerializationTest::DateTest );
+            Pt::Unit::TestSuite::registerMethod( "TimeTest", *this, &SerializationTest::TimeTest );
+            Pt::Unit::TestSuite::registerMethod( "DateTimeTest", *this, &SerializationTest::DateTimeTest );
+            Pt::Unit::TestSuite::registerMethod( "SerializeDeserializeTest", *this, &SerializationTest::SerializeDeserializeTest );
         }
 
     protected:
-        void perf()
-        {
-            std::cerr << std::endl;
-            Pt::Date date;
-            std::cerr << "Address of date: " << &date << std::endl;
 
-            DateRef dref;
-            dref.date = &date;
+        void BuiltInTypesTest();
+        void StdVectorTest();
+        void DateTest();
+        void TimeTest();
+        void DateTimeTest();
+        void SerializeDeserializeTest();
 
-            Serializer ser;
-            ser.serialize(date, "myDate");
-            ser.serialize(dref, "MyDateRef");
-            ser.fixup();
-
-            Pt::SerializationInfo si1;
-            si1.setName("myDate");
-            si1 <<= date;
-            si1.setId("11223344");
-
-            Pt::Date* pdate = &date;
-            Pt::SerializationInfo si2;
-            si2.setName("myDateRef");
-
-            Pt::SerializationInfo& pd = si2.addMember("date");
-            pd.setCategory( Pt::SerializationInfo::Reference );
-            pd.setId("11223344");
-
-            Deserializer dser;
-            dser.deserialize(si1, date);
-            dser.deserialize(si2, dref);
-            dser.fixup(si2);
-
-            std::cerr << "dref.date is: " << dref.date << std::endl;
-            /*
-            Pt::System::Clock c;
-            c.start();
-
-            Pt::System::TimeValue tv = c.stop();
-            std::cerr << "Duration: " << tv.seconds() << ":" << tv.microSeconds()/1000 << std::endl;
-            */
-        }
-
-        void StdVector()
-        {
-            std::vector<int> iv, iv2;
-            iv.push_back(1);
-            iv.push_back(2);
-            iv.push_back(3);
-
-            Pt::SerializationInfo si;
-            si <<= iv;
-
-            /*for(Pt::SerializationInfo::ConstIterator it = si.begin(); it != si.end(); ++it)
-            {
-                std::cerr << "elem: " << it->getValue<std::string>() << std::endl;
-            }*/
-            si >>= iv2;
-/*
-            Pt::ObjectManager manager;
-
-            Pt::SerializationInfo info;
-            Pt::SerializationInfo& object = info.addValue("object", 5);
-            info.addValue("pointerToObject", "object");
-
-            // read from source and fill object map
-            manager._objects["object"] =  &object;
-
-            int n = info.getValue<int>("object");
-            manager._objects["object"] =  &n;
-
-            std::string id = info.getValue<std::string>("pointerToObject");
-            int* px = 0;
-            manager._pointers[(void**)&px] = id;
-
-            // read done, now fixup
-            std::map<void**, std::string>::iterator it;
-            for(it = manager._pointers.begin(); it != manager._pointers.end(); ++it)
-            {
-                std::cerr << "\nfixing up to " << it->second << std::endl;
-                void* object = manager._objects[ it->second ];
-                void** p = it->first;
-                int** i = (int**)p;
-                std::cerr << "NEW VALUE: " << *( (int*)(object) ) << std::endl;
-                *i = &(*( (int*)(object) ));
-            }
-
-            std::cerr << "FIXED: " << px << " " << *px << std::endl;*/
-        }
-
-        void Date()
-        {
-            Pt::Date date(2000, 10, 20);
-            Pt::SerializationInfo si;
-            si <<=  date;
-
-            /*for(Pt::SerializationInfo::ConstIterator it = si.begin(); it != si.end(); ++it)
-            {
-                std::cerr << "iter: " << it->name() <<  " " << it->getValue<std::string>() << std::endl;
-            }*/
-
-            Pt::Date date2(1,1,1);
-            si >>= date2;
-
-            PT_UNIT_ASSERT(date == date2);
-        }
-
-        void Time()
-        {
-            Pt::Time time(18, 40, 5, 1);
-            Pt::SerializationInfo si;
-            si <<= time;
-
-            Pt::Time time2;
-            si >>= time2;
-
-            PT_UNIT_ASSERT(time == time2);
-        }
-
-        void DateTime()
-        {
-            Pt::DateTime datetime(2000, 10, 20, 18, 40, 5, 1);
-            Pt::SerializationInfo si;
-            si <<= datetime;
-
-            Pt::DateTime datetime2;
-            si >>= datetime2;
-
-            const Pt::Date date = datetime.date();
-            const Pt::Date date2 = datetime2.date();
-            PT_UNIT_ASSERT(date == date2);
-
-            const Pt::Time time = datetime.time();
-            const Pt::Time time2 = datetime2.time();
-
-            PT_UNIT_ASSERT(time == time2);
-        }
 };
 
 Pt::Unit::RegisterTest<SerializationTest> register_SerializationTest;
+
+
+void SerializationTest::SerializeDeserializeTest()
+{
+    Pt::Date date;
+    date.set(2020, 11, 21);
+    
+    DateRef dref;
+    dref.date = &date;
+
+    Serializer ser;
+    ser.serialize(date, "myDate");
+    ser.serialize(dref, "MyDateRef");
+    ser.fixup();
+
+    Pt::SerializationInfo si1;
+    si1.setName("myDate");
+    si1 <<= date;
+    si1.setId("11223344");
+
+    Pt::SerializationInfo si2;
+    si2.setName("myDateRef");
+
+    Pt::SerializationInfo& pd = si2.addMember("date");
+    pd.setCategory( Pt::SerializationInfo::Reference );
+    pd.setId("11223344");
+
+    Pt::Date date2;
+    DateRef dref2;
+    Deserializer dser;
+    dser.deserialize(si1, date2);
+    dser.deserialize(si2, dref2);
+    dser.fixup(si2);
+
+    PT_UNIT_ASSERT(date2 == date);
+    PT_UNIT_ASSERT(dref2.date == &date2);
+}
+
+
+void SerializationTest::BuiltInTypesTest()
+{
+
+    Pt::SerializationInfo si;
+
+    bool boolVal1 = true;
+    bool boolVal2;
+    si <<= boolVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "bool");
+    si.toValue(boolVal2);
+    PT_UNIT_ASSERT(boolVal2 == true);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"true") );
+    si >>= boolVal2;
+    PT_UNIT_ASSERT(boolVal2 == true);
+
+
+    char charVal1 = 'c';
+    char charVal2;
+    si <<= charVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "char");
+    si.toValue(charVal2);
+    PT_UNIT_ASSERT(charVal2 == 'c');
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"c") );
+    si >>= charVal2;
+    PT_UNIT_ASSERT(charVal2 == 'c');
+
+
+    signed char sigCharVal1 = -127;
+    signed char sigCharVal2;
+    si <<= sigCharVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "signed char");
+    si.toValue(sigCharVal2);
+    PT_UNIT_ASSERT(sigCharVal2 == -127);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"-127") );
+    si >>= sigCharVal2;
+    PT_UNIT_ASSERT(sigCharVal2 == -127);
+
+
+    unsigned char usigCharVal1 = 255;
+    unsigned char usigCharVal2;
+    si <<= usigCharVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "unsigned char");
+    si.toValue(usigCharVal2);
+    PT_UNIT_ASSERT(usigCharVal2 == 255);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"255") );
+    si >>= usigCharVal2;
+    PT_UNIT_ASSERT(usigCharVal2 == 255);
+
+
+    short shortVal1 = -32767;
+    short shortVal2;
+    si <<= shortVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "short");
+    si.toValue(shortVal2);
+    PT_UNIT_ASSERT(shortVal2 == -32767);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"-32767") );
+    si >>= shortVal2;
+    PT_UNIT_ASSERT(shortVal2 == -32767);
+
+
+    unsigned short ushortVal1 = 65535;
+    unsigned short ushortVal2;
+    si <<= ushortVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "unsigned short");
+    si.toValue(ushortVal2);
+    PT_UNIT_ASSERT(ushortVal2 == 65535);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"65535") );
+    si >>= ushortVal2;
+    PT_UNIT_ASSERT(ushortVal2 == 65535);
+
+
+    int intVal1 = -32767;
+    int intVal2;
+    si <<= intVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "int");
+    si.toValue(intVal2);
+    PT_UNIT_ASSERT(intVal2 == -32767);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"-32767") );
+    si >>= intVal2;
+    PT_UNIT_ASSERT(intVal2 == -32767);
+
+
+    unsigned int uintVal1 = 65535;
+    unsigned int uintVal2;
+    si <<= uintVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "unsigned int");
+    si.toValue(uintVal2);
+    PT_UNIT_ASSERT(uintVal2 == 65535);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"65535") );
+    si >>= uintVal2;
+    PT_UNIT_ASSERT(uintVal2 == 65535);
+
+
+    long longVal1 = -32767;
+    long longVal2;
+    si <<= longVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "long");
+    si.toValue(longVal2);
+    PT_UNIT_ASSERT(longVal2 == -32767);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"-32767") );
+    si >>= longVal2;
+    PT_UNIT_ASSERT(longVal2 == -32767);
+
+
+    unsigned long ulongVal1 = 65535;
+    unsigned long ulongVal2;
+    si <<= ulongVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "unsigned long");
+    si.toValue(ulongVal2);
+    PT_UNIT_ASSERT(ulongVal2 == 65535);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"65535") );
+    si >>= ulongVal2;
+    PT_UNIT_ASSERT(ulongVal2 == 65535);
+
+
+    float floatVal1 = 77.3547f;
+    float floatVal2;
+    si <<= floatVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "float");
+    si.toValue(floatVal2);
+    PT_UNIT_ASSERT(floatVal2 == 77.3547f);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"77.3547") );
+    si >>= floatVal2;
+    PT_UNIT_ASSERT(floatVal2 == 77.3547f);
+
+
+    double doubleVal1 = 198.819649;
+    double doubleVal2;
+    si <<= doubleVal1;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Value);
+    PT_UNIT_ASSERT(si.typeName() == "double");
+    si.toValue(doubleVal2);
+    PT_UNIT_ASSERT(doubleVal2 == 198.819649);
+    PT_UNIT_ASSERT(si.toString() == Pt::String(L"198.819649000000000") );
+    si >>= doubleVal2;
+    PT_UNIT_ASSERT(doubleVal2 == 198.819649);
+
+}
+
+
+void SerializationTest::StdVectorTest()
+{
+    std::vector<int> iv, iv2;
+    const int vectorSize = 123;
+
+    for(int i=0; i<vectorSize; ++i)
+    {
+        iv.push_back(i);
+    }
+
+    Pt::SerializationInfo si;
+    si <<= iv;
+    PT_UNIT_ASSERT(si.category() == Pt::SerializationInfo::Object);
+
+    Pt::SerializationInfo::Iterator it = si.begin();
+    int counter = 0;
+    int intValue;
+
+    while(it!=si.end())
+    {
+        it->toValue(intValue);
+
+        PT_UNIT_ASSERT(it->category() == Pt::SerializationInfo::Value);
+        PT_UNIT_ASSERT(intValue == counter);
+
+        ++it;
+        ++counter;
+    }
+    PT_UNIT_ASSERT(counter == vectorSize);
+
+    si >>= iv2;
+
+    for(int j=0; j<vectorSize; ++j)
+    {
+        PT_UNIT_ASSERT(iv2[j] == j);
+    }
+}
+
+void SerializationTest::DateTest()
+{
+    Pt::Date date(2000, 10, 20);
+    Pt::SerializationInfo si;
+    si <<=  date;
+
+    Pt::Date date2(1,1,1);
+    si >>= date2;
+
+    PT_UNIT_ASSERT(date == date2);
+}
+
+void SerializationTest::TimeTest()
+{
+    Pt::Time time(18, 40, 5, 1);
+    Pt::SerializationInfo si;
+    si <<= time;
+
+    Pt::Time time2;
+    si >>= time2;
+
+    PT_UNIT_ASSERT(time == time2);
+}
+
+void SerializationTest::DateTimeTest()
+{
+    Pt::DateTime datetime(2000, 10, 20, 18, 40, 5, 1);
+    Pt::SerializationInfo si;
+    si <<= datetime;
+
+    Pt::DateTime datetime2;
+    si >>= datetime2;
+
+    const Pt::Date date = datetime.date();
+    const Pt::Date date2 = datetime2.date();
+    PT_UNIT_ASSERT(date == date2);
+
+    const Pt::Time time = datetime.time();
+    const Pt::Time time2 = datetime2.time();
+
+    PT_UNIT_ASSERT(time == time2);
+}
