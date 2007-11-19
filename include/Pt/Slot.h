4482fd7b2651857e24ce0d9353d14d50bdce7ab3
sgbeal <sgbeal@private.net>
2007-11-19 23:23:26 +0000

    ported to use foo.tpp

--- include/Pt/Slot.h
+++ include/Pt/Slot.h
@@ -25,19 +25,7 @@ namespace Pt {
     };
 
 
-    template < typename R,
-               typename A1 = Pt::Void,
-               typename A2 = Pt::Void,
-               typename A3 = Pt::Void,
-               typename A4 = Pt::Void,
-               typename A5 = Pt::Void,
-               typename A6 = Pt::Void,
-               typename A7 = Pt::Void,
-               typename A8 = Pt::Void >
-    class BasicSlot : public Slot {
-        public:
-            virtual Slot* clone() const = 0;
-    };
+#include <Pt/gen-slot.h>
 
 }
 
