f884e39c1717883206afdde415930cdd5ff4ea44
d-marc <d-marc@private.net>
2007-12-09 13:12:44 +0000

    moved numpunct facet

--- include/Pt/Locale.h
+++ include/Pt/Locale.h
@@ -189,6 +189,50 @@ namespace std {
 
 namespace std {
 
+    /** @brief Numpunct localization facet
+        @ingroup Unicode
+    */
+    template <>
+    class PT_API numpunct<Pt::Char> : public locale::facet {
+        public:
+            typedef Pt::Char char_type;
+            typedef std::basic_string<Pt::Char> string_type;
+
+            // gcc 3.4.x violates the c++ standard by requiring a __numpunct_cache
+            #if __GNUC__ == 3 && __GNUC_MINOR__ == 4
+            typedef __numpunct_cache<Pt::Char>  __cache_type;
+            #endif
+
+            static locale::id id;
+
+        public:
+
+            explicit numpunct(size_t refs = 0);
+
+            virtual ~numpunct();
+
+            char_type decimal_point() const;
+
+            char_type thousands_sep() const;
+
+            string grouping() const;
+
+            string_type truename() const;
+
+            string_type falsename() const;
+
+        protected:
+            virtual char_type do_decimal_point() const;
+
+            virtual char_type do_thousands_sep() const;
+
+            virtual string do_grouping() const;
+
+            virtual string_type do_truename() const;
+
+            virtual string_type do_falsename() const;
+    };
+
     class ios_base;
 
 #if (defined _MSC_VER || defined __QNX__ || defined __xlC__)
@@ -293,50 +337,6 @@ namespace std {
 
             virtual const Pt::Char* do_narrow(const Pt::Char* lo, const Pt::Char* hi,
                                                char dfault, char* dest) const;
-    };
-
-    /** @brief Numpunct localization facet
-        @ingroup Unicode
-    */
-    template <>
-    class PT_API numpunct<Pt::Char> : public locale::facet {
-        public:
-            typedef Pt::Char char_type;
-            typedef std::basic_string<Pt::Char> string_type;
-
-            // gcc 3.4.x violates the c++ standard by requiring a __numpunct_cache
-            #if __GNUC__ == 3 && __GNUC_MINOR__ == 4
-            typedef __numpunct_cache<Pt::Char>  __cache_type;
-            #endif
-
-            static locale::id id;
-
-        public:
-
-            explicit numpunct(size_t refs = 0);
-
-            virtual ~numpunct();
-
-            char_type decimal_point() const;
-
-            char_type thousands_sep() const;
-
-            string grouping() const;
-
-            string_type truename() const;
-
-            string_type falsename() const;
-
-        protected:
-            virtual char_type do_decimal_point() const;
-
-            virtual char_type do_thousands_sep() const;
-
-            virtual string do_grouping() const;
-
-            virtual string_type do_truename() const;
-
-            virtual string_type do_falsename() const;
     };
 
 
