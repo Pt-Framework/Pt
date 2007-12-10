71ea7152bcf8fda0aaa0ace2447bc2758fbc7421
d-marc <d-marc@private.net>
2007-12-10 19:48:11 +0000

    added serialization base classes

---
+++ src/Pt/Deserializer.cpp
@@ -0,0 +1,108 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Marc Boris Duerner                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ **************************************************************************/
+#include "Pt/Deserializer.h"
+
+namespace Pt {
+
+Deserializer::Deserializer()
+: _peeking(false)
+{ }
+
+
+Deserializer::~Deserializer()
+{
+    this->fixup();
+}
+
+
+SerializationInfo& Deserializer::peek()
+{
+    if( ! _peeking )
+    {
+        _stack.push_back( SerializationInfo() );
+        Pt::SerializationInfo& si =_stack.back();
+        this->read( si );
+        _peeking = true;
+    }
+
+    _peeking = true;
+    return _stack.back();
+}
+
+
+void Deserializer::fixup()
+{
+    std::list<Pt::SerializationInfo>::iterator it;
+    for(it = _stack.begin(); it != _stack.end(); ++it)
+    {
+        this->fixup(*it);
+    }
+
+    _objects.clear();
+    _stack.clear();
+}
+
+
+Pt::SerializationInfo& Deserializer::next()
+{
+    if( ! _peeking )
+    {
+        _stack.push_back( SerializationInfo() );
+        Pt::SerializationInfo& si =_stack.back();
+        this->read( si );
+    }
+
+    _peeking = false;
+    return _stack.back();
+}
+
+
+void Deserializer::markFixup(Pt::SerializationInfo& si, void* type, Fixup fixup)
+{
+    if( ! si.id().empty() )
+    {
+        _objects[ si.id() ] = type;
+        _fixups[ si.id() ] = fixup;
+    }
+}
+
+
+void Deserializer::fixup(const Pt::SerializationInfo& si)
+{
+    Pt::SerializationInfo::ConstIterator it;
+    for(it = si.begin(); it != si.end(); ++it)
+    {
+        if(it->category() == Pt::SerializationInfo::Reference)
+        {
+            void* target = _objects[ it->toValue<std::string>() ];
+
+            void* d = it->fixupAddr();
+            void** destination = (void**)d;
+
+            _fixups[ it->toValue<std::string>() ]( destination, target);
+        }
+
+        if(it->category() == Pt::SerializationInfo::Object)
+        {
+            this->fixup(*it);
+        }
+    }
+}
+
+} // namespace Pt
---
+++ src/Pt/Serializer.cpp
@@ -0,0 +1,80 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Marc Boris Duerner                              *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ **************************************************************************/
+#include "Pt/Serializer.h"
+
+namespace Pt {
+
+Serializer::Serializer()
+{ }
+
+
+Serializer::~Serializer()
+{
+    this->fixdown();
+}
+
+
+SerializationInfo& Serializer::append(const void* obj)
+{
+    _stack.resize( _stack.size() + 1 );
+    SerializationInfo& si = _stack.back();
+
+    _objects[obj] = &si;
+    return si;
+}
+
+
+void Serializer::fixdown()
+{
+    std::list<Pt::SerializationInfo>::iterator it;
+    for(it = _stack.begin(); it != _stack.end(); ++it)
+    {
+        this->fixdown(*it);
+    }
+
+    for(it = _stack.begin(); it != _stack.end(); ++it)
+    {
+        this->write( *it );
+    }
+
+    _objects.clear();
+    _stack.clear();
+}
+
+
+void Serializer::fixdown(Pt::SerializationInfo& si)
+{
+    if(si.category() == Pt::SerializationInfo::Reference)
+    {
+        const void* p = si.toValue<void*>();
+        Pt::SerializationInfo* pointee = _objects[p];
+        pointee->setId( convert<std::string>(pointee) );
+        si.setReference( pointee );
+    }
+    else if(si.category() == Pt::SerializationInfo::Object)
+    {
+        Pt::SerializationInfo::Iterator it;
+        for(it = si.begin(); it != si.end(); ++it)
+        {
+            this->fixdown(*it);
+        }
+    }
+}
+
+} // namespace Pt
