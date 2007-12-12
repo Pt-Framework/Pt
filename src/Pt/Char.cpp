48baa2f58be46b53395df535eaf24775ef5e4de0
d-marc <d-marc@private.net>
2007-12-12 09:03:29 +0000

    inclusion order under WinCE

--- src/Pt/Char.cpp
+++ src/Pt/Char.cpp
@@ -18,7 +18,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include "Pt/Char.h"
-#include "Pt/String.h"
+
+//#include "Pt/String.h" // for CharDummyStream
 
 namespace Pt {
 /*
