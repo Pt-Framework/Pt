8212d366422189662daf444617abfea4fce15b03
d-marc <d-marc@private.net>
2007-12-13 21:25:16 +0000

    copyright

--- include/Pt/Blob.h
+++ include/Pt/Blob.h
@@ -1,5 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2007 by Tommi Mäkitalo                                  *
+ *   Copyright (C) 2007 by Tommi Maekitalo                                 *
+ *   Copyright (C) 2007 by Marc Duerner                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
