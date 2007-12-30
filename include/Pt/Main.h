40ff26f5f5a4d4ae4c163679ec94ace82827c816
d-marc <d-marc@private.net>
2007-12-30 00:03:22 +0000

    whitespace

--- include/Pt/Main.h
+++ include/Pt/Main.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2005-2006 by Dr. Marc Boris Dürner                      *
+ *   Copyright (C) 2005-2006 by Dr. Marc Boris Dï¿½rner                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
@@ -16,10 +16,8 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
 #ifndef PT_MAIN_H
 #define PT_MAIN_H
-
 
 #ifdef _WIN32_WCE
 
