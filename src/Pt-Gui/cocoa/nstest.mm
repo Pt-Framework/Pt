1f44bb57c27cd55adacd8af3eaac6f2ebc04f6e8
d-marc <d-marc@private.net>
2008-01-04 05:26:08 +0000

    simple hello world app

--- src/Pt-Gui/cocoa/nstest.mm
+++ src/Pt-Gui/cocoa/nstest.mm
@@ -1,14 +1,27 @@
 #import <AppKit/NSApplication.h>
+#import <AppKit/NSWindow.h>
 #import <iostream>
 
-class Test
-{
-};
-
-int main( int argc, char* argv[])
+int main( int argc, const char* argv[])
 {
     std::cerr << "Started" << std::endl;
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     NSApplication *app = [NSApplication sharedApplication];
+
+    NSWindow* window; 
+    window = [[NSWindow alloc] initWithContentRect:NSMakeRect(100.0, 350.0, 200.0, 40.0)      
+                                                   styleMask:NSTitledWindowMask | 
+                                                             NSClosableWindowMask | 
+                                                             NSMiniaturizableWindowMask
+                                                   backing:NSBackingStoreBuffered
+                                                   defer:YES];
+
+    [window setTitle:@"Hello, World!"];
+    [window makeKeyAndOrderFront:nil];
     [app run];
+    
+    [window release];
+    [app release];
+    [pool release];
     return 0;	
 }
