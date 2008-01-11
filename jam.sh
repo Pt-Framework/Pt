dad3be9679005f8cfa5d1c26bdb0a5ea2356978f
d-marc <d-marc@private.net>
2008-01-11 17:30:15 +0000

    use unix line end, cd into subdir inf jam.sh

--- jam.sh
+++ jam.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-JAM_SOURCES="./jam/src/command.c ./jam/src/compile.c ./jam/src/debug.c ./jam/src/execunix.c ./jam/src/expand.c ./jam/src/fileunix.c ./jam/src/glob.c ./jam/src/hash.c ./jam/src/hdrmacro.c ./jam/src/headers.c ./jam/src/jam.c ./jam/src/jambase.c ./jam/src/jamgram.c ./jam/src/lists.c ./jam/src/make.c ./jam/src/make1.c ./jam/src/newstr.c ./jam/src/option.c ./jam/src/parse.c ./jam/src/pathunix.c ./jam/src/pathvms.c ./jam/src/regexp.c ./jam/src/rules.c ./jam/src/scan.c ./jam/src/search.c ./jam/src/subst.c ./jam/src/timestamp.c ./jam/src/variable.c ./jam/src/modules.c ./jam/src/strings.c ./jam/src/filesys.c ./jam/src/builtins.c ./jam/src/pwd.c ./jam/src/class.c ./jam/src/w32_getreg.c ./jam/src/native.c ./jam/src/modules/set.c ./jam/src/modules/path.c ./jam/src/modules/regex.c ./jam/src/modules/property-set.c ./jam/src/modules/sequence.c ./jam/src/modules/order.c"
+JAM_SOURCES="./command.c ./compile.c ./debug.c ./execunix.c ./expand.c ./fileunix.c ./glob.c ./hash.c ./hdrmacro.c ./headers.c ./jam.c ./jambase.c ./jamgram.c ./lists.c ./make.c ./make1.c ./newstr.c ./option.c ./parse.c ./pathunix.c ./pathvms.c ./regexp.c ./rules.c ./scan.c ./search.c ./subst.c ./timestamp.c ./variable.c ./modules.c ./strings.c ./filesys.c ./builtins.c ./pwd.c ./class.c ./w32_getreg.c ./native.c ./modules/set.c ./modules/path.c ./modules/regex.c ./modules/property-set.c ./modules/sequence.c ./modules/order.c"
 
-if ! test -f ./jam.bin; then
+if test -f ./jam.bin; then
+    ./jam.bin $*
+else
     echo "Building jam executable..."
-    cc -o ./jam.bin $JAM_SOURCES
+    cd jam/src
+    cc -o ../../jam.bin $JAM_SOURCES
+    cd ../..
+    ./jam.bin $*
 fi
 
-./jam.bin $*
