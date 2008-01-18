4d377a4b105c6e1b9c2a595d2ed2fe6d608380a4
schnirom <schnirom@private.net>
2008-01-18 08:46:25 +0000

    CHANGED: disabled debug output

--- src/Pt-Xml/XmlSerializerTest.cpp
+++ src/Pt-Xml/XmlSerializerTest.cpp
@@ -77,9 +77,9 @@ class XmlSerializerTest: public Pt::Unit::TestSuite
             ser.finish();
             ser.flush();
 
-            std::cerr << "\n##########\n" << std::endl;
-            std::cerr << output.str() << std::endl;
-            std::cerr << "##########" << std::endl;
+            //std::cerr << "\n##########\n" << std::endl;
+            //std::cerr << output.str() << std::endl;
+            //std::cerr << "##########" << std::endl;
 
             Pt::Date date2(1, 1, 1);
             dr.date = 0;
