42ca3dcac18415f38dc6217cfac1af9e2439308c
d-marc <d-marc@private.net>
2008-01-27 15:06:06 +0000

    fixed docs

--- include/Pt/Pt.h
+++ include/Pt/Pt.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004-2006 by Marc Boris Dürner                          *
+ *   Copyright (C) 2004-2006 by Marc Boris Duerner                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
@@ -43,9 +43,9 @@
 
     This module is the base module for all other modules and has no dependency
     to any system specific libraries except the standard c++ library. It
-    provides some basic types, support for byte-order handling, type-traits,
-    an unicode string and character class, serialization and a signals/delegates
-    based callback mechanism.
+    provides some basic types, support for byte-order handling, atomic integer
+    operations, type-traits, an unicode string and character class, serialization
+    and a signals/delegates based callback mechanism.
 */
 namespace Pt {
 
@@ -159,114 +159,5 @@ namespace Pt {
     class SourceInfo;
     class NonCopyable;
 }
-
-/** \page "Basic Types"
-!!!Basic Types
-The core module defines the following plain data types of fixed size:
-|int8_t|uint8_t
-|int16_t|uint16_t
-|int32_t|uint32_t
-|int64_t|uint64_t
-
-For convenience special typedefs for unsigned plain datatypes are
-available:
-||typedef || type
-|uchar|unsigned char
-|ushort|unsigned short
-|uint|unsigned int
-|ulong|unsigned long
-*/
-
-/** \page "Byteorder  algorithms"
-!!! Byteorder algorithms
-The byteoder API consists of two layers. One layer is responsible for
-swapping the byteorder of a type and the high-level API can be used to
-convert from an exernal byteorder to the host byteorder.
-
-!!Swapping the byteorder
-
-The low-level API consists of a generic swab() function, which is able to
-swap the byteorder of a type by bytewise copying. This works for all
-bitwise copyable types. Though, for many types, byteorder sapping can be
-implemented with better performance. Therefore overloads of swab() are
-offered for plain data types such as uint16_t that use shift operations.
-
-The swab() function can be overloaded for custom types as well. For example
-the following struct can have an overloaded swab():
-
-@code
-struct value
-{
-    value(char a, uint16_t b, char c)
-    : _a(a), _b(b), _c(c)
-    char _a;
-    uint16_t _b;
-    char _c;
-};
-
-value swab(const value& val)
-{
-    return value(val._a, swab(val.b), val._c);
-}
-@endcode
-
-!!Converting from external byteorder to the host byteorder
-The low-level swab() algorithm is used by the high level functions for
-external to host byteorder conversions, for example beToHost(). On a little
-endian system this function needs to swap the byteorder, on a big endian
-system this funtion should return the value unchanged. The macros PTV_LE
-and PRV_BE define whether the code should be compiled for a little endian
-or big endian machine and adjust the behaviour of the high-level conversion
-functions. If a swab() algorithm is overloaded for a custom type it can
-be directly used with the high level functions:
-
-@code
-// some value in big endian byteorder
-value beVal = getBeValue();
-
-// convert to host byteorder
-value hostVal = beToHost(beVal);
-@endcode
-*/
-
-/** \page "Error and Exception Handling"
-!!! Error and Exception Handling
-All exceptions thrown by PPR are derived from std::exception, to fit well
-in the context of the C++ standard library. Similarily, the two main
-exception types are RuntimeError and LogicError.
-A RuntimeError can for example occur if an underlying file descriptor
-suddenly becomes invalid. These errors are usually out of the scope of
-the application and not easily avoidable. Sometimes the best the application
-can do is to shut down properly before data becomes corrupted.
-LogicErrors on the other hand are in the domain of the application. Often
-they are preventable and an example of a typical LogicError is if an invalid
-argument such as a malformed URL is passes to a function. IT is most likely
-that the program will just want to retry the operation under different
-conditions.
-The SourceInfo class plays a special role in PPR's error handling, since
-all exception classes are constructed with a SourceInfo object indicating
-the location where the exception was thrown. The code below will print
-the function name, the source file and the line where the esxception was thrown:
-
-@code
-try {
-    // monitored code
-}
-catch(ptv::Exception& e) {
-    cerr << "Exception in " << e.sourceInfo().func() << endl;
-         << e.sourceInfo().file() << ": " << e.sourceInfo().line() << endl;
-         << e.what() << endl;
-}
-@endcode
-
-Most modules add new exception classes to this hierachy.
-*/
-
-/** \page "Object Modeling"
-!! Object Modeling
-PPR supports various ways to help model your objects. This begins with simple
-mixin classes like the NonCopyable to disallow copies of a derived class or the
-Singleton to implement your class as a singleton.
-*/
 
 #endif
