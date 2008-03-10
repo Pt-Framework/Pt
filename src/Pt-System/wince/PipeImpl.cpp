ffe3be070bafc11336840b4c32ed3750a325b970
schnirom <schnirom@private.net>
2008-03-10 09:32:14 +0000

    CHANGED: reverting last commit: must not initialize the MSGQUEUEINFO strucutre to 0 (where as for the MSGQUEUEOPTIONS structure you have to...)

--- src/Pt-System/wince/PipeImpl.cpp
+++ src/Pt-System/wince/PipeImpl.cpp
@@ -59,7 +59,6 @@ void PipeIODevice::open(HANDLE handle, bool isAsync)
     _handle = handle; 
     
     MSGQUEUEINFO info;
-    memset(&info, 0, sizeof(info));
     info.dwSize = sizeof(MSGQUEUEINFO);
         
     if ( TRUE == GetMsgQueueInfo(_handle, &info) )
