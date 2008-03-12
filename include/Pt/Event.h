ff665051ab324954a2d075d1ac270ed6698e83a4
d-marc <d-marc@private.net>
2008-03-12 09:26:31 +0000

    docs updated

--- include/Pt/Event.h
+++ include/Pt/Event.h
@@ -1,8 +1,3 @@
-/***************************************************************************
- *   Copyright (C) 2006 Marc Boris D�rner                                  *
- *                                                                         *
- ***************************************************************************/
-
 #ifndef PT_EVENT_H
 #define PT_EVENT_H
 
@@ -10,50 +5,35 @@
 #include <Pt/Types.h>
 #include <typeinfo>
 
-
 namespace Pt {
 
-    /**
-     * \brief The root event class for all events.
-     *
-     * The implemenation of this class is empty. Specific Event objects, like for
-     * example MouseEvents, must subclass from Event and implement all necessary
-     * behaviour.
-     *
-     * Events are used by the EventLoop class and can be added using the method
-     * commitEvent(). These Event object will be delivered to the functions
-     * and methods, that where previously registered to receive events.
-     *
-     * The method clone() is abstract and needs to be defined by subclasses to
-     * allow cloning of the specific Event object.
+    /** \brief Base class for all event types.
+
+        Specific Event objects, subclass from Event and implement the clone()
+        and typeInfo() methods. The first is used to deep copy event objects
+        for example in an EventLoop and the latter one is used to dispatch
+        events by type.
      */
     class PT_EXPORT Event {
         public:
-            /**
-             * \brief Constructor to initialize the Event class.
-             * For the super-class Event the constructor is empty.
+            /** \brief Default Constructor.
              */
             Event()
             {}
 
-            /**
-             * \brief Destructor for Event.
-             * For the super-class Event the destructor is empty.
+            /** \brief Destructor.
              */
             virtual ~Event()
             {}
 
-            //! Clone method.
+            //! \brief Clone method.
             virtual Event* clone() const = 0;
 
-            //! Clone method.
+            //! \brief Clone method.
             virtual size_t clone(void* to, size_t available)
             { return 0; }
 
-           /**
-            * @brief Returns the type info for this class of events.
-            * 
-            * @return The type info for this class of events.
+            /** \brief Returns the type info for this class of events.
             */
             virtual const std::type_info& typeInfo() const = 0;
     };
