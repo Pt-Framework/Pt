bdce5dbcf353e8e7981f6edc91c5366f99158104
d-marc <d-marc@private.net>
2008-03-14 09:37:16 +0000

    removed using namespace

--- src/Pt-System/IOError.cpp
+++ src/Pt-System/IOError.cpp
@@ -19,12 +19,8 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
 #include "Pt/System/IOError.h"
-
 #include <string>
-using namespace std;
-
 
 namespace Pt {
 
