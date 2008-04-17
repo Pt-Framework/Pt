a593ccc3f7e1408b4eebb0cf0defd703dd5e3faf
d-marc <d-marc@private.net>
2008-04-17 12:07:27 +0000

    fixed timer bug remaining time if inactive

--- src/Pt-System/Timer.cpp
+++ src/Pt-System/Timer.cpp
@@ -2,11 +2,10 @@
 #include "Pt/System/Clock.h"
 #include <limits>
 
-
 namespace {
 
 Pt::size_t getCurrentMSecs()
-{    
+{
     return  Pt::System::Clock::getTime();
 }
 
@@ -72,15 +71,15 @@ void Timer::stop()
 
 
 bool Timer::update()
-{    
+{
     if(_active == false)
         return false;
 
     size_t current = getCurrentMSecs();
     _elapsed = current - _started;
-    
+
         if(_elapsed >= _interval)
-    {        
+    {
         _elapsed -= _interval;
         _started = current - _elapsed;
         timeout();
@@ -93,77 +92,8 @@ bool Timer::update()
 
 size_t Timer::remaining() const
 {
-    return _interval - _elapsed;
+    return _active ? (_interval - _elapsed) : InvalidTime ;
 }
-
-
-
-/*
-size_t Timer::_resolution = 10000;
-
-Timer::Timer()
-: _timerThread( *this )
-, _eventCounter( 0 )
-, _run( false )
-{
-}
-
-Timer::~Timer()
-{ }
-
-size_t Timer::resolution() 
-{ 
-    return _resolution / 1000; 
-}
-
-void Timer::start()
-{  
-    _timerThread.start(); 
-}
-
-void Timer::stop()
-{
-    _run = false;
-    _timerThread.wait();
-    _eventCounter = 0;
-}
-
-void Timer::run()
-{
-    _run = true;
-
-    size_t            events      = 0;
-    size_t            deltaRest = 0;
-    Clock            stopWatch;
-    TimeValue        deltaTime;
-
-    size_t sleepTime = _resolution / 1000;
-    stopWatch.start();
-
-    Thread::sleep( sleepTime );
-
-    while( _run )
-    {
-        deltaTime = stopWatch.stop();
-
-        stopWatch.start();
-
-        deltaRest  +=  ( deltaTime.seconds() / 1000000.0 + ( deltaTime.microSeconds() ) ); 
-
-        events = ( deltaRest / _resolution  );
-
-        if( events == 0 )
-            events = 1;
-
-        deltaRest  = ( deltaRest %  _resolution ); 
-
-        for( size_t i = 0; i < events; ++i )
-            onTime.send( ++_eventCounter );
-
-        Thread::sleep( sleepTime );
-    }
-}
-*/
 
 }
 
