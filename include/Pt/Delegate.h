c8bdf3d7bc9cce594fbe8215b857afd85b5002c0
d-marc <d-marc@private.net>
2008-05-07 09:54:06 +0000

    whitespace

--- include/Pt/Delegate.h
+++ include/Pt/Delegate.h
@@ -17,7 +17,6 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
-
 #ifndef Pt_Delegate_h
 #define Pt_Delegate_h
 
@@ -28,7 +27,6 @@
 #include <Pt/Connectable.h>
 #include "Pt/SourceInfo.h"
 #include <stdexcept>
-
 
 namespace Pt {
 
