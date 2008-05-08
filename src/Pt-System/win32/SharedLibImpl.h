be3e7775d0fb8ea9f1f6d8b32b9ca733bd5edee7
d-marc <d-marc@private.net>
2008-05-08 19:04:34 +0000

    Shared library does not use Directory and File classes

--- src/Pt-System/win32/SharedLibImpl.h
+++ src/Pt-System/win32/SharedLibImpl.h
@@ -1,7 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2006-2007 Marc Boris Duerner                            *
- *   Copyright (C) 2006-2007 Tobias Mueller                                *
- *   Copyright (C) 2006-2007 PTV AG                                        *
+ *   Copyright (C) 2006-2008 Marc Boris Duerner                            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
@@ -21,84 +19,56 @@
 #ifndef PT_SHAREDLIBIMPL_H
 #define PT_SHAREDLIBIMPL_H
 
-#include "Pt/Api.h"
 #include "Pt/System/SystemError.h"
 #include "Pt/System/SharedLib.h"
-#include "Pt/System/File.h"
-
 #include "win32.h"
-
 #include <vector>
 #include <iostream>
 #include <sstream>
 #include <windows.h>
 
-
 namespace Pt {
 
 namespace System {
 
-class SharedLibImpl {
+class SharedLibImpl
+{
     public:
-        //! @brief default Constructor
         SharedLibImpl()
         : _handle(0)
         { }
 
-        /**
-         * @brief Constructor which takes the path to the shared library to load
-         * @see SharedLib#SharedLib()
-         * @param libraryFile A File object referencing the library in the file system.
-         */
-        SharedLibImpl(const File& libraryFile)
+        SharedLibImpl(const std::string& path)
         : _handle(0)
         {
-            this->open(libraryFile);
+            this->open(path);
         }
 
-        //! @brief Destructor
         ~SharedLibImpl() throw()
         {
             if(_handle != 0) {
-                ::FreeLibrary(_handle);                
+                ::FreeLibrary(_handle);
             }
         }
 
-        /**
-         * @brief Loads the shared library specified by path
-         *
-         * This method holds the operating system dependend code to actually
-         * load the shared library.
-         * @see SharedLib#open()
-         * @param libraryFile A File object referencing the library in the file system.
-         */
-        void open(const File& libraryFile)
+        void open(const std::string& path)
         {
             if(_handle != 0) {
                 return;
             }
 
-            std::basic_string<TCHAR> tpath = win32::fromMultiByte(libraryFile.path());
+            std::basic_string<TCHAR> tpath = win32::fromMultiByte(path);
             _handle = ::LoadLibrary( tpath.c_str() );
 
             if(_handle == 0)
             {
                 Pt::uint32_t errorCode = GetLastError();
                 std::stringstream ss;
-                ss << "Could not open shared library(" << libraryFile.path() << "). System error code: " << errorCode;
+                ss << "Could not open shared library " << path << " error " << errorCode;
                 throw SystemError(ss.str(), PT_SOURCEINFO);
             }
         }
 
-        /**
-         * @brief Resolves the symbol specified by symbol
-         *
-         * This method holds the operating system dependend code to actually
-         * resolve the symbol within the shared library.
-         * @see SharedLib#resolve()
-         * @param symbol the symbol to resolve
-         * @return the resolved symbol or 0 if the symbol cannot be resolved
-         */
         void* resolve(const char* symbol)
         {
             if(_handle == 0) {
@@ -109,38 +79,8 @@ class SharedLibImpl {
             return (void*) ( ::GetProcAddress( _handle, tsymbol.c_str() ) );
         }
 
-        /**
-         * @brief Returns if the loading of the shared library was successful or not
-         * @see SharedLib#failed()
-         * @return true if the loading of the shared library has failed, false otherwise.
-         */
         bool failed()
         { return _handle == 0; }
-
-    public:
-
-        /**
-         * @brief Implicitely loads the shared library specified by path and tries to resolve the symbol specified by symbol
-         * 
-         * This method contains the operating system dependend code to load the
-         * shared library and to resolve the desired symbol.
-         * @see SharedLib::failed()
-         * @param libraryFile A File object referencing the library in the file system.
-         * @param symbol The symbol to resolve within the loaded library.
-         * @return The resolved symbol or 0 if the loading of the shared library has failed.
-         */
-        static void* openResolve(const File& libraryFile, const char* symbol)
-        {
-            std::basic_string<TCHAR> tpath = win32::fromMultiByte(libraryFile.path());
-            HMODULE handle = ::LoadLibrary( tpath.c_str() );
-
-            // TODO: throw different exception when path does not exist
-            if(handle == 0)
-                throw SystemError("Could not open shared library", PT_SOURCEINFO);
-
-            std::basic_string<TCHAR> tsymbol = win32::fromMultiByte(symbol);
-            return (void*) ::GetProcAddress( handle, tsymbol.c_str() );
-        }
 
     private:
         HMODULE _handle;
