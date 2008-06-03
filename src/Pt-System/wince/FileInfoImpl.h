0bdb9725d40d0df776ffa444cb0b7e5b51ed61fe
d-marc <d-marc@private.net>
2008-06-03 17:39:46 +0000

    renamed FileSystemNodeImpl to FileInfoImpl

--- src/Pt-System/wince/FileInfoImpl.h
+++ src/Pt-System/wince/FileInfoImpl.h
@@ -17,7 +17,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "../win32/FileSystemNodeImpl.h"
+#include "../win32/FileInfoImpl.h"
 
 
 
