50473e570c02abd20b927c296be014160ea76c15
d-marc <d-marc@private.net>
2008-06-10 21:35:39 +0000

    changed libname

--- src/Pt-System/PluginDemo.cpp
+++ src/Pt-System/PluginDemo.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
         std::locale::global( std::locale(std::locale(), new std::num_get<Pt::Char>) );
         std::locale::global( std::locale(std::locale(), new std::num_put<Pt::Char>) );
     */
-        std::string  shlibName = "libTestPlugin";
+        std::string  shlibName = "TestPlugin";
 
         #ifndef NDEBUG
             shlibName += "d";
