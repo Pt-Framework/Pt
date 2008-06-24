0b39007cfadbf616a33f585153e1dcca6a2b7d2c
d-marc <d-marc@private.net>
2008-06-24 11:20:56 +0000

    not inline anymore

---
+++ src/Pt-System/EventSource.cpp
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *   Copyright (C) 2008 Marc Boris Duerner                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "Pt/System/EventSource.h"
+
+namespace Pt {
+
+namespace System {
+
+EventSource::EventSource()
+: _mutex(Pt::System::Mutex::Normal)
+{ }
+
+
+EventSource::~EventSource()
+{
+    Connection connection;
+    while( true )
+    {
+        {
+            MutexLock lock( _mutex );
+
+                if( _connections.empty() )
+                break;
+
+                connection = _connections.front();
+                _connections.remove( connection );
+        }
+
+        connection.close();
+    }
+}
+
+
+Connection EventSource::connect( EventLoop& receiver )
+{
+    // Do not lock here, the Connection will call
+    // Connectable::opened on this object
+    return Connection( *this, slot(receiver, &EventLoop::commitEvent).clone() );
+}
+
+
+Connection EventSource::connect( const Slot& s )
+{
+    // Do not lock here, the Connection will call
+    // Connectable::opened on this object
+    return Connection( *this, s.clone() );
+}
+
+
+bool EventSource::opened( const Connection& c )
+{
+    MutexLock lock(_mutex);
+    bool accept = Connectable::opened(c);
+    return accept;
+}
+
+
+void EventSource::closed( const Connection& c )
+{
+    MutexLock lock(_mutex);
+    Connectable::closed(c);
+}
+
+
+void EventSource::send(const Pt::Event& ev) const
+{
+    typedef Pt::Invokable<const Pt::Event&, Pt::Void, Pt::Void> InvokableT;
+    MutexLock lock(_mutex);
+
+    std::list<Connection>::const_iterator it = Connectable::connections().begin();
+    for(; it != _connections.end(); ++it)
+    {
+        if( false == it->valid() || &( it->sender() ) != this  )
+            continue;
+
+        const InvokableT* invokable = static_cast<const InvokableT*>( it->slot().callable() );
+        invokable->invoke(ev);
+    }
+}
+
+} // namespace System
+
+} // namespace Pt
