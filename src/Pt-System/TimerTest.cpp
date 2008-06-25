a30c91a65e3af3866a883ebc8caa0260bc1e19d0
d-marc <d-marc@private.net>
2008-06-25 10:58:22 +0000

    consolidated system unit tests

---
+++ src/Pt-System/TimerTest.cpp
@@ -0,0 +1,40 @@
+#include "Pt/System/Timer.h"
+#include "Pt/System/Selector.h"
+#include "Pt/Unit/Assertion.h"
+#include "Pt/Unit/TestSuite.h"
+#include "Pt/Unit/RegisterTest.h"
+#include <iostream>
+#include <sstream>
+
+class TimerTest : public Pt::Unit::TestSuite
+{
+    public:
+        TimerTest()
+        : TestSuite("TimerTest")
+        , _count(0)
+        {
+            this->registerMethod("Timeout", *this, &TimerTest::timeout);
+        }
+
+        void timeout()
+        {
+            Pt::System::Timer timer;
+            connect( timer.timeout, *this, &TimerTest::onTimeout );
+            timer.start(1000);
+
+            Pt::System::Selector selector;
+            selector.add(timer);
+            selector.wait(50000);
+            PT_UNIT_ASSERT(_count == 1);
+        }
+
+        void onTimeout()
+        {
+            _count++;
+        }
+
+    private:
+        unsigned _count;
+};
+
+Pt::Unit::RegisterTest<TimerTest> register_TimerTest;
