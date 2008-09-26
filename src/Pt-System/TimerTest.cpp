86e4e95a730a0ea6ba948db37e01afb9665c84ad
d-marc <d-marc@private.net>
2008-09-26 19:47:04 +0000

    more timer tests

--- src/Pt-System/TimerTest.cpp
+++ src/Pt-System/TimerTest.cpp
@@ -11,20 +11,76 @@ class TimerTest : public Pt::Unit::TestSuite
     public:
         TimerTest()
         : TestSuite("TimerTest")
+        , _timer(0)
         , _count(0)
         {
-            this->registerMethod("Timeout", *this, &TimerTest::timeout);
+            this->registerMethod("Timeout", *this, &TimerTest::Timeout);
+            this->registerMethod("RemoveOnTimeout", *this, &TimerTest::RemoveOnTimeout);
+            this->registerMethod("RemoveAddOnTimeout", *this, &TimerTest::RemoveAddOnTimeout);
+            this->registerMethod("DestroyOnTimeout", *this, &TimerTest::DestroyOnTimeout);
         }
 
-        void timeout()
+        void setUp()
         {
-            Pt::System::Timer timer;
-            connect( timer.timeout, *this, &TimerTest::onTimeout );
-            timer.start(1000);
+            _count = 0;
+            _selector = new Pt::System::Selector();
+            _timer = new Pt::System::Timer();
+            _timer->start(500);
+            _selector->add(*_timer);
+        }
 
-            Pt::System::Selector selector;
-            selector.add(timer);
-            selector.wait(50000);
+        void tearDown()
+        {
+            delete _selector;
+            _selector = 0;
+            delete _timer;
+            _timer = 0;
+        }
+
+        void Timeout()
+        {
+            connect( _timer->timeout, *this, &TimerTest::onTimeout );
+
+            _selector->wait(1000);
+            PT_UNIT_ASSERT(_count == 1);
+
+            _selector->wait(1000);
+            PT_UNIT_ASSERT(_count == 2);
+        }
+
+        void RemoveOnTimeout()
+        {
+            connect( _timer->timeout, *this, &TimerTest::onTimeout );
+            connect( _timer->timeout, *this, &TimerTest::removeTimer );
+
+            _selector->wait(1000);
+            PT_UNIT_ASSERT(_count == 1);
+
+            _selector->wait(1000);
+            PT_UNIT_ASSERT(_count == 1);
+        }
+
+        void RemoveAddOnTimeout()
+        {
+            connect( _timer->timeout, *this, &TimerTest::onTimeout );
+            connect( _timer->timeout, *this, &TimerTest::removeAddTimer );
+
+            _selector->wait(1000);
+            PT_UNIT_ASSERT(_count == 1);
+
+            _selector->wait(1000);
+            PT_UNIT_ASSERT(_count == 2);
+        }
+
+        void DestroyOnTimeout()
+        {
+            connect( _timer->timeout, *this, &TimerTest::onTimeout );
+            connect( _timer->timeout, *this, &TimerTest::destroyTimer );
+
+            _selector->wait(1000);
+            PT_UNIT_ASSERT(_count == 1);
+
+            _selector->wait(1000);
             PT_UNIT_ASSERT(_count == 1);
         }
 
@@ -33,7 +89,26 @@ class TimerTest : public Pt::Unit::TestSuite
             _count++;
         }
 
+        void removeTimer()
+        {
+            _timer->setSelector(0);
+        }
+
+        void removeAddTimer()
+        {
+            _timer->setSelector(0);
+            _timer->setSelector(_selector);
+        }
+
+        void destroyTimer()
+        {
+            delete _timer;
+            _timer = 0;
+        }
+
     private:
+        Pt::System::Timer* _timer;
+        Pt::System::Selector* _selector;
         unsigned _count;
 };
 
