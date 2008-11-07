8f3278b877848a716577f3fb5feae944657f5033
d-marc <d-marc@private.net>
2008-11-07 17:16:15 +0000

    added ThreadTest

--- src/Pt-System/ThreadTest.cpp
+++ src/Pt-System/ThreadTest.cpp
@@ -17,26 +17,98 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include "Pt/System/Thread.h"
+#include "Pt/System/Mutex.h"
+#include "Pt/System/Condition.h"
 #include "Pt/Unit/Assertion.h"
 #include "Pt/Unit/TestSuite.h"
 #include "Pt/Unit/RegisterTest.h"
+
+class ThreadTest;
+
+class TestThread : public Pt::System::DetachedThread
+{
+    public:
+        TestThread(ThreadTest& test)
+        : _test(test)
+        {}
+
+        virtual void run();
+
+    private:
+        ThreadTest& _test;
+};
 
 class ThreadTest : public Pt::Unit::TestSuite
 {
     public:
         ThreadTest()
         : Pt::Unit::TestSuite("ThreadTest")
+        , _mutex(Pt::System::Mutex::Normal)
+        , _flag(false)
         {
-            Pt::Unit::TestSuite::registerMethod( "MethodAttached", *this, &ThreadTest::MethodAttached );
+            Pt::Unit::TestSuite::registerMethod( "Attached", *this, &ThreadTest::Attached );
+            Pt::Unit::TestSuite::registerMethod( "Detached", *this, &ThreadTest::Detached );
+        }
+
+        void setUp()
+        {
+            setFlag(false);
+        }
+
+        bool flag() const
+        {
+            Pt::System::MutexLock lock(_mutex);
+            return _flag;
+        }
+
+        void setFlag(bool f)
+        {
+            Pt::System::MutexLock lock(_mutex);
+            _flag = f;
+            _cond.signal();
         }
 
     protected:
-        void MethodAttached();
+        void Attached();
+        void Detached();
+
+        void thread1()
+        {
+            setFlag(true);
+        }
+
+    private:
+        mutable Pt::System::Mutex _mutex;
+        mutable Pt::System::Condition _cond;
+        bool _flag;
 };
 
 
-inline void ThreadTest::MethodAttached()
+inline void TestThread::run()
 {
+    _test.setFlag(true);
+}
+
+
+inline void ThreadTest::Attached()
+{
+    Pt::System::AttachedThread thread( callable(*this, &ThreadTest::thread1) );
+    thread.start();
+    thread.join();
+    PT_UNIT_ASSERT( flag() );
+}
+
+
+inline void ThreadTest::Detached()
+{
+    TestThread* thread = new TestThread(*this);
+    thread->start();
+
+    _mutex.lock();
+    _cond.wait(_mutex);
+
+    PT_UNIT_ASSERT( _flag );
+    _mutex.unlock();
 }
 
 
