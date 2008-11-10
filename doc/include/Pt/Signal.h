d773b9742ae41863c19906440ab8b0c5ed75c027
d-marc <d-marc@private.net>
2008-11-10 10:33:13 +0000

    added docs

--- doc/include/Pt/Signal.h
+++ doc/include/Pt/Signal.h
@@ -60,6 +60,97 @@ class Signal : public SignalBase {
         inline void operator()(ARGUMENTS args) const;
 };
 
+
+/** @brief  Wraps %Signal objects so that they can act as Slots.
+    @ingroup sigslot
+
+    SignalSlot is a "slot wrapper" for Signal objects. That is, it
+    effectively converts a Signal object into a Slot object, so that it
+    can be used as the target of another Signal. This allows chaining of
+    Signals.
+*/
+template <typename ARGUMENTS>
+class SignalSlot : public BasicSlot<void, ARGUMENTS>
+{
+    public:
+        //! @brief Constructs from signal
+        SignalSlot(Signal<ARGUMENTS>& signal);
+
+        // inherit doc
+        Slot* clone() const;
+
+        // inherit doc
+        virtual const void* callable() const;
+
+        // inherit doc
+        virtual void onConnect(const Connection& c);
+
+        // inherit doc
+        virtual void onDisconnect(const Connection& c);
+
+        // inherit doc
+        virtual bool equals(const Slot& slot) const;
+
+    private:
+        //! @internal
+        mutable ConstMethod<void, Signal<ARGUMENTS>, ARGUMENTS > _method;
+};
+
+
+/** @brief  Creates a %SignalSlot object from an equivalent %Signal.
+    @ingroup sigslot
+*/
+template <typename ARGS>
+SignalSlot<ARGS> slot( Signal<ARGS> & signal );
+
+/** @brief  Connects a %Signal to a function.
+    @ingroup sigslot
+*/
+template <typename R, typename ARGS>
+Connection connect(Signal<ARGS>& signal, R(*func)(ARGS));
+
+/** @brief  Connects a %Signal to a member function.
+    @ingroup sigslot
+*/
+template <typename R, class BaseT, class ClassT, class ARGS>
+Connection connect(Signal<ARGS>& signal, BaseT& object, R(ClassT::*memFunc)(ARGS));
+
+/** @brief  Connects a %Signal to a const member function.
+    @ingroup sigslot
+*/
+template <typename R, class BaseT, class ClassT, class ARGS>
+Connection connect(Signal<ARGS>& signal, BaseT& object, R(ClassT::*memFunc)(ARGS) const);
+
+/** @brief  Connects a %Signal to another %Signal.
+    @ingroup sigslot
+*/
+template <class ARGS>
+Connection connect(Signal<ARGS>& sender, Signal<ARGS>& receiver);
+
+/** @brief Disconnects a %Signal from a function
+    @ingroup sigslot
+*/
+template <typename R,class ARGS>
+void disconnect(Signal<ARGS>& signal, R(*func)(ARGS));
+
+/** @brief Disconnects a %Signal from a member function
+    @ingroup sigslot
+*/
+template <typename R, class BaseT, class ClassT,class ARGS>
+void disconnect(Signal<ARGS>& signal, BaseT& object, R(ClassT::*memFunc)(ARGS));
+
+/** @brief Disconnects a %Signal from a const member function
+    @ingroup sigslot
+*/
+template <typename R, class BaseT, class ClassT, class ARGS>
+void disconnect(Signal<ARGS>& signal, BaseT& object, R(ClassT::*memFunc)(ARGS) const);
+
+/** @brief Disconnects a %Signal from another %Signal
+    @ingroup sigslot
+*/
+template <class ARGS>
+void disconnect(Signal<ARGS>& sender, Signal<ARGS>& receiver);
+
 } // !namespace Pt
 
 #endif
