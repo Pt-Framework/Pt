17913548611d14b9e1ed995a1f72bf3a9b215939
d-marc <d-marc@private.net>
2008-11-25 21:25:20 +0000

    leaner exception construction, leaner string conversion for unicode win32 functions

--- src/Pt-System/IOError.cpp
+++ src/Pt-System/IOError.cpp
@@ -2,8 +2,6 @@
  *   Copyright (C) 2004 Marc Boris Dürner                                  *
  *   Copyright (C) 2005 Aloysius Indrayanto                                *
  *                                                                         *
- *   This code is based on code written by Christian Prochnow              *
- *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
  *   published by the Free Software Foundation; either version 2 of the    *
