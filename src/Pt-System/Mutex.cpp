53530133a16f11986ef6736b62ca1e15952677ad
d-marc <d-marc@private.net>
2008-12-01 16:00:44 +0000

    added docs

--- src/Pt-System/Mutex.cpp
+++ src/Pt-System/Mutex.cpp
@@ -1,6 +1,5 @@
 /***************************************************************************
  *   Copyright (C) 2005-2008 by Marc Boris Duerner                         *
- *   Copyright (C) 2005-2006 by Sebastian Pieck                            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
