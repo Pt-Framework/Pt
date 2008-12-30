55ba6237ceb7d949eedb743c9ab19d8f60c77a45
d-marc <d-marc@private.net>
2008-12-30 22:22:05 +0000

    improved base64 codec

--- src/Pt/Base64Codec.cpp
+++ src/Pt/Base64Codec.cpp
@@ -32,38 +32,33 @@ namespace Pt {
 
 char toBase64(uint8_t n)
 {
-    static const char b64Table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    return b64Table[n];
+    static const char b64enc[] 
+        = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    
+    return b64enc[n];
 }
 
 uint8_t fromBase64(char b64)
 {
-    switch(b64) {
-        case 'A': return 0; case 'B': return 1; case 'C': return 2;
-        case 'D': return 3; case 'E': return 4; case 'F': return 5;
-        case 'G': return 6; case 'H': return 7; case 'I': return 8;
-        case 'J': return 9; case 'K': return 10; case 'L': return 11;
-        case 'M': return 12; case 'N': return 13; case 'O': return 14;
-        case 'P': return 15; case 'Q': return 16; case 'R': return 17;
-        case 'S': return 18; case 'T': return 19; case 'U': return 20;
-        case 'V': return 21; case 'W': return 22; case 'X': return 23;
-        case 'Y': return 24; case 'Z': return 25; case 'a': return 26;
-        case 'b': return 27; case 'c': return 28; case 'd': return 29;
-        case 'e': return 30; case 'f': return 31; case 'g': return 32;
-        case 'h': return 33; case 'i': return 34; case 'j': return 35;
-        case 'k': return 36; case 'l': return 37; case 'm': return 38;
-        case 'n': return 39; case 'o': return 40; case 'p': return 41;
-        case 'q': return 42; case 'r': return 43; case 's': return 44;
-        case 't': return 45; case 'u': return 46; case 'v': return 47;
-        case 'w': return 48; case 'x': return 49; case 'y': return 50;
-        case 'z': return 51; case '0': return 52; case '1': return 53;
-        case '2': return 54; case '3': return 55; case '4': return 56;
-        case '5': return 57; case '6': return 58; case '7': return 59;
-        case '8': return 60; case '9': return 61; case '+': return 62;
-        case '/': return 63; case '=': return 64;
-    }
-
-    return 255;
+    static const char b64dec[] 
+        = { 255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,62,255,255,255,63,
+            52,53,54,55,56,57,58,59,60,61,255,255,255,64,255,255,
+            255, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,
+            15,16,17,18,19,20,21,22,23,24,25,255,255,255,255,255,
+            255,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+            41,42,43,44,45,46,47,48,49,50,51,255,255,255,255,255, 
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
+            255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255 };
+    
+    return b64dec[(int)b64];
 }
     
 Base64Codec::Base64Codec(size_t ref)
@@ -71,8 +66,7 @@ Base64Codec::Base64Codec(size_t ref)
 {}
 
 Base64Codec::~Base64Codec()
-{
-}
+{}
 
 Base64Codec::result Base64Codec::do_in(MBState& s, 
                                        const char* fromBegin,
