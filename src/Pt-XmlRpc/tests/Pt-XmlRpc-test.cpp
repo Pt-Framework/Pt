2be233d35192a9f0c534b96a194ae3d55d063024
d-marc <d-marc@private.net>
2009-04-07 18:43:22 +0000

    moved to tests directory

--- src/Pt-XmlRpc/tests/Pt-XmlRpc-test.cpp
+++ src/Pt-XmlRpc/tests/Pt-XmlRpc-test.cpp
@@ -25,8 +25,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-#undef PT_XMLRPC_API_EXPORT
-
 #include "Pt/Unit/TestSuite.h"
 #include "Pt/Unit/RegisterTest.h"
 #include "Pt/Unit/TestMain.h"
