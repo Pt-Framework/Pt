820344aa76fdd3c180a4f408f574c5be54ec1e51
d-marc <d-marc@private.net>
2009-08-27 21:18:03 +0000

    context might provide decomposition rules

--- src/Pt/Date.cpp
+++ src/Pt/Date.cpp
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2004-2008 Marc Boris Duerner
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * As a special exception, you may use this file as part of a free
  * software library without restriction. Specifically, if other files
  * instantiate templates or use macros or inline functions from this
@@ -15,18 +15,19 @@
  * License. This exception does not however invalidate any other
  * reasons why the executable file might be covered by the GNU Library
  * General Public License.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #include "Pt/Date.h"
 #include "Pt/SerializationInfo.h"
+#include "Pt/SerializationContext.h"
 #include <cctype>
 
 namespace Pt {
@@ -139,6 +140,12 @@ void operator>>=(const SerializationInfo& si, Date& date)
 
 void operator<<=(SerializationInfo& si, const Date& date)
 {
+    if( si.context() )
+    {
+        if( si.context()->decompose(si, date) )
+            return;
+    }
+
     std::string s;
     convert(s, date);
     si.setValue(s);
