07a3d8d1e285687900fdf2742a3a99d7c7f0043e
blue_wind_25 <blue_wind_25@private.net>
2011-05-18 08:02:22 +0000

    Adding docs about SSL session to SSLContext.cpp.

--- src/Pt-Ssl/SSLContext.cpp
+++ src/Pt-Ssl/SSLContext.cpp
@@ -121,28 +121,110 @@ SSLContext::SSLContext(const char* sessionID, Protocol    protocol)
         );
     }
 
-    /*
-        // Possible functions that will allow us to store/retrieve session data to/from disk.
+    /* Possible functions that will allow us to store/retrieve session data to/from disk.
 
         long SSL_CTX_set_session_cache_mode(SSL_CTX ctx, long mode);
-        int SSL_CTX_add_session(SSL_CTX *ctx, SSL_SESSION *c);
-        int SSL_CTX_remove_session(SSL_CTX *ctx, SSL_SESSION *c);
+            # Enables/disables session caching by setting the operational mode for ctx to <mode>.
+            # Returns the previously set cache mode.
+            + SSL_SESS_CACHE_OFF
+                No session caching for client or server takes place.
+            + SSL_SESS_CACHE_CLIENT
+                Client sessions are added to the session cache. As there is no reliable way for the OpenSSL library to know whether
+                a session should be reused or which session to choose (due to the abstract BIO layer the SSL engine does not have
+                details about the connection), the application must select the session to be reused by using the ssl_set_session(3)
+                function. This option is not activated by default.
+            + SSL_SESS_CACHE_SERVER
+                Server sessions are added to the session cache. When a client proposes a session to be reused, the server looks for
+                the corresponding session in (first) the internal session cache (unless SSL_SESS_CACHE_NO_INTERNAL_LOOKUP is set),
+                then (second) in the external cache if available. If the session is found, the server will try to reuse the session.
+                This is the default.
+            + SSL_SESS_CACHE_BOTH
+                Enable both SSL_SESS_CACHE_CLIENT and SSL_SESS_CACHE_SERVER at the same time.
+            + SSL_SESS_CACHE_NO_AUTO_CLEAR
+                Normally the session cache is checked for expired sessions every 255 connections using the ssl_ctx_flush_sessions(3)
+                function. Since this may lead to a delay which cannot be controlled, the automatic flushing may be disabled and
+                ssl_ctx_flush_sessions(3) can be called explicitly by the application.
+            + SSL_SESS_CACHE_NO_INTERNAL_LOOKUP
+                By setting this flag, session-resume operations in an SSL/TLS server will not automatically look up sessions in
+                the internal cache, even if sessions are automatically stored there. If external session caching callbacks are
+                in use, this flag guarantees that all lookups are directed to the external cache. As automatic lookup only applies
+                for SSL/TLS servers, the flag has no effect on clients.
+            + SSL_SESS_CACHE_NO_INTERNAL_STORE
+                Depending on the presence of SSL_SESS_CACHE_CLIENT and/or SSL_SESS_CACHE_SERVER, sessions negotiated in an SSL/TLS
+                handshake may be cached for possible reuse. Normally a new session is added to the internal cache as well as any
+                external session caching (callback) that is configured for the SSL_CTX . This flag will prevent sessions being stored
+                in the internal cache (though the application can add them manually using ssl_ctx_add_session(3)). Note: in any
+                SSL/TLS servers where external caching is configured, any successful session lookups in the external cache
+                (ie. for session-resume requests) would normally be copied into the local cache before processing continues -
+                this flag prevents these additions to the internal cache as well.
+            + SSL_SESS_CACHE_NO_INTERNAL
+                Enable both SSL_SESS_CACHE_NO_INTERNAL_LOOKUP and SSL_SESS_CACHE_NO_INTERNAL_STORE at the same time.
+
         long SSL_CTX_sess_set_cache_size(SSL_CTX *ctx, long t);
+            # The internal session cache size is SSL_SESSION_CACHE_MAX_SIZE_DEFAULT, currently 1024*20, so that up to 20000 sessions
+              can be held.
+            # A special case is the size 0, which is used for unlimited size.
+            # When the maximum number of sessions is reached, no more new sessions are added to the cache. New space may be added
+               by calling SSL_CTX_flush_sessions(3) to remove expired sessions.
+            # Returns the previously valid size.
+
+        void SSL_CTX_flush_sessions(SSL_CTX *ctx, long tm);
+            # The parameter tm specifies the time which should be used for the expiration test, in most cases the actual time given
+              by time(0) will be used.
+            # SSL_CTX_flush_sessions() will only check sessions stored in the internal cache. When a session is found and removed,
+               the remove_session_cb is however called to synchronize with the external cache (see ssl_ctx_sess_set_get_cb(3)).
+
+        int SSL_CTX_add_session(SSL_CTX *ctx, SSL_SESSION *c);
+            # Adds the session c to the context ctx. The reference count for session c is incremented by 1. If a session with
+              the same session id already exists, the old session is removed by calling SSL_SESSION_free(3).
+            # If a server SSL_CTX is configured with the SSL_SESS_CACHE_NO_INTERNAL_STORE flag then the internal cache will not
+              be populated automatically by new sessions negotiated by the SSL/TLS implementation, even though the internal cache
+              will be searched automatically for session-resume requests (the latter can be surpressed by
+              SSL_SESS_CACHE_NO_INTERNAL_LOOKUP). So the application can use SSL_CTX_add_session() directly to have full control
+              over the sessions that can be resumed if desired.
+            # Return 0 if the operation failed. In case of the add operation, it was tried to add
+              the same (identical) session twice.
+
+        int SSL_CTX_remove_session(SSL_CTX *ctx, SSL_SESSION *c);
+            # Removes the session c from the context ctx. SSL_SESSION_free(3) is called once for c.
+            # Return 0 if the operation failed. In case of the add operation, it was tried to add
+              the same (identical) session twice. In case of the remove operation, the
+              session was not found in the cache.
 
         void SSL_CTX_sess_set_new_cb(SSL_CTX *ctx, int (*new_session_cb)(SSL *, SSL_SESSION *));
+            # Callback: int (*new_session_cb)(struct ssl_st *ssl, SSL_SESSION *sess);
+            # The callback is called, whenever a new session has been negotiated and session caching is enabled.
+            # The callback is passed the ssl connection and the ssl session sess.
+            # If the callback returns 0, the session will be immediately removed again.
+
         void SSL_CTX_sess_set_remove_cb(SSL_CTX *ctx, void (*remove_session_cb)(SSL_CTX *ctx, SSL_SESSION *));
+            # void (*remove_session_cb)(struct ssl_ctx_st *ctx, SSL_SESSION *sess);
+            # The callback is called, whenever the SSL engine removes a session from the internal cache.
+            # This happens when the session is removed because it is expired or when a connection was not shutdown cleanly.
+              It also happens for all sessions in the internal session cache when ssl_ctx_free(3) is called.
+            # The callback is passed the ctx and the ssl session sess.
+
         void SSL_CTX_sess_set_get_cb(SSL_CTX *ctx, SSL_SESSION (*get_session_cb)(SSL *, unsigned char *, int, int *));
-
-        int (*SSL_CTX_sess_get_new_cb(SSL_CTX *ctx))(struct ssl_st *ssl, SSL_SESSION *sess);
-        void (*SSL_CTX_sess_get_remove_cb(SSL_CTX *ctx))(struct ssl_ctx_st *ctx, SSL_SESSION *sess);
-        SSL_SESSION *(*SSL_CTX_sess_get_get_cb(SSL_CTX *ctx))(struct ssl_st *ssl, unsigned char *data, int len, int *copy);
-
-        int (*new_session_cb)(struct ssl_st *ssl, SSL_SESSION *sess);
-        void (*remove_session_cb)(struct ssl_ctx_st *ctx, SSL_SESSION *sess);
-        SSL_SESSION *(*get_session_cb)(struct ssl_st *ssl, unsigned char *data, int len, int *copy);
+            # SSL_SESSION *(*get_session_cb)(struct ssl_st *ssl, unsigned char *data, int len, int *copy);
+            # The callback is only called on SSL/TLS servers with the session id proposed by the client.
+            # The callback is always called, also when session caching was disabled.
+            # The callback is passed the ssl connection, the session id of length len at the memory location data.
+            # With the parameter copy the callback can require the SSL engine to increment the reference count of the SSL_SESSION object.
+              Normally the reference count is not incremented and therefore the session must not be explicitly freed with ssl_session_free(3).
 
         SSL_SESSION *d2i_SSL_SESSION(SSL_SESSION **a, const unsigned char **pp, long length);
+            # Transforms the external ASN1 representation of an SSL/TLS session, stored as binary data at location pp with length length,
+              into an SSL_SESSION object.
+            # In case of failure the NULL-pointer is returned and the error message can be retrieved from the error stack.
+
         int i2d_SSL_SESSION(SSL_SESSION *in, unsigned char **pp);
+            # Transforms the SSL_SESSION object in into the ASN1 representation and stores it into the memory location pointed to by pp.
+            # The length of the resulting ASN1 representation is returned.
+            # If pp is the NULL pointer, only the length is calculated and returned.
+            # When the session is not valid, 0 is returned and no operation is performed.
+            # There is no known limit on the size of the created ASN1 representation, so the necessary amount of space should be obtained
+              by first calling i2d_SSL_SESSION() with pp=NULL, and obtain the size needed, then allocate the memory and call
+              i2d_SSL_SESSION() again.
     */
 }
 
