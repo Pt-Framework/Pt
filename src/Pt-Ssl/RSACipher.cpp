d1520e3b4065adb1b9621eb2b0344783f7a412f8
blue_wind_25 <blue_wind_25@private.net>
2011-05-27 05:11:36 +0000

    Adding the forgotten copyright :P

--- src/Pt-Ssl/RSACipher.cpp
+++ src/Pt-Ssl/RSACipher.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010-2010 by Aloysius Indrayanto
+ * Copyright (C) 2010-2010 by Marc Boris Duerner
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
