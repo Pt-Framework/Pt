1cb0ad2bebb42726428bc5fa8a43f899ee22c1b3
d-marc <d-marc@private.net>
2011-08-12 08:51:28 +0000

    custom double to string conversion

--- src/Pt-XmlRpc/Formatter.cpp
+++ src/Pt-XmlRpc/Formatter.cpp
@@ -90,9 +90,6 @@ inline const Pt::Char* signed_integer_to_string(Pt::Char* buf, size_t n, T i)
 template <typename T>
 inline const Pt::Char* unsigned_integer_to_string(Pt::Char* buf, size_t n, T i)
 {
-    const char* digits = "0123456789";
-    assert( *digits == '0' );
-
     if(0 == n)
         return 0;
 
@@ -107,21 +104,18 @@ inline const Pt::Char* unsigned_integer_to_string(Pt::Char* buf, size_t n, T i)
         T lsd = i % 10;
         i /= 10;
         --psz;
-        *psz = digits[lsd];
+        *psz = '0' + int(lsd);
 
     } while(i != 0);
 
     return psz;
 }
 
-inline const Pt::Char* double_to_string(Pt::Char* str, size_t n, double d)
+template <typename T>
+inline const Pt::Char* double_to_string(Pt::Char* str, size_t n, T d)
 {
-    const char* digits = "9876543210123456789";
-    digits += 9;
-    assert( *digits == '0' );
-
     Pt::Char* end = str + n;
-    Pt::Char* psz = end;
+    Pt::Char* psz = str;
 
     // 1. Test for not-a-number with x != x
     if( d != d ) 
@@ -134,7 +128,7 @@ inline const Pt::Char* double_to_string(Pt::Char* str, size_t n, double d)
     }
 
     //2. Test for infinity by comparing with max
-    if( d == std::numeric_limits<double>::infinity() ) 
+    if( d == std::numeric_limits<T>::infinity() ) 
     {
         if(n < 4)
             return 0;
@@ -143,66 +137,63 @@ inline const Pt::Char* double_to_string(Pt::Char* str, size_t n, double d)
         return str;
     }
     
-    //3. Split into integer and fractional parts with modf (<cmath>)
-    double intpart = 0;
-    double fract = std::modf(d, &intpart);
-    int digit = 0;
-	size_t places = std::numeric_limits<double>::digits10;
-    
-    //4. Repeatedly div-mod the integer part to extract the digits
-    //   left of the decimal point.
-    
-    if(intpart == 0)
+    //3. intergal part
+    if(d < 0.0)
     {
-		if(psz == str)
+		if(psz == end)
 			return 0;
-			
-		*(--psz) = '0';
+
+		*psz = '-';
+		++psz;
+    }
+
+	int digit = 0;
+	T num = std::fabs(d);
+	int m = static_cast<int>( std::log10(num) );
+	size_t places = std::numeric_limits<T>::digits10;
+    
+    if(num == 0.0 || m < 0)
+    {
+		if(psz == end)
+			return 0;
+
+		*psz = '0';
+		++psz;
     }
     else
     {
-		do
+		while(m >= 0)
 		{
-			if(psz == str)
-				return 0;
+			T weight = std::pow(10.0, m);
+			digit = static_cast<int>( floor(num / weight) );
+			num -= (digit * weight);
 			
-			double remain = std::fmod(intpart, 10);
-			digit = static_cast<int>( remain );
-			intpart -= remain;
-			intpart /= 10;
-			*(--psz) = digits[digit];
+			if(psz == end)
+				return 0;
+
+			*psz = '0' + digit;
+			++psz;
+	     
+			--m;
 			--places;
-		} 
-		while(intpart != 0.0);
+		}
     }
     
-    if(d < 0.0)
-    {
-		if(psz == str)
-			return 0;
-		*(--psz) = '-';
-    }
-    
-    size_t count = end - psz;
-    std::memmove(str, psz, count * sizeof(Pt::Char));
-    psz = str + count;
+    //4. fractional part
+	T fract = num;
     
     if(psz == end)
 		return 0;
 
     *psz = '.';
-    psz++;
-
-    //5. repeatedly multiply and modf the fractional part to get the 
-    //   digits to the right. It will probably be faster to work in base 
-    //   10^9 rather than 10.
+    ++psz;
 
     do
     {
         fract *= 10;
-        fract = modf(fract, &intpart);
-        digit = static_cast<int>(intpart);
-        char c = digits[digit];
+        digit = static_cast<int>( floor(fract) );
+        fract -= digit;
+        char c = '0' + digit;
 
         if(psz == end)
             return 0;
@@ -212,7 +203,7 @@ inline const Pt::Char* double_to_string(Pt::Char* str, size_t n, double d)
     } 
     while(--places != 0 && fract != 0.0);
 
-	//6. add null terminator
+	//5. add null terminator
     if(psz == end)
          return 0;
 
@@ -220,40 +211,17 @@ inline const Pt::Char* double_to_string(Pt::Char* str, size_t n, double d)
     return str;
 }
 
-
-
 /*
 struct Tester
 {
 	Tester()
 	{
 		std::cerr << "-----   TEST   -----" << std::endl;
-
-        char fstrbuf[80];
-        char* fstr = fstrbuf;
-        double num = 2.55;
-        int m = std::log10(num);
-        int digit;
-        double tolerance = .00001;
-
-        while (num > 0 + tolerance)
-        {
-            double weight = std::pow(10.0, m);
-            digit = floor(num / weight);
-            num -= (digit * weight);
-            *(fstr++) = '0' + digit;
-            if (m == 0)
-                *(fstr++) = '.';
-            m--;
-        }
-        *(fstr) = '\0';
-        std::cerr << "FFFFF: " << fstrbuf << std::endl;
-
         {
 		Pt::Char buf1[1024];
 		const Pt::Char* res = double_to_string(buf1, 1024, 123.1415);
 		Pt::String str1(res);
-		std::cerr << "R1: " << str1.narrow() << std::endl;
+		std::cerr << "123.1415 -> " << str1.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}
 		
@@ -261,7 +229,7 @@ struct Tester
 		Pt::Char buf2[1024];
 		const Pt::Char* res1 = double_to_string(buf2, 1024, 0.123);
 		Pt::String str2(res1);
-		std::cerr << "R2: " << str2.narrow() << std::endl;
+		std::cerr << "0.123 -> " << str2.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}
 		
@@ -269,24 +237,31 @@ struct Tester
 		Pt::Char buf3[1024];
 		const Pt::Char* res3 = double_to_string(buf3, 1024, 0.0);
 		Pt::String str3(res3);
-		std::cerr << "R3: " << str3.narrow() << std::endl;
+		std::cerr << "0.0 -> " << str3.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}
-		
+
 		{
 		Pt::Char buf3[1024];
-		const Pt::Char* res3 = double_to_string(buf3, 1024, 0.00001);
+		const Pt::Char* res3 = double_to_string(buf3, 1024, 0.00002);
 		Pt::String str3(res3);
-		std::cerr << "R4: " << str3.narrow() << std::endl;
+		std::cerr << "0.00002 -> " << str3.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}
-		
-		
+
+		{
+		Pt::Char buf3[1024];
+		const Pt::Char* res3 = double_to_string(buf3, 1024, -0.00002);
+		Pt::String str3(res3);
+		std::cerr << "-0.00002 -> " << str3.narrow() << std::endl;
+		std::cerr << "--------------------" << std::endl;
+		}	
+
 		{
 		Pt::Char buf3[1024];
 		const Pt::Char* res3 = double_to_string(buf3, 1024, 1.0);
 		Pt::String str3(res3);
-		std::cerr << "R5: " << str3.narrow() << std::endl;
+		std::cerr << "1.0 -> " << str3.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}
 		
@@ -294,7 +269,7 @@ struct Tester
 		Pt::Char buf3[1024];
 		const Pt::Char* res3 = double_to_string(buf3, 1024, 10000.0);
 		Pt::String str3(res3);
-		std::cerr << "R6: " << str3.narrow() << std::endl;
+		std::cerr << "10000.0 -> " << str3.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}
 		
@@ -302,7 +277,7 @@ struct Tester
 		Pt::Char buf3[1024];
 		const Pt::Char* res3 = double_to_string(buf3, 1024, -123.321);
 		Pt::String str3(res3);
-		std::cerr << "R6: " << str3.narrow() << std::endl;
+		std::cerr << "-123.321 -> " << str3.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}
 
@@ -310,7 +285,7 @@ struct Tester
 		Pt::Char buf3[1024];
 		const Pt::Char* res3 = double_to_string(buf3, 1024, std::numeric_limits<double>::quiet_NaN());
 		Pt::String str3(res3);
-		std::cerr << "R6: " << str3.narrow() << std::endl;
+		std::cerr << "nan -> " << str3.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}
 
@@ -318,15 +293,14 @@ struct Tester
 		Pt::Char buf3[1024];
 		const Pt::Char* res3 = double_to_string(buf3, 1024, std::numeric_limits<double>::infinity());
 		Pt::String str3(res3);
-		std::cerr << "R6: " << str3.narrow() << std::endl;
+		std::cerr << "inf -> " << str3.narrow() << std::endl;
 		std::cerr << "--------------------" << std::endl;
 		}	
-		
+
 		std::exit(1);
 	}
 } g_tester;
 */
-
 }
 
 namespace Pt {
