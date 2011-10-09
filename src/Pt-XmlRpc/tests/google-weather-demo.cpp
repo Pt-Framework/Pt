a80f3d4205e3280b7cf06b59e7665f646f35df36
d-marc <d-marc@private.net>
2011-10-09 14:45:38 +0000

    use worker thread

--- src/Pt-XmlRpc/tests/google-weather-demo.cpp
+++ src/Pt-XmlRpc/tests/google-weather-demo.cpp
@@ -1,8 +1,12 @@
 #include <Pt/Http/Client.h>
 #include <Pt/Http/Request.h>
 #include <Pt/Http/Reply.h>
+#include <Pt/System/Application.h>
 #include <Pt/System/MainLoop.h>
 #include <Pt/System/IOError.h>
+#include <Pt/System/Thread.h>
+#include <Pt/System/Timer.h>
+#include <Pt/System/EventSink.h>
 #include <Pt/Xml/XmlReader.h>
 #include <Pt/Xml/StartElement.h>
 #include <Pt/Xml/EndElement.h>
@@ -10,23 +14,64 @@
 #include <Pt/Connectable.h>
 #include <Pt/TextStream.h>
 #include <Pt/Utf8Codec.h>
+#include <Pt/Event.h>
 #include <iostream>
 
-struct WeatherData
+class WeatherEvent : public Pt::BasicEvent<WeatherEvent>
 {
-    Pt::String temperature;
-    Pt::String condition;
+    public:
+        WeatherEvent(const std::string& city)
+        : _city(city)
+        , _error(false)
+        { }
+
+        const std::string& city() const
+        { return _city; }
+
+        const Pt::String& temperature() const
+        { return _temp; }
+
+        void setTemperature(const Pt::String& temp)
+        { _temp = temp; }
+
+        const Pt::String& condition() const
+        { return _condition; }
+
+        void setCondition(const Pt::String& cond)
+        { _condition = cond; }
+
+        bool failed() const
+        { return _error; }
+
+        void setFailed(bool hasFailed)
+        { _error = hasFailed; }
+
+        void clear()
+        { 
+            _temp.clear(); 
+            _condition.clear(); 
+            _error = false;
+        }
+
+    private:
+        std::string _city;
+        Pt::String _temp;
+        Pt::String _condition;
+        bool _error;
 };
+
 
 class GoogleWeatherClient : public Pt::Connectable
 {
     public:
-        GoogleWeatherClient(Pt::System::MainLoop& loop)
-        : _loop(loop)
-        , _client(loop, "www.google.com", 80)
+        GoogleWeatherClient(const std::string& city)
+        : _thread(0)
+        , _client(_loop, "www.google.com", 80)
+        , _sink(0)
         , _ts(new Pt::Utf8Codec)
         , _reader(_ts)
         , _parseFunc(&GoogleWeatherClient::onXmlBegin)
+        , _weather(city)
         {
             // hook up to the events of the http client. We will receive headerReceived first
             // then bodyAvailable if the reply has a body, and finally replyFinished. All Errors
@@ -34,16 +79,49 @@ class GoogleWeatherClient : public Pt::Connectable
             _client.headerReceived += Pt::slot(*this,  &GoogleWeatherClient::onHeaderReceived);
             _client.bodyAvailable += Pt::slot( *this, &GoogleWeatherClient::onBodyAvailable);
             _client.replyFinished += Pt::slot( *this, &GoogleWeatherClient::onReplyFinished);
+
+            // run member function in a worker thread
+            _thread = new Pt::System::AttachedThread( Pt::callable(*this, &GoogleWeatherClient::run) );
         }
 
-        void beginRequest(const std::string& city)
+        ~GoogleWeatherClient()
         {
+            _loop.exit();
+            delete _thread;
+        }
+
+        void start(Pt::System::EventSink& sink)
+        {
+            _thread->start();
+            _sink = &sink;
+        }
+
+        void exit()
+        {
+            _loop.exit();
+            _thread->join();
+        }
+
+    private:
+        void run()
+        {
+            _timer.timeout += Pt::slot(*this, &GoogleWeatherClient::beginRequest);
+            _timer.start(2000);
+            _loop.add(_timer);
+
+            this->beginRequest();
+            _loop.run();
+        }
+
+        void beginRequest()
+        {
+            _client.cancel();
+            _weather.clear();
+
             _parseFunc = &GoogleWeatherClient::onXmlBegin;
-            _weather.condition.clear();
-            _weather.temperature.clear();
 
             _req.clear();
-            _req.url("/ig/api?weather=" + city);
+            _req.url("/ig/api?weather=" + _weather.city());
             _req.method("GET");
             _req.setHeader("Host", "www.google.com");
             _req.setHeader("Accept-Charset", "UTF-8");
@@ -51,7 +129,6 @@ class GoogleWeatherClient : public Pt::Connectable
             _client.beginExecute(_req);
         }
 
-    private:  
         void onHeaderReceived(Pt::Http::Client& client)
         {
             if(client.header().httpReturnCode() != 200)
@@ -100,7 +177,9 @@ class GoogleWeatherClient : public Pt::Connectable
             {
                 std::cerr << error.what() << std::endl;
                 _client.cancel();
-                _loop.exit();
+                _weather.setFailed(true);
+                if(_sink)
+                    _sink->commitEvent(_weather);
             }
 
             return n;
@@ -124,11 +203,11 @@ class GoogleWeatherClient : public Pt::Connectable
             {
                 std::cerr << "i/o error: " << e.what() << std::endl;
                 client.cancel();
+                _weather.setFailed(true);
             }
 
-            std::cerr << "Temperture: " << _weather.temperature.narrow() << std::endl;
-            std::cerr << "Condition: " << _weather.condition.narrow() << std::endl;
-            _loop.exit();
+            if(_sink)
+                _sink->commitEvent(_weather);
         }
 
     private:
@@ -182,9 +261,9 @@ class GoogleWeatherClient : public Pt::Connectable
             {
                 const Pt::Xml::StartElement& se = static_cast<const Pt::Xml::StartElement&>(node);
                 if(se.name() == L"temp_c")
-                    _weather.temperature = se.attribute(L"data");
+                    _weather.setTemperature( se.attribute(L"data") );
                 else if(se.name() == L"condition")
-                    _weather.condition = se.attribute(L"data");
+                    _weather.setCondition( se.attribute(L"data") );
             }
 
             if( node.type() == Pt::Xml::Node::EndElement)
@@ -196,14 +275,39 @@ class GoogleWeatherClient : public Pt::Connectable
         }
 
     private:
-        Pt::System::EventLoop& _loop;
+        Pt::System::MainLoop _loop;
+        Pt::System::AttachedThread* _thread;
+        Pt::System::Timer _timer;
         Pt::Http::Client _client;
+        Pt::System::EventSink* _sink;
         Pt::Http::Request _req;
         Pt::TextIStream _ts;
         Pt::Xml::XmlReader _reader;
         typedef void (GoogleWeatherClient::*ParseFunc)(const Pt::Xml::Node&);
         ParseFunc _parseFunc;
-        WeatherData _weather;
+        WeatherEvent _weather;
+};
+
+
+class WeatherApplet : public Pt::System::Application
+{
+    public:
+        WeatherApplet()
+        : _client("Berlin")
+        {
+            _client.start( this->loop() );
+            this->loop().event.subscribe( Pt::slot(*this, &WeatherApplet::printWeatherInfo) );
+        }
+
+    private:
+        void printWeatherInfo(const WeatherEvent& wev)
+        {
+            std::cerr << "Temperture: " << wev.temperature().narrow() << std::endl;
+            std::cerr << "Condition: " << wev.condition().narrow() << std::endl;
+        }
+
+    private:
+        GoogleWeatherClient _client;
 };
 
 
@@ -211,10 +315,8 @@ int main(int argc, char** argv)
 {
     try 
     {
-        Pt::System::MainLoop loop;
-        GoogleWeatherClient client(loop);
-        client.beginRequest("Berlin");
-        loop.run();
+        WeatherApplet app;
+        app.run();
         return 0;
     }
     catch(const std::exception& e)
