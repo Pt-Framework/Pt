56c4467d3c41fca3709c13e1092b28abda9949ea
d-marc <d-marc@private.net>
2012-01-19 13:33:07 +0000

    added InvalidUri exception

--- include/Pt/System/Uri.h
+++ include/Pt/System/Uri.h
@@ -32,73 +32,88 @@ namespace Pt {
 
 namespace System {
 
-  class PT_SYSTEM_API Uri
-  {
-      bool _ipv6;
-      std::string _protocol;
-      std::string _user;
-      std::string _password;
-      std::string _host;
-      unsigned short int _port;
-      std::string _path;
-      std::string _query;
-      std::string _fragment;
-
+class PT_SYSTEM_API InvalidUri : public std::invalid_argument
+{
     public:
-      Uri()  { }
+        InvalidUri(const std::string& uri);
 
-      Uri(const std::string& str);
+        ~InvalidUri() throw()
+        {}
 
-      void protocol(const std::string& protocol)
-        { _protocol = protocol; }
+        const std::string uri() const
+        { return _uri; }
 
-      const std::string& protocol() const
-        { return _protocol; }
+    private:
+        std::string _uri;
+};
 
-      void user(const std::string& user)
-        { _user = user; }
+class PT_SYSTEM_API Uri
+{
+  bool _ipv6;
+  std::string _protocol;
+  std::string _user;
+  std::string _password;
+  std::string _host;
+  unsigned short int _port;
+  std::string _path;
+  std::string _query;
+  std::string _fragment;
 
-      const std::string& user() const
-        { return _user; }
+public:
+  Uri()  { }
 
-      void password(const std::string& password)
-        { _password = password; }
+  Uri(const std::string& str);
 
-      const std::string& password() const
-        { return _password; }
+  void protocol(const std::string& protocol)
+    { _protocol = protocol; }
 
-      void host(const std::string& host)
-        { _host = host; }
+  const std::string& protocol() const
+    { return _protocol; }
 
-      const std::string& host() const
-        { return _host; }
+  void user(const std::string& user)
+    { _user = user; }
 
-      void port(unsigned short int p)
-        { _port = p; }
+  const std::string& user() const
+    { return _user; }
 
-      unsigned short int port() const
-        { return _port; }
+  void password(const std::string& password)
+    { _password = password; }
 
-      void path(const std::string& path)
-        { _path = path; }
+  const std::string& password() const
+    { return _password; }
 
-      const std::string& path() const
-        { return _path; }
+  void host(const std::string& host)
+    { _host = host; }
 
-      void query(const std::string& query)
-        { _query = query; }
+  const std::string& host() const
+    { return _host; }
 
-      const std::string& query() const
-        { return _query; }
+  void port(unsigned short int p)
+    { _port = p; }
 
-      void fragment(const std::string& fragment)
-        { _fragment = fragment; }
+  unsigned short int port() const
+    { return _port; }
 
-      const std::string& fragment() const
-        { return _fragment; }
+  void path(const std::string& path)
+    { _path = path; }
 
-      std::string str() const;
-  };
+  const std::string& path() const
+    { return _path; }
+
+  void query(const std::string& query)
+    { _query = query; }
+
+  const std::string& query() const
+    { return _query; }
+
+  void fragment(const std::string& fragment)
+    { _fragment = fragment; }
+
+  const std::string& fragment() const
+    { return _fragment; }
+
+  std::string str() const;
+};
 
 } // namespace System
 
--- src/Pt-System/Uri.cpp
+++ src/Pt-System/Uri.cpp
@@ -94,308 +94,318 @@
 #include <sstream>
 #include <cctype>
 
-namespace Pt
+namespace Pt {
+
+namespace System {
+
+InvalidUri::InvalidUri(const std::string& uri)
+: std::invalid_argument("Invalid URI")
+, _uri(uri)
 {
-namespace System
+}
+
+
+namespace {
+
+void throwInvalid(const std::string& uri)
 {
-  namespace
+  throw InvalidUri(uri);
+}
+
+}
+
+Uri::Uri(const std::string& uri)
+: _ipv6(false),
+  _port(0)
+{
+enum {
+  state_0,
+  state_protocol,
+  state_postprotocol,
+  state_postprotocol2,
+  state_postprotocol3,
+  state_user_or_host,
+  state_password_or_port,
+  state_password,
+  state_host,
+  state_ipv6,
+  state_ipv6ok,
+  state_ipv6end,
+  state_port,
+  state_path,
+  state_query,
+  state_fragment
+} state = state_0;
+
+std::string token;
+bool hasPort = false;
+
+for (std::string::const_iterator it = uri.begin(); it != uri.end(); ++it)
+{
+  char ch = *it;
+  switch (state)
   {
-    void throwInvalid(const std::string& uri)
-    {
-      throw std::runtime_error("invalid uri <" + uri + '>');
-    }
-  }
-
-  Uri::Uri(const std::string& uri)
-    : _ipv6(false),
-      _port(0)
-  {
-    enum {
-      state_0,
-      state_protocol,
-      state_postprotocol,
-      state_postprotocol2,
-      state_postprotocol3,
-      state_user_or_host,
-      state_password_or_port,
-      state_password,
-      state_host,
-      state_ipv6,
-      state_ipv6ok,
-      state_ipv6end,
-      state_port,
-      state_path,
-      state_query,
-      state_fragment
-    } state = state_0;
-
-    std::string token;
-    bool hasPort = false;
-
-    for (std::string::const_iterator it = uri.begin(); it != uri.end(); ++it)
-    {
-      char ch = *it;
-      switch (state)
+    case state_0:
+      if (std::isalpha(ch))
       {
-        case state_0:
-          if (std::isalpha(ch))
-          {
-            _protocol = ch;
-            state = state_protocol;
-          }
-          else if (!std::isspace(ch))
-            throwInvalid(uri);
-          break;
-
-        case state_protocol:
-          if (std::isalpha(ch))
-            _protocol += ch;
-          else if (ch == ':')
-            state = state_postprotocol;
-          else
-            throwInvalid(uri);
-          break;
-
-        case state_postprotocol:
-          if (ch == '/')
-            state = state_postprotocol2;
-          else
-            throwInvalid(uri);
-          break;
-
-        case state_postprotocol2:
-          if (ch == '/')
-            state = state_postprotocol3;
-          else
-            throwInvalid(uri);
-          break;
-
-        case state_postprotocol3:
-          if (ch == '[')
-          {
-            _ipv6 = true;
-            state = state_ipv6;
-          }
-          else if (ch == '/')
-          {
-            if(_protocol != "file") // TODO: the third slash is not part of the url-path
-              _path = ch;
-            state = state_path;
-          }
-          else
-          {
-            _user = ch;
-            state = state_user_or_host;
-          }
-          break;
-
-        case state_user_or_host:
-          if (ch == ':')
-            state = state_password_or_port;
-          else if (ch == '/')
-          {
-            _host = _user;
-            _user.clear();
-            _path = ch;
-            state = state_path;
-          }
-          else if (ch == '@')
-            state = state_host;
-          else
-            _user += ch;
-          break;
-
-        case state_password_or_port:
-          if (ch == '@')
-          {
-            _port = 0;
-            state = state_host;
-          }
-          else if (ch == '/')
-          {
-            _host = _user;
-            _user.clear();
-            _password.clear();
-            _path = ch;
-            state = state_path;
-          }
-          else if (std::isdigit(ch))
-          {
-            hasPort = true;
-            _password += ch;
-            _port = _port * 10 + ch - '0';
-          }
-          else
-          {
-            _port = 0;
-            hasPort = false;
-            _password += ch;
-            state = state_password;
-          }
-          break;
-
-        case state_password:
-          if (ch == '@')
-            state = state_host;
-          else
-            _password += ch;
-          break;
-
-        case state_host:
-          if (ch == '/')
-          {
-            _path = ch;
-            state = state_path;
-          }
-          else if (ch == ':')
-            state = state_port;
-          else if (_host.empty() && ch == '[')
-          {
-            _ipv6 = true;
-            state = state_ipv6;
-          }
-          else
-            _host += ch;
-          break;
-
-        case state_ipv6:
-          if (ch == ':')
-          {
-            _host += ch;
-            state = state_ipv6ok;
-          }
-          else if (std::isdigit(ch)
-                || (ch >= 'a' && ch <= 'f')
-                || (ch >= 'F' && ch <= 'F'))
-            _host += ch;
-          else
-            throwInvalid(uri);
-          break;
-
-        case state_ipv6ok:
-          if (ch == ']')
-            state = state_ipv6end;
-          else if (std::isdigit(ch)
-                || (ch >= 'a' && ch <= 'f')
-                || (ch >= 'F' && ch <= 'F')
-                || ch == ':')
-            _host += ch;
-          else
-            throwInvalid(uri);
-          break;
-
-        case state_ipv6end:
-          if (ch == ':')
-          {
-            hasPort = true;
-            state = state_port;
-          }
-          else if (ch == '/')
-          {
-            _path = ch;
-            state = state_path;
-          }
-          else
-            throwInvalid(uri);
-          break;
-
-        case state_port:
-          if (ch == '/')
-          {
-            _path = ch;
-            state = state_path;
-          }
-          else if (std::isdigit(ch))
-          {
-            hasPort = true;
-            _port = _port * 10 + ch - '0';
-          }
-          else
-            throwInvalid(uri);
-          break;
-
-        case state_path:
-          if (ch == '?')
-            state = state_query;
-          else if (ch == '#')
-            state = state_fragment;
-          else
-            _path += ch;
-          break;
-
-        case state_query:
-          if (ch == '#')
-            state = state_fragment;
-          else
-            _query += ch;
-          break;
-
-        case state_fragment:
-          _fragment += ch;
-          break;
+        _protocol = ch;
+        state = state_protocol;
       }
-    }
+      else if (!std::isspace(ch))
+        throwInvalid(uri);
+      break;
 
-    switch (state)
-    {
-      case state_port:
-      case state_host:
-      case state_path:
-      case state_query:
-      case state_fragment:
-        break;
+    case state_protocol:
+      if (std::isalpha(ch))
+        _protocol += ch;
+      else if (ch == ':')
+        state = state_postprotocol;
+      else
+        throwInvalid(uri);
+      break;
 
-      case state_user_or_host:
+    case state_postprotocol:
+      if (ch == '/')
+        state = state_postprotocol2;
+      else
+        throwInvalid(uri);
+      break;
+
+    case state_postprotocol2:
+      if (ch == '/')
+        state = state_postprotocol3;
+      else
+        throwInvalid(uri);
+      break;
+
+    case state_postprotocol3:
+      if (ch == '[')
+      {
+        _ipv6 = true;
+        state = state_ipv6;
+      }
+      else if (ch == '/')
+      {
+        if(_protocol != "file") // TODO: the third slash is not part of the url-path
+          _path = ch;
+        state = state_path;
+      }
+      else
+      {
+        _user = ch;
+        state = state_user_or_host;
+      }
+      break;
+
+    case state_user_or_host:
+      if (ch == ':')
+        state = state_password_or_port;
+      else if (ch == '/')
+      {
         _host = _user;
         _user.clear();
-        break;
+        _path = ch;
+        state = state_path;
+      }
+      else if (ch == '@')
+        state = state_host;
+      else
+        _user += ch;
+      break;
 
-      default:
+    case state_password_or_port:
+      if (ch == '@')
+      {
+        _port = 0;
+        state = state_host;
+      }
+      else if (ch == '/')
+      {
+        _host = _user;
+        _user.clear();
+        _password.clear();
+        _path = ch;
+        state = state_path;
+      }
+      else if (std::isdigit(ch))
+      {
+        hasPort = true;
+        _password += ch;
+        _port = _port * 10 + ch - '0';
+      }
+      else
+      {
+        _port = 0;
+        hasPort = false;
+        _password += ch;
+        state = state_password;
+      }
+      break;
+
+    case state_password:
+      if (ch == '@')
+        state = state_host;
+      else
+        _password += ch;
+      break;
+
+    case state_host:
+      if (ch == '/')
+      {
+        _path = ch;
+        state = state_path;
+      }
+      else if (ch == ':')
+        state = state_port;
+      else if (_host.empty() && ch == '[')
+      {
+        _ipv6 = true;
+        state = state_ipv6;
+      }
+      else
+        _host += ch;
+      break;
+
+    case state_ipv6:
+      if (ch == ':')
+      {
+        _host += ch;
+        state = state_ipv6ok;
+      }
+      else if (std::isdigit(ch)
+            || (ch >= 'a' && ch <= 'f')
+            || (ch >= 'F' && ch <= 'F'))
+        _host += ch;
+      else
         throwInvalid(uri);
-    }
+      break;
 
-    if (!hasPort)
-    {
-      if (_protocol == "http")
-        _port = 80;
-      else if (_protocol == "https")
-        _port = 443;
-      else if (_protocol == "ftp")
-        _port = 21;
-    }
+    case state_ipv6ok:
+      if (ch == ']')
+        state = state_ipv6end;
+      else if (std::isdigit(ch)
+            || (ch >= 'a' && ch <= 'f')
+            || (ch >= 'F' && ch <= 'F')
+            || ch == ':')
+        _host += ch;
+      else
+        throwInvalid(uri);
+      break;
+
+    case state_ipv6end:
+      if (ch == ':')
+      {
+        hasPort = true;
+        state = state_port;
+      }
+      else if (ch == '/')
+      {
+        _path = ch;
+        state = state_path;
+      }
+      else
+        throwInvalid(uri);
+      break;
+
+    case state_port:
+      if (ch == '/')
+      {
+        _path = ch;
+        state = state_path;
+      }
+      else if (std::isdigit(ch))
+      {
+        hasPort = true;
+        _port = _port * 10 + ch - '0';
+      }
+      else
+        throwInvalid(uri);
+      break;
+
+    case state_path:
+      if (ch == '?')
+        state = state_query;
+      else if (ch == '#')
+        state = state_fragment;
+      else
+        _path += ch;
+      break;
+
+    case state_query:
+      if (ch == '#')
+        state = state_fragment;
+      else
+        _query += ch;
+      break;
+
+    case state_fragment:
+      _fragment += ch;
+      break;
   }
+}
 
-  std::string Uri::str() const
-  {
-    std::ostringstream s;
-    s << _protocol << "://";
-    if (!_user.empty() || !_password.empty())
-    {
-      s << _user;
-      if (!_password.empty())
-        s << ':' << _password;
-      s << '@';
-    }
+switch (state)
+{
+  case state_port:
+  case state_host:
+  case state_path:
+  case state_query:
+  case state_fragment:
+    break;
 
-    if (_ipv6)
-      s << '[' << _host << ']';
-    else
-      s << _host;
+  case state_user_or_host:
+    _host = _user;
+    _user.clear();
+    break;
 
-    if (!(_port == 0
-       || (_protocol == "http"  && _port == 80)
-       || (_protocol == "https" && _port == 443)
-       || (_protocol == "ftp"   && _port == 21)))
-      s << ':' << _port;
+  default:
+    throwInvalid(uri);
+}
 
-    s << _path;
-    if (!_query.empty())
-      s << '?' << _query;
-    if (!_fragment.empty())
-      s << '#' << _fragment;
-
-    return s.str();
-  }
-
+if (!hasPort)
+{
+  if (_protocol == "http")
+    _port = 80;
+  else if (_protocol == "https")
+    _port = 443;
+  else if (_protocol == "ftp")
+    _port = 21;
 }
 }
+
+std::string Uri::str() const
+{
+std::ostringstream s;
+s << _protocol << "://";
+if (!_user.empty() || !_password.empty())
+{
+  s << _user;
+  if (!_password.empty())
+    s << ':' << _password;
+  s << '@';
+}
+
+if (_ipv6)
+  s << '[' << _host << ']';
+else
+  s << _host;
+
+if (!(_port == 0
+   || (_protocol == "http"  && _port == 80)
+   || (_protocol == "https" && _port == 443)
+   || (_protocol == "ftp"   && _port == 21)))
+  s << ':' << _port;
+
+s << _path;
+if (!_query.empty())
+  s << '?' << _query;
+if (!_fragment.empty())
+  s << '#' << _fragment;
+
+return s.str();
+}
+
+}
+
+}
+
