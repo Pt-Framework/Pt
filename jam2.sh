3d3c365ce4ed5d3212216e7e7b4c73d6acafc799
d-marc <d-marc@private.net>
2012-02-03 08:47:09 +0000

    updated build script

--- jam2.sh
+++ jam2.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-JAM_SOURCES="./command.c ./compile.c ./debug.c ./execunix.c ./expand.c ./fileunix.c ./glob.c ./hash.c ./hdrmacro.c ./headers.c ./jam.c ./jambase.c ./jamgram.c ./lists.c ./make.c ./make1.c ./newstr.c ./option.c ./parse.c ./pathunix.c ./pathvms.c ./regexp.c ./rules.c ./scan.c ./search.c ./subst.c ./timestamp.c ./variable.c ./modules.c ./strings.c ./filesys.c ./builtins.c ./pwd.c ./class.c ./w32_getreg.c ./native.c ./modules/set.c ./modules/path.c ./modules/regex.c ./modules/property-set.c ./modules/sequence.c ./modules/order.c"
+JAM_SOURCES="./builtins.c ./class.c ./command.c ./compile.c ./constants.c ./debug.c ./execunix.c ./expand.c ./fileunix.c ./filesys.c ./function.c ./glob.c ./hash.c ./hcache.c ./hdrmacro.c ./headers.c ./jam.c ./jambase.c ./jamgram.c ./lists.c ./make.c ./make1.c ./md5.c ./mem.c ./modules.c ./native.c ./object.c ./option.c ./output.c ./parse.c ./pathunix.c ./pwd.c ./regexp.c ./rules.c ./scan.c ./search.c ./strings.c ./subst.c ./timestamp.c ./variable.c ./w32_getreg.c ./modules/set.c ./modules/path.c ./modules/regex.c ./modules/property-set.c ./modules/sequence.c ./modules/order.c"
+
+cc -o ../jam.bin -DPT_MCHECK $JAM_SOURCES
 
 if test ! -f ./jam2.bin; then
     echo "Building jam executable..."
@@ -10,6 +12,6 @@ if test ! -f ./jam2.bin; then
     echo "Built jam2.bin"
 fi
 
-./jam2.bin $*
+./jam2.bin -fJambase1 $*
 
 
