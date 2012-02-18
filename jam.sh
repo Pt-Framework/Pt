8d039d1badeff246e0b3b466193aa3ca76a18e36
d-marc <d-marc@private.net>
2012-02-18 11:37:13 +0000

    jam updates

---
+++ jam.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+JAM_SOURCES="./builtins.c ./class.c ./command.c ./compile.c ./constants.c ./debug.c ./execunix.c ./expand.c ./fileunix.c ./filesys.c ./function.c ./glob.c ./hash.c ./hcache.c ./hdrmacro.c ./headers.c ./jam.c ./jambase.c ./jamgram.c ./lists.c ./make.c ./make1.c ./md5.c ./mem.c ./modules.c ./native.c ./object.c ./option.c ./output.c ./parse.c ./pathunix.c ./pwd.c ./regexp.c ./rules.c ./scan.c ./search.c ./strings.c ./subst.c ./timestamp.c ./variable.c ./w32_getreg.c ./modules/set.c ./modules/path.c ./modules/regex.c ./modules/property-set.c ./modules/sequence.c ./modules/order.c"
+
+#use -DPT_MCHECK for memory usage statistics
+
+if test ! -f ./jam.bin; then
+    echo "Building jam executable..."
+    cd jam/src
+    cc -o ../../jam.bin $JAM_SOURCES
+    cd ../..
+    echo "Built jam.bin"
+fi
+
+./jam.bin -fJambase $*
+
+
