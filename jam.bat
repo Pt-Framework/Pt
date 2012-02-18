7e4f5f862123bf38004e945e31ca326dca2d3368
d-marc <d-marc@private.net>
2012-02-18 11:11:56 +0000

    jam updates

--- jam.bat
+++ jam.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-if not exist jam2.exe (
-    echo "jam2.exe not found"
+if not exist jam.exe (
+    echo "jam.exe not found"
 
     cl.exe
     if %errorlevel% neq 0 (
@@ -11,6 +11,6 @@ if not exist jam2.exe (
     goto :eof
 )
 
-call .\jam2.exe -fJambase1 %*
+call .\jam.exe -fJambase %*
 goto :eof
 
