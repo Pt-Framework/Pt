3a5182804318e627e41eae01d2ab713f70d23804
d-marc <d-marc@private.net>
2012-09-15 09:12:27 +0000

    static member not inline anymore, gcc had a problem

--- src/Pt-Http/tests/ServerTest.cpp
+++ src/Pt-Http/tests/ServerTest.cpp
@@ -109,7 +109,7 @@ class ServerTest : public Pt::Unit::TestSuite
         ServerTest()
         : Pt::Unit::TestSuite("ServerTest")
         {
-            Pt::System::Logger::setLogLevel("Pt", Pt::System::Trace);
+            Pt::System::Logger::setLogLevel("Pt.Http", Pt::System::Error);
 
             this->registerMethod( "NotFound", *this, &ServerTest::NotFound);
 #ifdef PT_HTTP_WITH_SSL
