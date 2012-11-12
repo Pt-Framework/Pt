158260ca1608842dc02faa40565c3adf897c9eca
d-marc <d-marc@private.net>
2012-11-12 14:06:46 +0000

    updated jam binary

--- jam.sh
+++ jam.sh
@@ -2,7 +2,7 @@
 
 #TODO set JAMDIR to jam
 
-JAM_SOURCES="./builtins.c ./class.c ./command.c ./compile.c ./constants.c ./debug.c ./execunix.c ./expand.c ./fileunix.c ./filesys.c ./function.c ./glob.c ./hash.c ./hcache.c ./hdrmacro.c ./headers.c ./jam.c ./jambase.c ./jamgram.c ./lists.c ./make.c ./make1.c ./md5.c ./mem.c ./modules.c ./native.c ./object.c ./option.c ./output.c ./parse.c ./pathunix.c ./pwd.c ./regexp.c ./rules.c ./scan.c ./search.c ./strings.c ./subst.c ./timestamp.c ./variable.c ./w32_getreg.c ./modules/set.c ./modules/path.c ./modules/regex.c ./modules/property-set.c ./modules/sequence.c ./modules/order.c"
+JAM_SOURCES="./builtins.c ./class.c ./command.c ./compile.c ./constants.c ./debug.c ./execcmd.c ./execunix.c ./fileunix.c ./filesys.c ./function.c ./frames.c ./glob.c ./hash.c ./hcache.c ./hdrmacro.c ./headers.c ./jam.c ./jambase.c ./jamgram.c ./lists.c ./make.c ./make1.c ./md5.c ./mem.c ./modules.c ./native.c ./object.c ./option.c ./output.c ./parse.c ./pathunix.c ./pathsys.c ./cwd.c ./regexp.c ./rules.c ./scan.c ./search.c ./strings.c ./subst.c ./timestamp.c ./variable.c ./w32_getreg.c ./modules/set.c ./modules/path.c ./modules/regex.c ./modules/property-set.c ./modules/sequence.c ./modules/order.c"
 
 #use -DPT_MCHECK for memory usage statistics
 
