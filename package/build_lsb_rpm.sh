b396997c6c7640c12889f1f40055e45b1b5fc91c
blue_wind_25 <blue_wind_25@private.net>
2014-01-17 23:40:44 +0000

    Fixing the script for generating LSB RPMs

--- package/build_lsb_rpm.sh
+++ package/build_lsb_rpm.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# Executable for making LSB RPM
+# Location of the executable for making LSB RPMs
+EXEC_FK_ROOT='/opt/lsb/bin/fakeroot'
 EXEC_GEN_RPM='/opt/lsb/bin/makelsbpkg'
 
-# Final installation directories
+# Final installation directories; modify them as needed
 INST_INC='/opt/lsbpt/include/'
 INST_LIB='/opt/lsbpt/lib/'
+
+# Increment this if for some reason the we need to rebuild the RPM
+# for the same version of Pt; otherwise, it should be set to '1'
+RELEASE='1'
+
+# Check if the tree has been configure for LSB build
+LSBCC=`cat Jamrules | grep lsbcc`
+LSBCP=`cat Jamrules | grep lsbc++`
+if [[ "x$LSBCC" == 'x' && "x$LSBCP" == 'x' ]]; then
+    echo 'The tree was not configured for LSB build!'
+    exit 1
+fi;
 
 # Get the output directory
 # Modified from http://www.shelldorado.com/goodcoding/cmdargs.html
@@ -30,7 +43,7 @@ if [[ `pwd` == *package ]]; then
     cd ..
 fi
 
-# Determine the installation directory under ('deploy') and the additional package name
+# Determine the installation directory (under 'deploy') and the additional package name
 ADDPN=''
 SSDIR='release'
 RSDIR='deploy/release/'
@@ -43,6 +56,13 @@ fi
 
 # Determine the architecture
 ARCH=`cat Jamrules | grep TARGET_OSPLAT | awk '{print $3}' | awk -F'"' '{print $2}'`
+
+# Get the version of Pt
+V1=`cat include/Pt/Api.h`
+V1=`cat include/Pt/Api.h | grep PT_VERSION_MAJOR | awk '{print $3}'`
+V2=`cat include/Pt/Api.h | grep PT_VERSION_MINOR | awk '{print $3}'`
+V3=`cat include/Pt/Api.h | grep PT_VERSION_REVISION | awk '{print $3}'`
+VERSION="$V1.$V2.$V3"
 
 # Invoke the install command
 mkdir -p $RSDIR
@@ -71,24 +91,24 @@ DESC=$'Platinum (Pt) is a comprehensive C++ framework, which allows developers t
 
 rm -f *.rpm
 
-$EXEC_GEN_RPM                           \
+$EXEC_FK_ROOT $EXEC_GEN_RPM             \
     pt-1.0.0$ADDPN-$ARCH $TMP_BIN       \
     --verbose                           \
     --license     'LGPL'                \
-    --version     '1.0.0'               \
-    --release     '1'                   \
+    --version     "$VERSION"            \
+    --release     "$RELEASE"            \
     --group       'C++ Framework'       \
     --package     'Pt Development Team' \
     --vendor      'Pt Development Team' \
     --summary     "$SBIN"               \
     --description "$DESC"
 
-$EXEC_GEN_RPM                           \
+$EXEC_FK_ROOT $EXEC_GEN_RPM             \
     pt-1.0.0$ADDPN-devel-$ARCH $TMP_DEV \
     --verbose                           \
     --license     'LGPL'                \
-    --version     '1.0.0'               \
-    --release     '1'                   \
+    --version     "$VERSION"            \
+    --release     "$RELEASE"            \
     --group       'C++ Framework'       \
     --package     'Pt Development Team' \
     --vendor      'Pt Development Team' \
