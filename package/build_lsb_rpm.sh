f99ce1cd24fdab65f32864d4cb630728f0e4e4d7
d-marc <d-marc@private.net>
2014-01-19 13:52:16 +0000

    minor changes to lsb rpm build script

--- package/build_lsb_rpm.sh
+++ package/build_lsb_rpm.sh
@@ -4,10 +4,6 @@
 EXEC_FK_ROOT='/opt/lsb/bin/fakeroot'
 EXEC_GEN_RPM='/opt/lsb/bin/makelsbpkg'
 
-# Final installation directories; modify them as needed
-INST_INC='/opt/lsbpt/include/'
-INST_LIB='/opt/lsbpt/lib/'
-
 # Increment this if for some reason the we need to rebuild the RPM
 # for the same version of Pt; otherwise, it should be set to '1'
 RELEASE='1'
@@ -22,7 +18,7 @@ fi;
 
 # Get the output directory
 # Modified from http://www.shelldorado.com/goodcoding/cmdargs.html
-OUTDIR=
+OUTDIR='package'
 while [ $# -gt 0 ]
 do
     case "$1" in
@@ -34,22 +30,17 @@ do
     esac
     shift
 done
-if [[ "x$OUTDIR" == 'x' ]]; then
-    OUTDIR='package'
-fi
 
-# Go up to the parent directory as needed
+# Go up to the parent directory if needed
 if [[ `pwd` == *package ]]; then
     cd ..
 fi
 
 # Determine the installation directory (under 'deploy') and the additional package name
 ADDPN=''
-SSDIR='release'
 RSDIR='deploy/release/'
 DEBUG=`cat Jamrules | grep build/debug`
 if [[ "x$DEBUG" != 'x' ]]; then
-    SSDIR='debug'
     RSDIR='deploy/debug/'
     ADDPN='-debug'
 fi
@@ -58,7 +49,6 @@ fi
 ARCH=`cat Jamrules | grep TARGET_OSPLAT | awk '{print $3}' | awk -F'"' '{print $2}'`
 
 # Get the version of Pt
-V1=`cat include/Pt/Api.h`
 V1=`cat include/Pt/Api.h | grep PT_VERSION_MAJOR | awk '{print $3}'`
 V2=`cat include/Pt/Api.h | grep PT_VERSION_MINOR | awk '{print $3}'`
 V3=`cat include/Pt/Api.h | grep PT_VERSION_REVISION | awk '{print $3}'`
@@ -68,13 +58,14 @@ VERSION="$V1.$V2.$V3"
 mkdir -p $RSDIR
 ./jam.sh install -sPT_INSTALL_LIBDIR=$RSDIR/lib -sPT_INSTALL_INCLUDEDIR=$RSDIR/include
 
-# Ensure all shared libraries have executable permission
-cd $RSDIR
-chmod a+x lib/*
+# Prepare the package directory structure
+INST_INC='/opt/Pt-'$VERSION'/include/'
+INST_LIB='/opt/Pt-'$VERSION'/lib/'
 
-# Mimic the final installation directory structure
-TMP_BIN='tmpb'
-TMP_DEV='tmpd'
+chmod a+x $RSDIR/lib/*
+
+TMP_BIN=$RSDIR/tmpb
+TMP_DEV=$RSDIR/tmpd
 
 rm -rvf $TMP_BIN
 mkdir -p $TMP_BIN$INST_LIB
@@ -94,7 +85,7 @@ rm -f *.rpm
 $EXEC_FK_ROOT $EXEC_GEN_RPM             \
     pt-1.0.0$ADDPN-$ARCH $TMP_BIN       \
     --verbose                           \
-    --license     'LGPL'                \
+    --license     'extended LGPL'                \
     --version     "$VERSION"            \
     --release     "$RELEASE"            \
     --group       'C++ Framework'       \
@@ -106,7 +97,7 @@ $EXEC_FK_ROOT $EXEC_GEN_RPM             \
 $EXEC_FK_ROOT $EXEC_GEN_RPM             \
     pt-1.0.0$ADDPN-devel-$ARCH $TMP_DEV \
     --verbose                           \
-    --license     'LGPL'                \
+    --license     'extended LGPL'                \
     --version     "$VERSION"            \
     --release     "$RELEASE"            \
     --group       'C++ Framework'       \
@@ -115,12 +106,12 @@ $EXEC_FK_ROOT $EXEC_GEN_RPM             \
     --summary     "$SDEV"               \
     --description "$DESC"
 
+# Move RPM packages to output directoy
 if [[ $OUTDIR == /* ]]; then
     mv *.rpm $OUTDIR
 else
     mv *.rpm ../../$OUTDIR
 fi
 
-# Clean-ups
-cd ..
-rm -rvf $SSDIR
+# Clean up
+rm -rvf $RSDIR
