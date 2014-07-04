f10ed75e8a7d8390b0681e0597e3a2f2da9277f7
d-marc <d-marc@private.net>
2014-07-04 14:31:07 +0000

    fixed showmanyc() of TextBuffer

--- include/Pt/TextBuffer.h
+++ include/Pt/TextBuffer.h
@@ -347,8 +347,15 @@ class BasicTextBuffer : public BasicStreamBuffer<CharT>
         // inheritdoc
         virtual std::streamsize showmanyc()
         {
-            return _target && _target->rdbuf() ? _target->rdbuf()->in_avail() 
-                                               : -1;
+            //return _target && _target->rdbuf() ? _target->rdbuf()->in_avail() 
+            //                                   : -1;
+
+            // Return 0, because we can not predict how many characters
+            // can be decoded from the available external byte sequence.
+            // Also, if we returned a number > 0, the next call to underflow()
+            // must exactly read this number of bytes without blocking.
+
+            return _target && _target->rdbuf() ? 0 : -1;
         }
 
         // inheritdoc
