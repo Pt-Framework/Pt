0fe8c10898c57053faa0ccaac12c9be6b30b3052
l-crisan <l-crisan@private.net>
2015-09-15 18:11:37 +0000

    HMI: Render path experiment.

---
+++ include/Pt/Ui/FillEllipseOp.h
@@ -0,0 +1,66 @@
+ /* Copyright (C) 2015 Laurentiu-Gheorghe Crisan
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  
+  As a special exception, you may use this file as part of a free
+  software library without restriction. Specifically, if other files
+  instantiate templates or use macros or inline functions from this
+  file, or you compile this file and link it with other files to
+  produce an executable, this file does not by itself cause the
+  resulting executable to be covered by the GNU General Public
+  License. This exception does not however invalidate any other
+  reasons why the executable file might be covered by the GNU Library
+  General Public License.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA*/
+#ifndef PT_UI_FILLELLIPSEOP_H
+#define PT_UI_FILLELLIPSEOP_H
+
+#include <Pt/Ui/RenderOp.h>
+#include <Pt/Ui/Brush.h>
+#include <Pt/Ui/Point.h>
+#include <Pt/Ui/Size.h>
+
+namespace Pt {
+namespace Ui {
+
+class FillEllipseOp : public RenderOp
+{  
+  public:
+    FillEllipseOp( const Ui::Brush& brush, const Ui::PointF& topLeft,  const Ui::SizeF& size)
+    : _brush(brush)
+    , _topLeft(topLeft)
+    , _size(size)
+    {
+    }
+
+    virtual void execute( Painter& painter ) const
+    {
+				painter.setBrush( _brush );
+				painter.fillEllipse( _topLeft, _size );
+    }
+
+    virtual RenderOp* clone() const
+    {
+      return new FillEllipseOp( _brush, _topLeft, _size );
+    }
+
+  private:
+     Ui::Brush    _brush;
+     Ui::PointF _topLeft;
+     Ui::SizeF  _size;
+};
+
+}}
+
+#endif
---
+++ include/Pt/Ui/ImageOp.h
@@ -0,0 +1,71 @@
+ /* Copyright (C) 2015 Laurentiu-Gheorghe Crisan
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  
+  As a special exception, you may use this file as part of a free
+  software library without restriction. Specifically, if other files
+  instantiate templates or use macros or inline functions from this
+  file, or you compile this file and link it with other files to
+  produce an executable, this file does not by itself cause the
+  resulting executable to be covered by the GNU General Public
+  License. This exception does not however invalidate any other
+  reasons why the executable file might be covered by the GNU Library
+  General Public License.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA*/
+#ifndef PT_UI_IMAGEOP_H
+#define PT_UI_IMAGEOP_H
+
+#include <Pt/Ui/RenderOp.h>
+#include <Pt/Ui/Point.h>
+#include <Pt/Ui/Image.h>
+
+namespace Pt {
+namespace Ui {
+
+class ImageOp : public RenderOp
+{  
+  public:
+    ImageOp( const Ui::Image& image, const Ui::PointF& to, const Ui::Region* pmRegion = 0 )
+    : _image(image)
+    , _to(to)     
+    {
+      if( pmRegion != 0)
+        _region = *pmRegion;
+      else
+        _region.setLeft(-1);
+    }
+
+
+    virtual void execute( Painter& painter ) const
+    {
+        painter.drawImage( _to, _image, _region );
+    }
+
+    virtual RenderOp* clone() const
+    {
+      if( _region.left() == -1)
+        return new ImageOp( _image, _to, 0 );
+      
+       return new ImageOp( _image, _to, &_region );
+    }
+   
+  private:
+     Ui::Image _image;
+     Ui::PointF _to;
+     Ui::Region _region;
+};
+
+}}
+
+#endif
