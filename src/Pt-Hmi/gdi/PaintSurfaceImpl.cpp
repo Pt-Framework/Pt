1dc902d1ec1a4f31740318626ca358249c2d67f5
d-marc <d-marc@private.net>
2016-02-25 14:17:46 +0000

    include order

--- src/Pt-Hmi/gdi/PaintSurfaceImpl.cpp
+++ src/Pt-Hmi/gdi/PaintSurfaceImpl.cpp
@@ -25,10 +25,11 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
 #include "PaintSurfaceImpl.h"
 #include "PainterImpl.h"
-#include <tchar.h>
 #include <Pt/Hmi/Application.h>
+#include <tchar.h>
 
 namespace Pt {
 
