f2a4bea96472d932b3f4a91ae215b0ed65a7813d
blue_wind_25 <blue_wind_25@private.net>
2017-02-10 07:24:22 +0000

    Experiment with fill polygon AA - not working

--- src/Pt-Gfx/Rasterizer2_Polygon.cpp
+++ src/Pt-Gfx/Rasterizer2_Polygon.cpp
@@ -80,8 +80,10 @@ void Rasterizer2::fillPolygon(const Point* points, size_t pointCount, bool useAn
     if(useAntiAliasing) {
       //rasterPolygonAreaTrueSSAA(clipped.data(), clipped.size(), _brush.color(), minX, minY, maxX, maxY);
       //rasterPolygonAreaEdgeSSAA(clipped.data(), clipped.size(), _brush.color(), minX, minY, maxX, maxY);
-        rasterPolygonAreaFastSSAA(clipped.data(), clipped.size(), _brush.color(), minX, minY, maxX, maxY);
+      //rasterPolygonAreaFastSSAA(clipped.data(), clipped.size(), _brush.color(), minX, minY, maxX, maxY);
+        rasterPolygonAreaFastAASC(clipped.data(), clipped.size(), _brush.color(), minX, minY, maxX, maxY);
     }
+
     else {
         rasterPolygonAreaFastNOAA(clipped.data(), clipped.size(), _brush.color(), minX, minY, maxX, maxY);
       //rasterPolygonAreaTrueSSAA(clipped.data(), clipped.size(), _brush.color(), minX, minY, maxX, maxY);
@@ -862,6 +864,298 @@ void Rasterizer2::rasterPolygonAreaFastSSAA(const Point* points, size_t pointCou
     #undef FSAA_MAX_ALPHA
 }
 
+// Fast Anti-Aliasing Polygon Scan Conversion
+// Jack Morrison, Graphics Gems, Academic Press, 1990
+// Dan Field, Incremental Linear Interpolation, ACM Transactions on Graphics, 1985
+// http://www.realtimerendering.com/resources/GraphicsGems/gems/AAPolyScan.c
+// http://www.realtimerendering.com/resources/GraphicsGems/category.html
+
+    #define SUBYRES   8                   // Subpixel Y resolution per scanline
+    #define SUBXRES   16                  // Subpixel X resolution per pixel
+    #define MODRES(Y) ((Y) & 7)           // Subpixel Y modulo
+    #define MAX_X     0x7FFF              // Subpixel X beyond the right edge
+    #define MAX_AREA  (SUBYRES * SUBXRES)
+
+inline void vLerp(double alpha, const Point* Va, const Point* Vb, Point* Vout)
+{
+    double ialpha = 1.0 - alpha;
+
+    Vout->setX(alpha * Va->x() + ialpha * Vb->x());
+    Vout->setY(alpha * Va->y() + ialpha * Vb->y());
+}
+    // Subpixel extents for scanline
+    struct SubPixel {
+        int xLeft;
+        int xRight;
+    } sp[SUBYRES];
+    // Subpixel X extremes for scanline
+
+    int xLmin, xLmax;
+    int xRmax, xRmin; // Optimization shortcut
+
+/* Compute bitmask indicating which subpixels are covered by
+ * polygon at current pixel. (Not all hidden-surface methods
+ * need this mask. )
+*/
+void computePixelMask(
+    int x,          /* left subpixel of pixel */
+    unsigned mask[]    /* output bitmask */
+)
+{
+    static unsigned leftMaskTable[] =
+        { 0xFFFF, 0x7FFF, 0x3FFF, 0x1FFF, 0x0FFF, 0x07FF, 0x03FF,
+          0x01FF, 0x00FF, 0x007F, 0x003F, 0x001F, 0x000F, 0x0007,
+          0x0003, 0x0001  };
+    static unsigned rightMaskTable[] =
+        { 0x8000, 0xC000, 0xE000, 0xF000, 0xF800, 0xFC00,
+          0xFE00, 0xFF00, 0xFF80, 0xFFC0, 0xFFE0, 0xFFF0,
+          0xFFF8, 0xFFFC, 0xFFFE, 0xFFFF   };
+    unsigned leftMask, rightMask;       /* partial masks */
+    int xr = x+SUBXRES-1;           /* right subpixel of pixel */
+    int y;
+
+/* shortcut for common case of fully covered pixel */
+    if (x>xLmax && x<xRmin)     {
+        for (y=0; y<SUBYRES; y++)
+            mask[y] = 0xFFFF;
+    } else  {
+        for (y=0; y<SUBYRES; y++)   {
+            if (sp[y].xLeft < x)    /* completely left of pixel*/
+                leftMask = 0xFFFF;
+            else if (sp[y].xLeft > xr)  /* completely right */
+                leftMask = 0;
+            else
+                leftMask = leftMaskTable[sp[y].xLeft -x];
+
+            if (sp[y].xRight > xr)      /* completely  */
+                            /* right of pixel*/
+                rightMask = 0xFFFF;
+            else if (sp[y].xRight < x)  /*completely left */
+                rightMask = 0;
+            else
+                rightMask = rightMaskTable[sp[y].xRight -x];
+            mask[y] = leftMask & rightMask;
+        }
+    }
+}
+
+int Coverage(int x /* left subpixel of pixel */)
+{
+    int  area;          /* total covered area */
+    int partialArea;      /* covered area for current subpixel y */
+    int xr = x+SUBXRES-1;   /*right subpixel of pixel */
+    int y;
+
+    /* shortcut for common case of fully covered pixel */
+    if (x>xLmax && x<xRmin)
+        return MAX_AREA;
+
+    for (area=y=0; y<SUBYRES; y++) {
+        partialArea = std::min(sp[y].xRight, xr)
+             - std::max(sp[y].xLeft, x) + 1;
+        if (partialArea > 0)
+            area += partialArea;
+    }
+    return area;
+}
+void Rasterizer2::renderScanline(const Point* Vl, const Point* Vr, int y)
+{
+    Point Vpixel;  /*object info interpolated at one pixel */
+    unsigned mask[SUBYRES]; /*pixel coverage bitmask */
+    int x;          /* leftmost subpixel of current pixel */
+
+    for (x=SUBXRES*floor((double)(xLmin/SUBXRES)); x<=xRmax; x+=SUBXRES) {
+        vLerp((double)(x-xLmin)/(xRmax-xLmin), Vl, Vr, &Vpixel);
+        computePixelMask(x, mask);
+
+        Pixel      dstPixel(_image->view(), x/SUBXRES, y);
+        _image->format().setPixel(dstPixel, _brush.color(), _compositionMode, 255);
+
+//extern void renderPixel(/* int x, y, Vertex *V,
+                       // int area, unsigned mask[],
+                     //   Surface *object */);
+
+        //renderPixel(, y, &Vpixel,
+                //    /*computePixel*/Coverage(x), mask, object);
+    }
+}
+
+void Rasterizer2::rasterPolygonAreaFastAASC(const Point* points, size_t pointCount, const Color& color, Pt::int32_t minX, Pt::int32_t minY, Pt::int32_t maxX, Pt::int32_t maxY)
+{
+
+
+    // Current left edge
+    const Point* Vleft;
+    const Point* VnextLeft;
+
+    // Current right edge
+    const Point* Vright;
+    const Point* VnextRight;
+
+    // End of polygon vertex list
+    const Point* endPoly;
+
+    Point VscanLeft, VscanRight;   /* interpolated vertices */                                 /* at scanline */
+    double aLeft, aRight;           /* interpolation ratios */
+    struct SubPixel *sp_ptr;        /* current subpixel info */
+    int xLeft, xNextLeft;           /* subpixel coordinates for */
+    int  xRight, xNextRight;        /* active polygon edges */
+
+    #define screenX(X) (X->x())
+#define LERP(W, B, A) ( (1.0-(W)) * (A) + (W) * (B) )
+
+
+    // Find vertex with minimum Y
+    Vleft = points;    for(int i = 1; i < pointCount; ++i) {
+        if(points[i].y() < Vleft->y()) Vleft = &points[i];
+    }
+    endPoly = &points[pointCount - 1];
+
+    // Initialize scanning edges
+    Vright = VnextRight = VnextLeft = Vleft;
+
+    // Prepare bottom of initial scanline - no coverage by polygon
+    for(int i = 0; i < SUBYRES; ++i) {
+        sp[i].xLeft = sp[i].xRight = -1;
+    }
+    xLmin = xRmin = MAX_X;
+    xLmax = xRmax = -1;
+
+    // Scan convert for each subpixel from bottom to top
+    for(int y = Vleft->y(); ; ++y) {
+        while(y == VnextLeft->y())   {           // reached next left vertex
+            VnextLeft = (Vleft = VnextLeft) + 1;  //* advance */
+            if(VnextLeft > endPoly)              //* (wraparound) */
+                VnextLeft = points;
+            if(VnextLeft == Vright)              /* all y's same?  */
+                return;                          /* (null polygon) */
+            xLeft = screenX(Vleft);
+            xNextLeft = screenX(VnextLeft);
+        }
+
+        while (y == VnextRight->y())  { /*reached next right vertex */
+            VnextRight = (Vright = VnextRight) -1;
+            if (VnextRight < points)           /* (wraparound) */
+                VnextRight = endPoly;
+            xRight = screenX(Vright);
+            xNextRight = screenX(VnextRight);
+        }
+
+        if (y>VnextLeft->y() || y>VnextRight->y())  {
+                    /* done, mark uncovered part of last scanline */
+            for (; MODRES(y); y++)
+                sp[MODRES(y)].xLeft = sp[MODRES(y)].xRight = -1;
+            renderScanline(Vleft, Vright, y/SUBYRES);
+            return;
+        }
+
+    /*
+     * Interpolate sub-pixel x endpoints at this y,
+     * and update extremes for pixel coherence optimization
+     */
+
+
+        sp_ptr = &sp[MODRES(y)];
+        aLeft = (double)(y - Vleft->y()) / (VnextLeft->y() - Vleft->y());
+        sp_ptr->xLeft = LERP(aLeft, xLeft, xNextLeft);
+        if (sp_ptr->xLeft < xLmin)
+            xLmin = sp_ptr->xLeft;
+        if (sp_ptr->xLeft > xLmax)
+            xLmax = sp_ptr->xLeft;
+
+        aRight = (double)(y - Vright->y()) / (VnextRight->y()
+                        - Vright->y());
+        sp_ptr->xRight = LERP(aRight, xRight, xNextRight);
+
+        if (sp_ptr->xRight < xRmin)
+            xRmin = sp_ptr->xRight;
+        if (sp_ptr->xRight > xRmax)
+            xRmax = sp_ptr->xRight;
+
+        if (MODRES(y) == SUBYRES-1) {   /* end of scanline */
+                /* interpolate edges to this scanline */
+            vLerp(aLeft, Vleft, VnextLeft, &VscanLeft);
+            vLerp(aRight, Vright, VnextRight, &VscanRight);
+            renderScanline(&VscanLeft, &VscanRight, y/SUBYRES);
+            xLmin = xRmin = MAX_X;      /* reset extremes */
+            xLmax = xRmax = -1;
+        }
+  }
+
+#if 0
+
+
+
+/*
+ * Compute number of subpixels covered by polygon at current pixel
+*/
+/*computePixel*/Coverage(x)
+    int x;          /* left subpixel of pixel */
+{
+    int  area;          /* total covered area */
+    int partialArea;      /* covered area for current subpixel y */
+    int xr = x+SUBXRES-1;   /*right subpixel of pixel */
+    int y;
+
+    /* shortcut for common case of fully covered pixel */
+    if (x>xLmax && x<xRmin)
+        return MAX_AREA;
+
+    for (area=y=0; y<SUBYRES; y++) {
+        partialArea = MIN(sp[y].xRight, xr)
+             - MAX(sp[y].xLeft, x) + 1;
+        if (partialArea > 0)
+            area += partialArea;
+    }
+    return area;
+}
+
+/* Compute bitmask indicating which subpixels are covered by
+ * polygon at current pixel. (Not all hidden-surface methods
+ * need this mask. )
+*/
+computePixelMask(x, mask)
+    int x;          /* left subpixel of pixel */
+    unsigned mask[];    /* output bitmask */
+{
+    static unsigned leftMaskTable[] =
+        { 0xFFFF, 0x7FFF, 0x3FFF, 0x1FFF, 0x0FFF, 0x07FF, 0x03FF,
+          0x01FF, 0x00FF, 0x007F, 0x003F, 0x001F, 0x000F, 0x0007,
+          0x0003, 0x0001  };
+    static unsigned rightMaskTable[] =
+        { 0x8000, 0xC000, 0xE000, 0xF000, 0xF800, 0xFC00,
+          0xFE00, 0xFF00, 0xFF80, 0xFFC0, 0xFFE0, 0xFFF0,
+          0xFFF8, 0xFFFC, 0xFFFE, 0xFFFF   };
+    unsigned leftMask, rightMask;       /* partial masks */
+    int xr = x+SUBXRES-1;           /* right subpixel of pixel */
+    int y;
+
+/* shortcut for common case of fully covered pixel */
+    if (x>xLmax && x<xRmin)     {
+        for (y=0; y<SUBYRES; y++)
+            mask[y] = 0xFFFF;
+    } else  {
+        for (y=0; y<SUBYRES; y++)   {
+            if (sp[y].xLeft < x)    /* completely left of pixel*/
+                leftMask = 0xFFFF;
+            else if (sp[y].xLeft > xr)  /* completely right */
+                leftMask = 0;
+            else
+                leftMask = leftMaskTable[sp[y].xLeft -x];
+
+            if (sp[y].xRight > xr)      /* completely  */
+                            /* right of pixel*/
+                rightMask = 0xFFFF;
+            else if (sp[y].xRight < x)  /*completely left */
+                rightMask = 0;
+            else
+                rightMask = rightMaskTable[sp[y].xRight -x];
+            mask[y] = leftMask & rightMask;
+        }
+    }
+}
+#endif
+}
 
 } // namespace
 } // namespace
