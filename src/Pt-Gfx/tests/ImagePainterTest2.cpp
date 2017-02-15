1c7936e2a4300fd9e0431f1b294d2877a9bc00d8
blue_wind_25 <blue_wind_25@private.net>
2017-02-15 05:54:32 +0000

    Clean-up

--- src/Pt-Gfx/tests/ImagePainterTest2.cpp
+++ src/Pt-Gfx/tests/ImagePainterTest2.cpp
@@ -39,27 +39,27 @@ using namespace Pt::Gfx;
 #define BENCHMARK_CAIRO_CHECK_RESULTING_IMAGE 0
 
 // Comparison with GD (solid-filled polygons only)
-#define DO_BENCHMARKING_GD                 1
+#define DO_BENCHMARKING_GD                 0
 #define BENCHMARK_GD_CHECK_RESULTING_IMAGE 0
 
 // General settings for Pt-Gfx
 #define DO_TEST_DRAW    1
-#define DO_BENCHMARKING 1
+#define DO_BENCHMARKING 0
 
 // Detailed-test enable settings for Pt-Gfx
 #define TEST_SOURCECOPY                        1
-#define TEST_SOURCEOVER                        1
+#define TEST_SOURCEOVER                        0
 
-#define TEST_DRAW_LINE_AND_TEXT                1
-#define TEST_DRAW_RECTANGLES_FILLED_RECTANGLES 1
+#define TEST_DRAW_LINE_AND_TEXT                0
+#define TEST_DRAW_RECTANGLES_FILLED_RECTANGLES 0
 
-#define TEST_DRAW_SOLID_FILLED_POLYGONS        1
-#define TEST_DRAW_GRADIENT_FILLED_POLYGONS     1
-#define TEST_DRAW_TEXTURE_FILLED_POLYGONS      1
+#define TEST_DRAW_SOLID_FILLED_POLYGONS        0
+#define TEST_DRAW_GRADIENT_FILLED_POLYGONS     0
+#define TEST_DRAW_TEXTURE_FILLED_POLYGONS      0
 
 #define TEST_DRAW_SOLID_FILLED_ELLIPSE         1
-#define TEST_DRAW_GRADIENT_FILLED_ELLIPSE      1
-#define TEST_DRAW_TEXTURE_FILLED_ELLIPSE       1
+#define TEST_DRAW_GRADIENT_FILLED_ELLIPSE      0
+#define TEST_DRAW_TEXTURE_FILLED_ELLIPSE       0
 
 #define TEST_COMPARE_WITH_OLD_PAINTER          0 // (for some shapes only)
 
@@ -67,7 +67,7 @@ using namespace Pt::Gfx;
 #define BENCHMARK_CHECK_RESULTING_IMAGE     0
 
 #define BENCHMARK_IMAGE_SIZE                Size(1280, 800)
-#define BENCHMARK_LOOP_COUNT                5000
+#define BENCHMARK_LOOP_COUNT                500
 
 #define BENCHMARK_TEXT                      1
 #define BENCHMARK_LINE                      1
