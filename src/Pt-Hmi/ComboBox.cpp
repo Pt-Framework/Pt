a3a94765cbfc7d2502a290477486b3e7c586ff97
d-marc <d-marc@private.net>
2017-02-26 17:35:10 +0000

    minor cleanup

--- src/Pt-Hmi/ComboBox.cpp
+++ src/Pt-Hmi/ComboBox.cpp
@@ -96,18 +96,6 @@ void ComboBoxMenu::onPaintBackground(const Gfx::RectF& rect)
 void ComboBoxMenu::onPaintContent(const Gfx::RectF& rect)
 {
     Base::onPaintContent(rect);
-
-    //const StyleOptions& options = Application::instance().styleOptions();
-
-    //Painter painter( surface() );
-    //painter.setClip(rect);
-
-    //
-    // menu border
-    //
-    //Gfx::RectF borderRect(size());
-    //painter.setPen( options.contour() );
-    //painter.drawRect(borderRect);
 }
 
 
