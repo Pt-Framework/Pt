3b099eb952a3db368b1f6fa44a669b1caf251f91
blue_wind_25 <blue_wind_25@private.net>
2017-03-02 00:40:17 +0000

    Clean-up

--- src/Pt-Gfx/tests/ImagePainterTest2.cpp
+++ src/Pt-Gfx/tests/ImagePainterTest2.cpp
@@ -44,7 +44,7 @@ using namespace Pt::Gfx;
 #define DO_MATH_BENCHMARKING_ONLY 0
 
 // Comparison with Cairo (solid-filled polygons and ellipses only)
-#define DO_BENCHMARKING_CAIRO                 1
+#define DO_BENCHMARKING_CAIRO                 0
 #define BENCHMARK_CAIRO_CHECK_RESULTING_IMAGE 0
 
 // General settings for Pt-Gfx
@@ -52,14 +52,14 @@ using namespace Pt::Gfx;
 #define DO_BENCHMARKING 1
 
 // Detailed-test enable settings for Pt-Gfx
-#define TEST_SOURCECOPY                         0
-#define TEST_SOURCEOVER                         1
+#define TEST_SOURCECOPY                         1
+#define TEST_SOURCEOVER                         0
 
-#define TEST_DRAW_SOLID_LINE_AND_TEXT           1
+#define TEST_DRAW_SOLID_LINE_AND_TEXT           0
 #define TEST_DRAW_PATTERNED_LINE                1
 #define TEST_DRAW_RECTANGLES_FILLED_RECTANGLES  0
 
-#define TEST_DRAW_ELLIPSES_ARCS                 1
+#define TEST_DRAW_ELLIPSES_ARCS                 0
 
 #define TEST_DRAW_SOLID_FILLED_POLYGONS         0
 #define TEST_DRAW_GRADIENT_FILLED_POLYGONS      0
@@ -80,31 +80,31 @@ using namespace Pt::Gfx;
 #define BENCHMARK_IMAGE_SIZE                Size(1280, 800)
 #define BENCHMARK_LOOP_COUNT                ( 500 * (BENCHMARK_RESULT_HTML ? 10 : 1) )
 
-#define BENCHMARK_TEXT                      1
-#define BENCHMARK_ROTATED_TEXT              1
-#define BENCHMARK_SOLID_LINE                1
-#define BENCHMARK_PATTERNED_LINE            1
-#define BENCHMARK_ELLIPSE                   1
-#define BENCHMARK_ARC                       1
+#define BENCHMARK_TEXT                      0
+#define BENCHMARK_ROTATED_TEXT              0
+#define BENCHMARK_SOLID_LINE                0
+#define BENCHMARK_PATTERNED_LINE            0
+#define BENCHMARK_ELLIPSE                   0
+#define BENCHMARK_ARC                       0
 #define BENCHMARK_SOLID_BEZIER              1
 #define BENCHMARK_PATTERNED_BEZIER          1
 
-#define BENCHMARK_RECTANGLE                 1
-#define BENCHMARK_SOLID_FILLED_RECTANGLE    1
-#define BENCHMARK_GRADIENT_FILLED_RECTANGLE 1
-#define BENCHMARK_TEXTURE_FILLED_RECTANGLE  1
+#define BENCHMARK_RECTANGLE                 0
+#define BENCHMARK_SOLID_FILLED_RECTANGLE    0
+#define BENCHMARK_GRADIENT_FILLED_RECTANGLE 0
+#define BENCHMARK_TEXTURE_FILLED_RECTANGLE  0
 
-#define BENCHMARK_SOLID_FILLED_POLYGON      1
-#define BENCHMARK_GRADIENT_FILLED_POLYGON   1
-#define BENCHMARK_TEXTURE_FILLED_POLYGON    1
+#define BENCHMARK_SOLID_FILLED_POLYGON      0
+#define BENCHMARK_GRADIENT_FILLED_POLYGON   0
+#define BENCHMARK_TEXTURE_FILLED_POLYGON    0
 
-#define BENCHMARK_SOLID_FILLED_ELLIPSE      1
-#define BENCHMARK_GRADIENT_FILLED_ELLIPSE   1
-#define BENCHMARK_TEXTURE_FILLED_ELLIPSE    1
+#define BENCHMARK_SOLID_FILLED_ELLIPSE      0
+#define BENCHMARK_GRADIENT_FILLED_ELLIPSE   0
+#define BENCHMARK_TEXTURE_FILLED_ELLIPSE    0
 
-#define BENCHMARK_SOLID_FILLED_ARC          1
-#define BENCHMARK_GRADIENT_FILLED_ARC       1
-#define BENCHMARK_TEXTURE_FILLED_ARC        1
+#define BENCHMARK_SOLID_FILLED_ARC          0
+#define BENCHMARK_GRADIENT_FILLED_ARC       0
+#define BENCHMARK_TEXTURE_FILLED_ARC        0
 
 // Configurations and objects
 #define FONT_DIR    "../src/Pt-Gfx/fonts"
