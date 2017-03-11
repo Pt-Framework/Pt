cbcd96a7e29d34fcbbe0e0d266bd791ea325d792
blue_wind_25 <blue_wind_25@private.net>
2017-03-11 23:50:11 +0000

    Improving the thick line benchmark

--- src/Pt-Gfx/tests/ImagePainterTest2_Benchmark_Thick.cpp
+++ src/Pt-Gfx/tests/ImagePainterTest2_Benchmark_Thick.cpp
@@ -8,6 +8,12 @@ static size_t benchDrawSolidThickLine(int loopCount, CompositionMode cm, AntiAli
     PainterT painter(image);
     painter.setCompositionMode(cm);
 
+    Pen penRCapBJoin(Color::fromRgb8(255, 255, 255, 175), 12, Pen::Solid, Pen::RoundCap,  Pen::BevelJoin);
+    Pen penSCapBJoin(Color::fromRgb8(255, 255, 255, 175), 12, Pen::Solid, Pen::SquareCap, Pen::BevelJoin);
+    Pen penBCapBJoin(Color::fromRgb8(255, 255, 255, 175), 12, Pen::Solid, Pen::ButtCap,   Pen::BevelJoin);
+    Pen penBCapMJoin(Color::fromRgb8(255, 255, 255, 175), 12, Pen::Solid, Pen::ButtCap,   Pen::MiterJoin);
+    Pen penBCapRJoin(Color::fromRgb8(255, 255, 255, 175), 12, Pen::Solid, Pen::ButtCap,   Pen::RoundJoin);
+
     ImagePainter2* ip2 = dynamic_cast<ImagePainter2*>(dynamic_cast<Painter*>(&painter));
 
     for(int i = 0; i < loopCount; ++i) {
@@ -15,8 +21,75 @@ static size_t benchDrawSolidThickLine(int loopCount, CompositionMode cm, AntiAli
         clock.start();
 
         if(ip2) ip2->setAntiAliasingMode(antiAliasingMode);
-        painter.setPen( Pen(Color::fromRgb8(255, 255, 255, 175), 12, Pen::Solid, Pen::FlatCap, Pen::BevelJoin) );
-        painter.drawLine( PointF(50, 50), PointF(400, 150) );
+
+        painter.setPen(penRCapBJoin);
+        painter.drawLine( PointF( 20, 300 - 30), PointF(100, 350 - 30) );
+        painter.drawLine( PointF( 20, 430 - 30), PointF(100, 370 - 30) );
+
+        painter.setPen(penSCapBJoin);
+        painter.drawLine( PointF(120, 300 - 30), PointF(200, 350 - 30) );
+        painter.drawLine( PointF(120, 430 - 30), PointF(200, 370 - 30) );
+
+        painter.setPen(penBCapBJoin);
+        painter.drawLine( PointF(220, 300 - 30), PointF(300, 350 - 30) );
+        painter.drawLine( PointF(220, 430 - 30), PointF(300, 370 - 30) );
+
+        painter.setPen(penBCapBJoin);
+        const PointF poly1a[] = { // CCW
+            PointF(670 + 10, 120),
+            PointF(700 + 10, 180),
+            PointF(800 + 10, 130),
+            PointF(650 + 10,  20)
+        };
+        if(ip2) ip2->drawPolyline( poly1a, sizeof(poly1a) / sizeof(poly1a[0]), false );
+        else painter.drawPolyline( poly1a, sizeof(poly1a) / sizeof(poly1a[0]) );
+
+        const PointF poly1b[] = { // CCW
+            PointF(670 + 170, 120),
+            PointF(700 + 170, 180),
+            PointF(800 + 170, 130),
+            PointF(650 + 170,  20)
+        };
+        if(ip2) ip2->drawPolyline( poly1b, sizeof(poly1b) / sizeof(poly1b[0]), true );
+        else painter.drawPolyline( poly1b, sizeof(poly1b) / sizeof(poly1b[0]) );
+
+        painter.setPen(penBCapMJoin);
+        const PointF poly2a[] = { // CCW
+            PointF(670 + 10, 120 + 200),
+            PointF(700 + 10, 180 + 200),
+            PointF(800 + 10, 130 + 200),
+            PointF(650 + 10,  20 + 200)
+        };
+        if(ip2) ip2->drawPolyline( poly2a, sizeof(poly2a) / sizeof(poly2a[0]), false );
+        else painter.drawPolyline( poly2a, sizeof(poly2a) / sizeof(poly2a[0]) );
+
+        const PointF poly2b[] = { // CCW
+            PointF(670 + 170, 120 + 200),
+            PointF(700 + 170, 180 + 200),
+            PointF(800 + 170, 130 + 200),
+            PointF(650 + 170,  20 + 200)
+        };
+        if(ip2) ip2->drawPolyline( poly2b, sizeof(poly2b) / sizeof(poly2b[0]), true );
+        else painter.drawPolyline( poly2b, sizeof(poly2b) / sizeof(poly2b[0]) );
+
+        painter.setPen(penBCapRJoin);
+        const PointF poly3a[] = { // CCW
+            PointF(670 + 10, 120 + 400),
+            PointF(700 + 10, 180 + 400),
+            PointF(800 + 10, 130 + 400),
+            PointF(650 + 10,  20 + 400)
+        };
+        if(ip2) ip2->drawPolyline( poly3a, sizeof(poly3a) / sizeof(poly3a[0]), false );
+        else painter.drawPolyline( poly3a, sizeof(poly3a) / sizeof(poly3a[0]) );
+
+        const PointF poly3b[] = { // CCW
+            PointF(670 + 170, 120 + 400),
+            PointF(700 + 170, 180 + 400),
+            PointF(800 + 170, 130 + 400),
+            PointF(650 + 170,  20 + 400)
+        };
+        if(ip2) ip2->drawPolyline( poly3b, sizeof(poly3b) / sizeof(poly3b[0]), true );
+        else painter.drawPolyline( poly3b, sizeof(poly3b) / sizeof(poly3b[0]) );
 
         sum += clock.stop().toUSecs();
 
