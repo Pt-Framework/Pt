f9a943533de1a03350ca7247e4e1e659f68aa208
d-marc <d-marc@private.net>
2018-08-31 15:11:01 +0000

    minor cleanup

--- src/Pt-Hmi/Cursor.cpp
+++ src/Pt-Hmi/Cursor.cpp
@@ -22,7 +22,8 @@
  
  You should have received a copy of the GNU Lesser General Public
  License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ MA  02110-1301  USA
 */
 
 #include <Pt/Hmi/Cursor.h>
@@ -38,9 +39,9 @@
 #include "MoveCursor.h"
 #include "WaitCursor.h"
 
-namespace Pt{
+namespace Pt {
 
-namespace Hmi{
+namespace Hmi {
 
 Cursor::Cursor()
 : _width(0)
@@ -269,4 +270,6 @@ void Cursor::fromImage( const Gfx::Image& image, Cursor& cursor)
 	}
 }
 
-}}
+}
+
+}
