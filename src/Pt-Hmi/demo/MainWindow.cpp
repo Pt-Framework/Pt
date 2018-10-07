a485f4d106fd942730e68da8fbe4156d4bcec4e3
l-crisan <l-crisan@private.net>
2018-10-07 17:19:50 +0000

    minor cleanup

--- src/Pt-Hmi/demo/MainWindow.cpp
+++ src/Pt-Hmi/demo/MainWindow.cpp
@@ -252,6 +252,7 @@ void MainWindow::onPaintBackground(const Gfx::RectF& rect)
 {
     Window::onPaintBackground(rect);
     Painter painter( surface() );
+
     return;
 
     Gfx::Image image( painter.format(), Gfx::Size(600, 600) );
--- src/Pt-Hmi/xorg/PixmapSurfaceImpl.h
+++ src/Pt-Hmi/xorg/PixmapSurfaceImpl.h
@@ -114,7 +114,7 @@ class PixmapSurfaceImpl : public PaintSurfaceImpl
     private:
         Gfx::SizeF            _size;
         Gfx::Image            _image;
-        Gfx::ImagePainter2     _painter;
+        Gfx::ImagePainter2    _painter;
 };
 
 } // namespace
