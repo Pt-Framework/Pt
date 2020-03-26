89b5eb126f15b120a4f478329055304d47352783
blue_wind_25 <blue_wind_25@private.net>
2020-03-26 02:23:27 +0000

    Cleanup

--- src/Pt-Gfx/tests/ImagePainterTest2.cpp
+++ src/Pt-Gfx/tests/ImagePainterTest2.cpp
@@ -71,7 +71,7 @@ using namespace Pt::Gfx;
 
 // Detailed-test enable settings for Pt-Gfx
 #define TEST_SOURCECOPY                         1
-#define TEST_SOURCEOVER                         0
+#define TEST_SOURCEOVER                         1
 
 #define TEST_DRAW_SOLID_LINE_AND_TEXT           0 // (including bezier)
 #define TEST_DRAW_PATTERNED_LINE                0 // (including bezier)
