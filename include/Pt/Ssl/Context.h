85220a476fa702068e13cf5fb7cab6e1631d50ed
d-marc <d-marc@private.net>
2021-04-22 16:48:35 +0000

    SSL protocol version

--- include/Pt/Ssl/Context.h
+++ include/Pt/Ssl/Context.h
@@ -59,9 +59,11 @@ enum OpenMode
 enum Protocol 
 {
     SSLv2,    //!< SSL version 2
-    SSLv3or2, //!< SSL version 2 or 3
+    SSLv3or2, //!< highest possible protocol version
     SSLv3,    //!< SSL version 3
-    TLSv1     //!< TLS version 1
+    TLSv1,    //!< TLS version 1.0
+    TLSv1_1,  //!< TLS version 1.1
+    TLSv1_2,  //!< TLS version 1.2
 };
 
 //! @brief Verification mode.
