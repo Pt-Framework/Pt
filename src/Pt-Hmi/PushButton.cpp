66d02186990317489c660fc39adaf6b6fee36c28
d-marc <d-marc@private.net>
2021-06-02 16:59:37 +0000

    merged: setPress() for toggle button

--- src/Pt-Hmi/PushButton.cpp
+++ src/Pt-Hmi/PushButton.cpp
@@ -255,7 +255,8 @@ bool PushButton::isPressed() const
 void PushButton::setPressed(bool pressed)
 {
     _isPressed = pressed;
-
+    _isBeingToggled = false;
+    
     const StyleOptions& options = Application::instance().styleOptions();
 
     if(_renderer)
@@ -283,12 +284,16 @@ void PushButton::onReleased()
 {
     Base::onReleased();
 
-    _isBeingToggled = false;
-
-    if( ! isToggle() )
+    if(_isBeingToggled)
+    {
+        _isBeingToggled = false;
+        clicked().send();
+    }
+    else
+    {
         setPressed(false);
-
-    clicked().send();
+        clicked().send();
+    }
 }
 
 
@@ -296,13 +301,10 @@ void PushButton::onCanceled()
 {
     Base::onCanceled();
 
-    if( isToggle() )
+    if( _isBeingToggled )
     { 
-        if(_isBeingToggled)
-        {
-            _isBeingToggled = false;
-            setPressed( ! isPressed() );
-        }
+        _isBeingToggled = false;
+        setPressed( ! isPressed() );
     }
     else
         setPressed(false);
