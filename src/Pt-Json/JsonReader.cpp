eabb3a550dbca94d297df32c904dac17a72dbc52
d-marc <d-marc@private.net>
2023-04-21 11:48:47 +0000

    JSON node reader

---
+++ src/Pt-Json/JsonReader.cpp
@@ -0,0 +1,620 @@
+/*
+   Copyright (C) 2015-2023 by Dr. Marc Boris Duerner
+  
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   As a special exception, you may use this file as part of a free
+   software library without restriction. Specifically, if other files
+   instantiate templates or use macros or inline functions from this
+   file, or you compile this file and link it with other files to
+   produce an executable, this file does not by itself cause the
+   resulting executable to be covered by the GNU General Public
+   License. This exception does not however invalidate any other
+   reasons why the executable file might be covered by the GNU Library
+   General Public License.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+   MA 02110-1301 USA
+*/
+
+#include <Pt/Json/JsonReader.h>
+#include <Pt/Json/JsonError.h>
+#include <Pt/Json/EndDocument.h>
+#include <Pt/System/Logger.h>
+#include <Pt/Convert.h>
+
+#include <stack>
+#include <cassert>
+
+PT_LOG_DEFINE("Pt.Json.JsonReader")
+
+namespace Pt {
+
+namespace Json {
+
+class JsonReaderImpl
+{
+    typedef std::char_traits<Char>::int_type int_type;
+
+    typedef void (JsonReaderImpl::*ParseFunc)(int_type);
+
+    private:
+        void onDocument(int_type c)
+        {
+            if( c == std::char_traits<Char>::eof() )
+            {
+                std::clog << "DOCUMENT END" << std::endl;
+                _current = &_endDoc;
+                return;
+            }
+
+            Char ch = c;
+
+            if( isSpace(ch) )
+                return;
+
+            std::clog << std::endl;
+            std::clog << "DOCUMENT BEGIN" << std::endl;
+            pushParseState(&JsonReaderImpl::onDocumentEnd);
+            onElement(c);
+        }
+
+        void onDocumentEnd(int_type c)
+        {
+            Char ch = c;
+
+            if( c == std::char_traits<Char>::eof() )
+            {
+                std::clog << "DOCUMENT END" << std::endl;
+                _current = &_endDoc;
+                return;
+            }
+            
+            if( ! isSpace(ch) )
+            {
+                throw SyntaxError("JSON syntax error", line());
+            }
+        }
+
+        void onObject(int_type c)
+        {
+            Char ch = notEof(c);
+
+            if( isSpace(ch) )
+                return;
+
+            if(ch == '}')
+            {
+                std::clog << "OBJECT END" << std::endl;
+                popParseState();
+            }
+            else if(ch == '"')
+            {
+                _token.clear();
+                _parse = &JsonReaderImpl::onMemberName;
+            }
+            else
+            {
+                throw SyntaxError("JSON syntax error", line());
+            }
+        }
+
+        void onMemberName(int_type c)
+        {
+            Char ch = notEof(c);
+
+            if(ch == '"')
+            {
+                std::clog << "MEMBER: " << _token.narrow() << std::endl;
+                _parse = &JsonReaderImpl::onMemberNameEnd;
+                return;
+            }
+
+            _token += ch;
+        }
+
+        void onMemberNameEnd(int_type c)
+        {
+            Char ch = c;
+
+            if( isSpace(ch) )
+                return;
+
+            if(ch == ':')
+            {
+                _parse = &JsonReaderImpl::onMemberAssign;
+            }
+            else
+            {
+                throw SyntaxError("JSON syntax error", line());
+            }
+        }
+
+        void onMemberAssign(int_type c)
+        {
+            Char ch = c;
+
+            if( isSpace(ch) )
+                return;
+
+            pushParseState( &JsonReaderImpl::onMemberEnd);
+            onElement(c);
+        }
+
+        void onMemberEnd(int_type c)
+        {
+            Char ch = c;
+
+            if( isSpace(ch) )
+                return;
+
+            if(ch == '}')
+            {
+                std::clog << "OBJECT END" << std::endl;
+                popParseState();
+            }
+            else if(ch == ',')
+            {
+                _parse = &JsonReaderImpl::onObject;
+            }
+            else
+            {
+                throw SyntaxError("JSON syntax error", line());
+            }
+        }
+
+        void onArray(int_type c)
+        {
+            Char ch = notEof(c);
+
+            if( isSpace(ch) )
+                return;
+
+            if(ch == ']')
+            {
+                std::clog << "ARRAY END" << std::endl;
+                popParseState();
+                return;
+            }
+            
+            pushParseState(&JsonReaderImpl::onArrayElementEnd);
+            onElement(c);
+        }
+
+        void onArrayElementEnd(int_type c)
+        {
+            Char ch = c;
+
+            if( isSpace(ch) )
+                return;
+
+            if(ch == ']')
+            {
+                std::clog << "ARRAY END" << std::endl;
+                popParseState();
+            }
+            else if(ch == ',')
+            {
+                _parse = &JsonReaderImpl::onArray;
+            }
+            else
+            {
+                throw SyntaxError("JSON syntax error", line());
+            }
+        }
+
+        void onElement(int_type c)
+        {
+            Char ch = c;
+
+            if( isSpace(ch) )
+                return;
+
+            if(ch == '{')
+            {
+                std::clog << "OBJECT BEGIN" << std::endl;
+                _parse = &JsonReaderImpl::onObject;
+            }
+            else if(ch == '[')
+            {
+                std::clog << "ARRAY BEGIN" << std::endl;
+                _parse = &JsonReaderImpl::onArray;
+            }
+            else if(ch == '"')
+            {
+                _token.clear();
+                _parse = &JsonReaderImpl::onString;
+            }
+            else if( isdigit(ch) || ch == '-' )
+            {
+                _token.clear();
+                _parse = &JsonReaderImpl::onNumber;
+                (this->*_parse)(c);
+            }
+            else
+            {
+                throw SyntaxError("JSON syntax error", line());
+            }
+        }
+
+        void onString(int_type c)
+        {
+            Char ch = notEof(c);
+
+            if(ch == '"')
+            {
+                std::clog << "STRING: " << _token.narrow() << std::endl;
+
+                popParseState();
+                return;
+            }
+
+            _token += ch;
+        }
+
+        void onNumber(int_type c)
+        {
+            Char ch = c;
+
+            if( c != std::char_traits<Char>::eof() )
+            {
+                if( isdigit(ch) || ch == '-' || ch == '+' )
+                {
+                    _token += ch;
+                    return;
+                }
+            }
+
+            double n = 0;
+            Pt::parseFloat(_token.begin(), _token.end(), n);
+            std::clog << "NUMBER: " << _token.narrow() << std::endl;
+
+            popParseState();
+            (this->*_parse)(c);
+        }
+
+    private:
+        inline Char notEof(int_type c) const
+        {
+            if( c == std::char_traits<Char>::eof() )
+            {
+                throw SyntaxError("JSON syntax error", line());
+            }
+
+            return Char(c);
+        }
+
+        inline bool isSpace(Char ch) const
+        {
+            return ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n';
+        }
+
+        inline void pushParseState(ParseFunc parse)
+        {
+            // TODO: push state should add to _usedSize...
+            _parseStack.push(parse);
+        }
+
+        inline void popParseState()
+        {
+            // TODO: push state should shrink _usedSize...
+            if( _parseStack.empty() )
+                throw SyntaxError("JSON syntax error", line());
+
+            _parse = _parseStack.top();
+            _parseStack.pop();
+        }
+
+    public:
+        JsonReaderImpl()
+        : _is(0)
+        , _parse(0)
+        , _line(1)
+        , _depth(0)
+        , _usedSize(0)
+        , _maxSize(2048)
+        , _maxChunkSize(2048)
+        , _chunkSize(0) 
+        , _current(0)
+        {
+            _parse = &JsonReaderImpl::onDocument;
+        }
+
+        JsonReaderImpl(std::basic_istream<Pt::Char>& is)
+        : _is(&is)
+        , _parse(0)
+        , _line(1)
+        , _depth(0)
+        , _usedSize(0)
+        , _maxSize(2048)
+        , _maxChunkSize(2048)
+        , _chunkSize(0)
+        , _current(0)
+        {
+            _parse = &JsonReaderImpl::onDocument;
+        }
+
+        ~JsonReaderImpl()
+        { }
+        
+        void setMaxSize(std::size_t n)
+        {
+            _maxSize = n;
+        }
+
+        std::size_t maxSize() const
+        {
+            return _maxSize;
+        }
+
+        std::size_t usedSize() const
+        {
+            return _usedSize;
+        }
+
+        void setChunkSize(std::size_t n)
+        {
+            _maxChunkSize = n;
+        }
+
+        void reset()
+        {
+            _is = 0;
+           
+            _line = 1;
+            _depth = 0;
+            _usedSize = 0;
+            _chunkSize = 0;
+
+            _parse = &JsonReaderImpl::onDocument;
+
+            while( ! _parseStack.empty() )
+                _parseStack.pop();
+
+            _token.clear(); 
+            _current = 0;
+
+            // nodes are cleared before they are parsed
+        }
+
+        std::basic_istream<Pt::Char>* input()
+        {
+            return _is;
+        }
+
+        void reset(std::basic_istream<Pt::Char>& is)
+        {
+            reset();
+            _is = &is;
+        }
+
+        void setInput(std::basic_istream<Pt::Char>& is)
+        { 
+            _is = &is;
+        }
+
+        std::size_t depth() const
+        { 
+            return _depth; 
+        }
+
+        inline void incDepth()
+        {           
+            _depth++;
+        }
+
+        inline void decDepth()
+        {
+            _depth--;
+        }
+
+        std::size_t line() const
+        { 
+            return _line; 
+        }
+
+        void bumpLine()
+        {
+            ++_line;
+        }
+
+        Node& get()
+        {
+            if( ! _current )
+            {
+                this->next();
+            }
+
+            assert(_current != 0);
+
+            return *_current;
+        }
+
+        Node& next()
+        {
+            _current = 0;
+            const std::char_traits<Char>::int_type eof = std::char_traits<Char>::eof();
+            
+            if( ! _is )
+                throw JsonError("no input");
+
+            while( ! _current )
+            {
+                std::char_traits<Char>::int_type c = _is->get();
+
+                //std::cerr << char(c) << " (" << c << ")\n";
+                (this->*_parse)(c);
+
+                if(c == '\n')
+                    bumpLine();
+            }
+
+            return *_current;
+        }
+
+        Node* advance()
+        {
+            _current = 0;
+
+            //do
+            //{
+            //    std::streamsize n = _input.avail();            
+
+            //    if(n > 0)
+            //    {
+            //        std::char_traits<Char>::int_type c = _input.get();
+            //        
+            //        (this->*_parse)(c);
+
+            //        if(c == '\n')
+            //            _input.bumpLine();
+            //    }
+            //    else if(n < 0)
+            //    {
+            //        _input.removeInput();
+
+            //        if( _input.empty() )
+            //            (this->*_parse)( std::char_traits<Char>::eof() );
+            //    }
+            //    else if (n == 0)
+            //    {
+            //        n = _input.import();
+            //        if(n == 0)
+            //            break;
+            //    }
+            //} 
+            //while( ! _current);
+
+            return _current;
+        }
+
+    private:
+        std::basic_istream<Pt::Char>*  _is;
+
+        ParseFunc             _parse;
+        std::stack<ParseFunc> _parseStack;
+
+        String       _token;
+        std::size_t  _line;
+        std::size_t  _depth;
+        std::size_t  _usedSize;
+        std::size_t  _maxSize;
+
+        std::size_t _maxChunkSize;
+        std::size_t _chunkSize;
+
+        Node*     _current;
+        EndDocument _endDoc;
+};
+
+
+JsonReader::JsonReader()
+: _impl(0)
+{
+    _impl = new JsonReaderImpl();
+}
+
+
+JsonReader::JsonReader(std::basic_istream<Pt::Char>& is)
+: _impl(0)
+{
+    _impl = new JsonReaderImpl(is);
+}
+
+
+JsonReader::~JsonReader()
+{
+    delete _impl;
+}
+
+
+std::basic_istream<Pt::Char>* JsonReader::input()
+{
+    return _impl->input();
+}
+
+
+void JsonReader::reset()
+{
+    _impl->reset();
+}
+
+
+void JsonReader::reset(std::basic_istream<Pt::Char>& is)
+{
+    _impl->reset(is);
+}
+
+
+void JsonReader::setInput(std::basic_istream<Pt::Char>& in)
+{
+    _impl->setInput(in);
+}
+
+
+void JsonReader::setMaxSize(std::size_t n)
+{
+    return _impl->setMaxSize(n);
+}
+
+
+std::size_t JsonReader::maxSize() const
+{
+    return _impl->maxSize();
+}
+
+
+std::size_t JsonReader::usedSize() const
+{
+    return _impl->usedSize();
+}
+
+
+void JsonReader::setChunkSize(std::size_t n)
+{
+    return _impl->setChunkSize(n);
+}
+
+
+std::size_t JsonReader::depth() const
+{
+    return _impl->depth();
+}
+
+
+std::size_t JsonReader::line() const
+{
+    return _impl->line();
+}
+
+
+Node& JsonReader::get()
+{
+    return _impl->get();
+}
+
+
+Node& JsonReader::next()
+{
+    return _impl->next();
+}
+
+
+Node* JsonReader::advance()
+{
+    return _impl->advance();
+}
+
+} // namespace
+
+} // namespace
---
+++ src/Pt-Json/tests/Pt-Json-test.cpp
@@ -0,0 +1,82 @@
+/*
+   Copyright (C) 2015-2023 by Dr. Marc Boris Duerner
+  
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   As a special exception, you may use this file as part of a free
+   software library without restriction. Specifically, if other files
+   instantiate templates or use macros or inline functions from this
+   file, or you compile this file and link it with other files to
+   produce an executable, this file does not by itself cause the
+   resulting executable to be covered by the GNU General Public
+   License. This exception does not however invalidate any other
+   reasons why the executable file might be covered by the GNU Library
+   General Public License.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+   MA 02110-1301 USA
+*/
+
+#include <Pt/Unit/TestSuite.h>
+#include <Pt/Unit/TestMain.h>
+#include <Pt/Unit/RegisterTest.h>
+
+#include <Pt/Json/EndDocument.h>
+#include <Pt/Json/JsonReader.h>
+#include <Pt/StringStream.h>
+
+class JsonReaderTest : public Pt::Unit::TestSuite
+{
+  public:
+    JsonReaderTest()
+    : Pt::Unit::TestSuite("JsonReaderTest")
+    {
+      this->registerMethod("EmptyDocument", *this, &JsonReaderTest::EmptyDocument);
+    }
+
+    void EmptyDocument();
+};
+
+
+Pt::Unit::RegisterTest<JsonReaderTest> register_PtJsonTest;
+
+
+void JsonReaderTest::EmptyDocument()
+{
+  using namespace Pt;
+  using namespace Pt::Json;
+
+  //String s = "{\"a\":\"b\",\"o\":{\"x\":\"y\"}}";
+  //String s = "{}";
+  //String s = " { } ";
+
+  //String s = "[\"a\",\"b\"]";
+  //String s = "[]";
+  //String s = " [ ] ";
+  
+  //String s = " \"Hello\" ";
+
+  String s = "123";
+
+  IStringStream iss(s);
+
+  JsonReader reader(iss);
+
+  while( reader.next().type() != Node::EndDocument )
+  {
+      Node& node = reader.get();
+  }
+
+  Node& node = reader.get();
+  EndDocument& endDoc = toEndDocument(node);
+}
