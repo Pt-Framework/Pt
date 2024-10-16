9b128d868e46222841411a0207b6f57c0258c75a
d-marc <d-marc@private.net>
2024-10-16 16:38:33 +0000

    minor cleanup

--- include/Pt/Gfx/Painter.h
+++ include/Pt/Gfx/Painter.h
@@ -72,6 +72,8 @@ class PT_GFX_API Painter
 
         void finish();
 
+        // TODO: return pointer to surface instead of format or scaling
+
         /** @brief Returns the image format.
         */
         const ImageFormat& format() const;
