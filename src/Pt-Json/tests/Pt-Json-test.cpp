89b973c481ceb44845ef20b4d2867ddfb4a37b97
l-crisan <l-crisan@private.net>
2025-03-14 09:57:43 +0000

    Json Settings test added.

--- src/Pt-Json/tests/Pt-Json-test.cpp
+++ src/Pt-Json/tests/Pt-Json-test.cpp
@@ -46,12 +46,159 @@
 #include <Pt/Json/EndArray.h>
 #include <Pt/Json/JsonReader.h>
 #include <Pt/Json/JsonWriter.h>
+#include <Pt/Json/JsonFormatter.h>
 
 #include <Pt/StringStream.h>
 #include <Pt/String.h>
+#include <Pt/IOStream.h>
+#include <Pt/TextStream.h>
+#include <Pt/Utf8Codec.h>
+#include <Pt/Settings.h>
 
 #include <vector>
 #include <cmath>
+
+class SerDemo
+{
+    public:
+        class SubItem
+        {
+            public:
+            SubItem()
+            : _intValue(7411)
+            , _boolValue(true)
+            , _floatValue(7411.7411f)
+            , _doubelValue(3.123123)
+            , _array(4)
+            {
+                _array[0] = 1;
+                _array[1] = 2;
+                _array[2] = 3;
+                _array[3] = 4;
+            }
+
+            ~SubItem()
+            {
+
+            }
+
+            void clear()
+            {
+                _intValue = 0;
+                _boolValue = 0;
+                _floatValue = 0;
+                _doubelValue = 0;
+                _array.clear();
+            }
+
+
+            bool operator!=(const SubItem& obj)
+            {
+                return !operator==(obj);
+            }
+
+            bool operator==(const SubItem& obj)
+            {
+                if (_intValue != obj._intValue)
+                    return false;
+
+                if (_floatValue != obj._floatValue)
+                    return false;
+
+                if (_doubelValue != obj._doubelValue)
+                    return false;
+
+                if (_array.size() != obj._array.size())
+                    return false;
+
+                for (size_t i = 0; i < _array.size(); ++i)
+                {
+                    if (_array[i] != obj._array[i])
+                        return false;
+                }
+
+                return true;
+            }
+
+            int _intValue;
+            bool _boolValue;
+            float _floatValue;
+            double _doubelValue;
+            std::vector<int> _array;
+        };
+
+        SerDemo()
+        : _intValue(12)
+        , _boolValue(false)
+        , _floatValue(23.33f)
+        , _doubelValue(123.1234567689)
+        , _items(3)
+        , _structValue()
+        {
+
+        }
+
+        ~SerDemo()
+        {
+
+        }
+
+
+        void clear()
+        {
+            _intValue = 0;
+            _boolValue = 0;
+            _floatValue = 0;
+            _doubelValue = 0;
+            _items.clear();
+            _structValue.clear();
+        }
+
+
+        bool operator!=(const SerDemo& obj)
+        {
+            return !operator==(obj);
+        }
+
+        bool operator==(const SerDemo& obj)
+        {
+            if (_intValue != obj._intValue)
+                return false;
+
+            if (_boolValue != obj._boolValue)
+                return false;
+
+
+            if (_floatValue != obj._floatValue)
+                return false;
+
+
+            if (_doubelValue != obj._doubelValue)
+                return false;
+            
+            
+            if (_items.size() != obj._items.size())
+                return false;
+
+            for (size_t i = 0; i < _items.size(); ++i)
+            {
+                if (_items[i] != obj._items[i])
+                    return false;
+            }
+
+            if (_structValue != obj._structValue)
+                return false;
+
+            return true;
+        }
+
+        int _intValue;
+        bool _boolValue;
+        float _floatValue;
+        double _doubelValue;
+        std::vector<SubItem> _items;
+        SubItem _structValue;
+};
 
 class JsonReaderTest : public Pt::Unit::TestSuite
 {
@@ -63,14 +210,55 @@ class JsonReaderTest : public Pt::Unit::TestSuite
       this->registerMethod("Float", *this, &JsonReaderTest::Float);
       this->registerMethod("SimpleArray", *this, &JsonReaderTest::SimpleArray);
       this->registerMethod("SimpleObject", *this, &JsonReaderTest::SimpleObject);
+      this->registerMethod("Settings", *this, &JsonReaderTest::Settings);
     }
 
     void EmptyDocument();
     void Float();
     void SimpleArray();
     void SimpleObject();
+    void Settings();
+
 };
 
+void operator<<=(Pt::SerializationInfo& si, const SerDemo::SubItem& obj)
+{
+    si.addMember("intValue") <<= obj._intValue;
+    si.addMember("boolValue") <<= obj._boolValue;
+    si.addMember("floatValue") <<= obj._floatValue;
+    si.addMember("doubelValue") <<= obj._doubelValue;
+    si.addMember("array") <<= obj._array;
+}
+
+
+void operator>>=(const Pt::SerializationInfo& si, SerDemo::SubItem& obj)
+{
+    si.getMember("intValue") >>= obj._intValue;
+    si.getMember("boolValue") >>= obj._boolValue;
+    si.getMember("floatValue") >>= obj._floatValue;
+    si.getMember("doubelValue") >>= obj._doubelValue;
+    si.getMember("array") >>= obj._array;
+}
+
+void operator<<=(Pt::SerializationInfo& si, const SerDemo& obj)
+{
+    si.addMember("intValue") <<= obj._intValue;
+    si.addMember("boolValue") <<= obj._boolValue;
+    si.addMember("floatValue") <<= obj._floatValue;
+    si.addMember("doubelValue") <<= obj._doubelValue;
+    si.addMember("items") <<= obj._items;
+    si.addMember("structValue") <<= obj._structValue;
+}
+
+void operator>>=(const Pt::SerializationInfo& si, SerDemo& obj)
+{
+    si.getMember("intValue") >>= obj._intValue;
+    si.getMember("boolValue") >>= obj._boolValue;
+    si.getMember("floatValue") >>= obj._floatValue;
+    si.getMember("doubelValue") >>= obj._doubelValue;
+    si.getMember("items") >>= obj._items;
+    si.getMember("structValue") >>= obj._structValue;
+}
 
 Pt::Unit::RegisterTest<JsonReaderTest> register_PtJsonTest;
 
@@ -196,4 +384,47 @@ void JsonReaderTest::SimpleObject()
     //writer.writeObjectEnd();
 
     //std::cout << "\n" << oss.str().narrow() << std::endl;
+}
+
+
+void JsonReaderTest::Settings()
+{
+    SerDemo serObj;
+    std::string str;
+
+    {//Serialize
+        std::stringstream       ss;
+        Pt::TextOStream         os(ss, new Pt::Utf8Codec());
+        Pt::Json::JsonWriter    writer(os);
+        Pt::Json::JsonFormatter formater(writer);
+
+        Pt::Settings settings;
+
+        settings.addEntry("SerDemo").set(serObj);
+
+        settings.save(formater);
+        os.flush();
+        ss.flush();
+
+        str = ss.str();
+    }
+
+    SerDemo desObj;
+
+    desObj.clear();
+
+    {//Deserialize
+        std::stringstream       ss(str);
+        Pt::TextIStream         is(ss, new Pt::Utf8Codec());
+        Pt::Json::JsonReader    reader(is);
+        Pt::Json::JsonFormatter formater(reader);
+
+        Pt::Settings settings;
+
+        settings.load(formater);
+
+        settings["SerDemo"].get(desObj);
+    }
+
+    PT_UNIT_ASSERT(desObj == serObj);
 }
