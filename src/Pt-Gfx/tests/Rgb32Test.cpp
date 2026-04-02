8284379202c852c6be75e7640a019bce8f060392
d-marc <1781530+d-marc@users.noreply.github.com>
2026-04-02 23:26:54 +0200

    Added Rgb32 tests

--- src/Pt-Gfx/tests/Rgb32Test.cpp
+++ src/Pt-Gfx/tests/Rgb32Test.cpp
@@ -47,31 +47,34 @@ class Rgb32Test : public Pt::Unit::TestSuite
         Rgb32Test()
         : Pt::Unit::TestSuite("Pt::Gfx::Rgb32Test")
         , inputColors {
-                Color(255, 200, 100, 50),  Color(0,   200, 100, 50),
-                Color(128, 200, 100, 50),  Color(64,  180,  60, 30),
-            }
+            Color(255, 200, 100, 50),  Color(0,   200, 100, 50),
+            Color(128, 200, 100, 50),  Color(64,  180,  60, 30),
+        }
         , rgb32Colors {
-                Gfx::Rgb32Color(255, 200, 100, 50),  Gfx::Rgb32Color(  0,   0,   0,  0),
-                Gfx::Rgb32Color(128, 100,  50, 25),  Gfx::Rgb32Color( 64,  45,  15,  8),
-            }
+            Gfx::Rgb32Color(255, 200, 100, 50),  Gfx::Rgb32Color(  0,   0,   0,  0),
+            Gfx::Rgb32Color(128, 100,  50, 25),  Gfx::Rgb32Color( 64,  45,  15,  8),
+        }
         , rgb32Data {
-                50, 100, 200, 255,   0,   0,   0,   0,
-                25,  50, 100, 128,   8,  15,  45,  64,
-            }
+            50, 100, 200, 255,   0,   0,   0,   0,
+            25,  50, 100, 128,   8,  15,  45,  64,
+        }
         , expectedColors {
-                Color(255, 200, 100, 50),  Color(0,     0,   0,  0),
-                Color(128, 199,  99, 49),  Color(64,  179,  59, 31),
-            }
+            Color(255, 200, 100, 50),  Color(0,     0,   0,  0),
+            Color(128, 199,  99, 49),  Color(64,  179,  59, 31),
+        }
         , inputColorsF {
-                ColorF(65535, 51400, 25700, 12850),  ColorF(0,     51400, 25700, 12850),
-                ColorF(32896, 51400, 25700, 12850), ColorF(16448, 46260, 15420,  8224),
-            }
+            ColorF(65535, 51400, 25700, 12850),  ColorF(0,     51400, 25700, 12850),
+            ColorF(32896, 51400, 25700, 12850), ColorF(16448, 46260, 15420,  8224),
+        }
         , expectedColorsF {
-                ColorF(65535, 51400, 25700, 12850),  ColorF(0,         0,     0,     0),
-                ColorF(32896, 51199, 25599, 12799), ColorF(16448, 46079, 15359,  8191),
-            }
+            ColorF(65535, 51400, 25700, 12850),  ColorF(0,         0,     0,     0),
+            ColorF(32896, 51199, 25599, 12799), ColorF(16448, 46079, 15359,  8191),
+        }
         {
             registerMethod("ImageTraits", *this, &Rgb32Test::ImageTraits);
+            registerMethod("GetSetChannels", *this, &Rgb32Test::GetSetChannels);
+            registerMethod("ComparePixel", *this, &Rgb32Test::ComparePixel);
+            registerMethod("MovePixel", *this, &Rgb32Test::MovePixel);
             registerMethod("GenericGetColor", *this, &Rgb32Test::GenericGetColor);
             registerMethod("GenericGetColorF", *this, &Rgb32Test::GenericGetColorF);
             registerMethod("DirectGetColor", *this, &Rgb32Test::DirectGetColor);
@@ -107,31 +110,183 @@ class Rgb32Test : public Pt::Unit::TestSuite
             // Rgb32Image
             Gfx::Rgb32Image rgb32Image(10, 5);
             PT_UNIT_ASSERT_EQUAL(rgb32Image.pixelStride(), Pt::ssize_t(4));
-            PT_UNIT_ASSERT_EQUAL(rgb32Image.size(), std::size_t(200));
+            PT_UNIT_ASSERT_EQUAL(rgb32Image.size(), Rgb32::get().imageSize(10, 5, 0));
             PT_UNIT_ASSERT(rgb32Image.format().type() == typeid(Rgb32));
 
-            const Gfx::Rgb32ConstImage constRgb32Image(rgb32Image);
+            Gfx::Rgb32Image rgb32PaddedImage(10, 5, 4);
+            const Gfx::Rgb32ConstImage constRgb32Image(rgb32PaddedImage);
             PT_UNIT_ASSERT_EQUAL(constRgb32Image.pixelStride(), Pt::ssize_t(4));
-            PT_UNIT_ASSERT_EQUAL(constRgb32Image.size(), std::size_t(200));
+            PT_UNIT_ASSERT_EQUAL(constRgb32Image.size(), Rgb32::get().imageSize(10, 5, 4));
             PT_UNIT_ASSERT(constRgb32Image.format().type() == typeid(Rgb32));
 
             // Image
             Gfx::Image image(10, 5, Rgb32::get());
             PT_UNIT_ASSERT_EQUAL(image.pixelStride(), Pt::ssize_t(4));
-            PT_UNIT_ASSERT_EQUAL(image.size(), std::size_t(200));
+            PT_UNIT_ASSERT_EQUAL(image.size(), Rgb32::get().imageSize(10, 5, 0));
 
-            const Gfx::ConstImage constImage(image);
+            Gfx::Image paddedImage(10, 5, 4, Rgb32::get());
+            const Gfx::ConstImage constImage(paddedImage);
             PT_UNIT_ASSERT_EQUAL(constImage.pixelStride(), Pt::ssize_t(4));
-            PT_UNIT_ASSERT_EQUAL(constImage.size(), std::size_t(200));
+            PT_UNIT_ASSERT_EQUAL(constImage.size(), Rgb32::get().imageSize(10, 5, 4));
 
             // ImageF
             Gfx::ImageF imageF(10, 5, Rgb32::get());
             PT_UNIT_ASSERT_EQUAL(imageF.pixelStride(), Pt::ssize_t(4));
-            PT_UNIT_ASSERT_EQUAL(imageF.size(), std::size_t(200));
+            PT_UNIT_ASSERT_EQUAL(imageF.size(), Rgb32::get().imageSize(10, 5, 0));
 
-            const Gfx::ConstImageF constImageF(imageF);
+            Gfx::ImageF paddedImageF(10, 5, 4, Rgb32::get());
+            const Gfx::ConstImageF constImageF(paddedImageF);
             PT_UNIT_ASSERT_EQUAL(constImageF.pixelStride(), Pt::ssize_t(4));
-            PT_UNIT_ASSERT_EQUAL(constImageF.size(), std::size_t(200));
+            PT_UNIT_ASSERT_EQUAL(constImageF.size(), Rgb32::get().imageSize(10, 5, 4));
+        }
+
+        void GetSetChannels()
+        {
+            const Gfx::Rgb32Color inputColor(0x11, 0x22, 0x33, 0x44);
+            const Gfx::Rgb32Color expectedColor(0xAA, 0xBB, 0xCC, 0xDD);
+
+            Gfx::Rgb32Image image(1, 1);
+            Gfx::Rgb32Pixel pixel(image, 0, 0);
+            pixel = inputColor;
+
+            PT_UNIT_ASSERT_EQUAL(pixel.alpha(), inputColor.alpha());
+            PT_UNIT_ASSERT_EQUAL(pixel.red(), inputColor.red());
+            PT_UNIT_ASSERT_EQUAL(pixel.green(), inputColor.green());
+            PT_UNIT_ASSERT_EQUAL(pixel.blue(), inputColor.blue());
+
+            Gfx::Rgb32ConstPixel constPixel(image, 0, 0);
+            PT_UNIT_ASSERT_EQUAL(constPixel.alpha(), inputColor.alpha());
+            PT_UNIT_ASSERT_EQUAL(constPixel.red(), inputColor.red());
+            PT_UNIT_ASSERT_EQUAL(constPixel.green(), inputColor.green());
+            PT_UNIT_ASSERT_EQUAL(constPixel.blue(), inputColor.blue());
+
+            pixel.setAlpha(expectedColor.alpha());
+            PT_UNIT_ASSERT_EQUAL(pixel.alpha(), expectedColor.alpha());
+            PT_UNIT_ASSERT_EQUAL(pixel.red(), inputColor.red());
+            PT_UNIT_ASSERT_EQUAL(pixel.green(), inputColor.green());
+            PT_UNIT_ASSERT_EQUAL(pixel.blue(), inputColor.blue());
+
+            pixel.setRed(expectedColor.red());
+            PT_UNIT_ASSERT_EQUAL(pixel.alpha(), expectedColor.alpha());
+            PT_UNIT_ASSERT_EQUAL(pixel.red(), expectedColor.red());
+            PT_UNIT_ASSERT_EQUAL(pixel.green(), inputColor.green());
+            PT_UNIT_ASSERT_EQUAL(pixel.blue(), inputColor.blue());
+
+            pixel.setGreen(expectedColor.green());
+            PT_UNIT_ASSERT_EQUAL(pixel.alpha(), expectedColor.alpha());
+            PT_UNIT_ASSERT_EQUAL(pixel.red(), expectedColor.red());
+            PT_UNIT_ASSERT_EQUAL(pixel.green(), expectedColor.green());
+            PT_UNIT_ASSERT_EQUAL(pixel.blue(), inputColor.blue());
+
+            pixel.setBlue(expectedColor.blue());
+            PT_UNIT_ASSERT_EQUAL(pixel.alpha(), expectedColor.alpha());
+            PT_UNIT_ASSERT_EQUAL(pixel.red(), expectedColor.red());
+            PT_UNIT_ASSERT_EQUAL(pixel.green(), expectedColor.green());
+            PT_UNIT_ASSERT_EQUAL(pixel.blue(), expectedColor.blue());
+            PT_UNIT_ASSERT_EQUAL(pixel.color().value(), expectedColor.value());
+
+            PT_UNIT_ASSERT_EQUAL(constPixel.alpha(), expectedColor.alpha());
+            PT_UNIT_ASSERT_EQUAL(constPixel.red(), expectedColor.red());
+            PT_UNIT_ASSERT_EQUAL(constPixel.green(), expectedColor.green());
+            PT_UNIT_ASSERT_EQUAL(constPixel.blue(), expectedColor.blue());
+            PT_UNIT_ASSERT_EQUAL(constPixel.color().value(), expectedColor.value());
+        }
+
+        void ComparePixel()
+        {
+            const std::vector<Pt::uint8_t> data {
+                1,  2,  3,  4,   5,  6,  7,  8,  0xEE, 0xEE, 0xEE, 0xEE,
+                9, 10, 11, 12,  13, 14, 15, 16,  0xEE, 0xEE, 0xEE, 0xEE,
+            };
+
+            Gfx::Rgb32Image image(2, 2, 4);
+            std::memcpy(image.data(), data.data(), data.size());
+
+            Gfx::Rgb32Pixel pixel(image, 0, 1);
+            Gfx::Rgb32Pixel equalPixel(image, 0, 1);
+            Gfx::Rgb32Pixel differentPixel(image, 1, 1);
+            PT_UNIT_ASSERT(pixel.equals(equalPixel));
+            PT_UNIT_ASSERT(!pixel.equals(differentPixel));
+
+            Gfx::Rgb32ConstPixel constPixel(image, 0, 1);
+            Gfx::Rgb32ConstPixel equalConstPixel(image, 0, 1);
+            Gfx::Rgb32ConstPixel differentConstPixel(image, 1, 0);
+            PT_UNIT_ASSERT(pixel.equals(equalConstPixel));
+            PT_UNIT_ASSERT(!pixel.equals(differentConstPixel));
+            PT_UNIT_ASSERT(constPixel.equals(equalConstPixel));
+            PT_UNIT_ASSERT(!constPixel.equals(differentConstPixel));
+            PT_UNIT_ASSERT(constPixel.equals(pixel));
+            PT_UNIT_ASSERT(!constPixel.equals(differentPixel));
+        }
+
+        void MovePixel()
+        {
+            const std::vector<Pt::uint8_t> data {
+                1,  2,  3,  4,   5,  6,  7,  8,  0xEE, 0xEE, 0xEE, 0xEE,
+                9, 10, 11, 12,  13, 14, 15, 16,  0xEE, 0xEE, 0xEE, 0xEE,
+            };
+            const Gfx::Rgb32Color color00(4,  3,  2,  1);
+            const Gfx::Rgb32Color color10(8,  7,  6,  5);
+            const Gfx::Rgb32Color color01(12, 11, 10, 9);
+            const Gfx::Rgb32Color color11(16, 15, 14, 13);
+
+            Gfx::Rgb32Image image(2, 2, 4);
+            std::memcpy(image.data(), data.data(), data.size());
+
+            Gfx::Rgb32Pixel pixel(image, 0, 0);
+            PT_UNIT_ASSERT(pixel.base() == image.data());
+            PT_UNIT_ASSERT_EQUAL(pixel.color().value(), color00.value());
+
+            pixel.advance();
+            PT_UNIT_ASSERT(pixel.base() == image.data() + 4);
+            PT_UNIT_ASSERT_EQUAL(pixel.color().value(), color10.value());
+
+            pixel.reset(image, 0, 0);
+            pixel.advance(2);
+            PT_UNIT_ASSERT(pixel.base() == image.data() + 8);
+            pixel.skipPadding();
+            PT_UNIT_ASSERT(pixel.base() == image.data() + 12);
+            PT_UNIT_ASSERT_EQUAL(pixel.color().value(), color01.value());
+
+            pixel.reset(image, 0, 0);
+            pixel.advanceLines(1);
+            PT_UNIT_ASSERT(pixel.base() == image.data() + 12);
+            PT_UNIT_ASSERT_EQUAL(pixel.color().value(), color01.value());
+
+            Gfx::Rgb32Pixel other(image, 1, 1);
+            pixel.reset(other);
+            PT_UNIT_ASSERT(pixel.base() == image.data() + 16);
+            PT_UNIT_ASSERT_EQUAL(pixel.color().value(), color11.value());
+
+            Gfx::Rgb32ConstPixel constPixel(image, 0, 0);
+            PT_UNIT_ASSERT(constPixel.base() == image.data());
+            PT_UNIT_ASSERT_EQUAL(constPixel.color().value(), color00.value());
+
+            constPixel.advance();
+            PT_UNIT_ASSERT(constPixel.base() == image.data() + 4);
+            PT_UNIT_ASSERT_EQUAL(constPixel.color().value(), color10.value());
+
+            constPixel.reset(image, 0, 0);
+            constPixel.advance(2);
+            PT_UNIT_ASSERT(constPixel.base() == image.data() + 8);
+            constPixel.skipPadding();
+            PT_UNIT_ASSERT(constPixel.base() == image.data() + 12);
+            PT_UNIT_ASSERT_EQUAL(constPixel.color().value(), color01.value());
+
+            constPixel.reset(image, 0, 0);
+            constPixel.advanceLines(1);
+            PT_UNIT_ASSERT(constPixel.base() == image.data() + 12);
+            PT_UNIT_ASSERT_EQUAL(constPixel.color().value(), color01.value());
+
+            Gfx::Rgb32ConstPixel otherConst(image, 1, 0);
+            constPixel.reset(otherConst);
+            PT_UNIT_ASSERT(constPixel.base() == image.data() + 4);
+            PT_UNIT_ASSERT_EQUAL(constPixel.color().value(), color10.value());
+
+            pixel.reset(image, 1, 1);
+            constPixel.reset(pixel);
+            PT_UNIT_ASSERT(constPixel.base() == image.data() + 16);
+            PT_UNIT_ASSERT_EQUAL(constPixel.color().value(), color11.value());
         }
 
         void GenericGetColor()
@@ -600,14 +755,13 @@ class Rgb32Test : public Pt::Unit::TestSuite
 
         void SourceCopyRgb32Color()
         {
-            const Gfx::Rgb32Color sourceColor = rgb32Colors.back();
-            const std::vector<Pt::uint8_t> initial = toRgb32Data(rgb32Colors.front(), rgb32Colors.size());
+            const Gfx::Rgb32Color sourceColor(64, 45, 15, 8);
 
             Gfx::Rgb32Image image(2, 2);
-            std::memcpy(image.data(), initial.data(), initial.size());
+            std::memset(image.data(), 0, image.size());
 
-            Gfx::Rgb32Pixel pixel(image, 0, 0);
-            Gfx::sourceCopy(pixel, rgb32Colors.size(), sourceColor);
+            Gfx::Rgb32Pixel to(image, 0, 0);
+            Gfx::sourceCopy(to, image.width() * image.height(), sourceColor);
 
             const std::vector<Pt::uint8_t> expected = toRgb32Data(sourceColor, rgb32Colors.size());
             const bool isEqual = std::equal(expected.begin(), expected.end(), image.data());
@@ -617,17 +771,15 @@ class Rgb32Test : public Pt::Unit::TestSuite
 
         void SourceCopyPixels()
         {
-            const std::vector<Pt::uint8_t> initial = toRgb32Data(rgb32Colors.back(), rgb32Colors.size());
-
             Gfx::Rgb32Image sourceImage(2, 2);
             std::memcpy(sourceImage.data(), rgb32Data.data(), rgb32Data.size());
 
             Gfx::Rgb32Image image(2, 2);
-            std::memcpy(image.data(), initial.data(), initial.size());
+            std::memset(image.data(), 0, image.size());
 
-            Gfx::Rgb32ConstPixel source(sourceImage, 0, 0);
-            Gfx::Rgb32Pixel pixel(image, 0, 0);
-            Gfx::sourceCopy(pixel, source, rgb32Colors.size());
+            Gfx::Rgb32ConstPixel from(sourceImage, 0, 0);
+            Gfx::Rgb32Pixel to(image, 0, 0);
+            Gfx::sourceCopy(to, from, image.width() * image.height());
 
             const bool isEqual = std::equal(rgb32Data.begin(), rgb32Data.end(), image.data());
             PT_UNIT_ASSERT_MSG(isEqual,
@@ -636,15 +788,22 @@ class Rgb32Test : public Pt::Unit::TestSuite
 
         void SourceOverRgb32Color()
         {
-            const Gfx::Rgb32Color sourceColor = rgb32Colors.back();
+            const Gfx::Rgb32Color color(64, 45, 15, 8);
+            const std::vector<Pt::uint8_t> data {
+                50, 100, 200, 255,   0,   0,   0,   0,
+                25,  50, 100, 128,   8,  15,  45,  64,
+            };
+            const std::vector<Pt::uint8_t> expected {
+                45,  89, 194, 254,   8,  15,  45,  64,
+                26,  52, 119, 159,  13,  26,  78, 111,
+            };
 
             Gfx::Rgb32Image image(2, 2);
-            std::memcpy(image.data(), rgb32Data.data(), rgb32Data.size());
+            std::memcpy(image.data(), data.data(), data.size());
 
-            Gfx::Rgb32Pixel pixel(image, 0, 0);
-            Gfx::sourceOver(pixel, rgb32Colors.size(), sourceColor);
+            Gfx::Rgb32Pixel to(image, 0, 0);
+            Gfx::sourceOver(to, image.width() * image.height(), color);
 
-            const std::vector<Pt::uint8_t> expected = toSourceOverRgb32Data(rgb32Data, sourceColor);
             const bool isEqual = std::equal(expected.begin(), expected.end(), image.data());
             PT_UNIT_ASSERT_MSG(isEqual,
                                "unexpected: " << printBytes(image.data(), image.size()));
@@ -652,19 +811,29 @@ class Rgb32Test : public Pt::Unit::TestSuite
 
         void SourceOverPixels()
         {
-            const std::vector<Pt::uint8_t> initial = toRgb32Data(rgb32Colors.back(), rgb32Colors.size());
+            const std::vector<Pt::uint8_t> data1 {
+                50, 100, 200, 255,   0,   0,   0,   0,
+                25,  50, 100, 128,   8,  15,  45,  64,
+            };
+            const std::vector<Pt::uint8_t> data2 {
+                8,  15,  45,  64,   8,  15,  45,  64,
+                8,  15,  45,  64,   8,  15,  45,  64,
+            };
+            const std::vector<Pt::uint8_t> expected {
+                50, 100, 200, 255,   7,  14,  44,  63,
+                28,  57, 122, 159,  13,  26,  78, 111,
+            };
 
             Gfx::Rgb32Image sourceImage(2, 2);
-            std::memcpy(sourceImage.data(), rgb32Data.data(), rgb32Data.size());
+            std::memcpy(sourceImage.data(), data1.data(), data1.size());
 
             Gfx::Rgb32Image image(2, 2);
-            std::memcpy(image.data(), initial.data(), initial.size());
+            std::memcpy(image.data(), data2.data(), data2.size());
 
-            Gfx::Rgb32ConstPixel source(sourceImage, 0, 0);
-            Gfx::Rgb32Pixel pixel(image, 0, 0);
-            Gfx::sourceOver(pixel, source, rgb32Colors.size());
+            Gfx::Rgb32ConstPixel from(sourceImage, 0, 0);
+            Gfx::Rgb32Pixel to(image, 0, 0);
+            Gfx::sourceOver(to, from, image.width() * image.height());
 
-            const std::vector<Pt::uint8_t> expected = toSourceOverRgb32Data(initial, rgb32Data);
             const bool isEqual = std::equal(expected.begin(), expected.end(), image.data());
             PT_UNIT_ASSERT_MSG(isEqual,
                                "unexpected: " << printBytes(image.data(), image.size()));
@@ -714,29 +883,6 @@ class Rgb32Test : public Pt::Unit::TestSuite
             const Pt::uint8_t blue = Pt::uint8_t((Pt::uint32_t(color.blue() >> 8) * alpha) / 255);
 
             return toRgb32Data(Gfx::Rgb32Color(Pt::uint8_t(alpha), red, green, blue), count);
-        }
-
-        static std::vector<Pt::uint8_t> toSourceOverRgb32Data(const std::vector<Pt::uint8_t>& target,
-                                                              const Gfx::Rgb32Color& source)
-        {
-            return toSourceOverRgb32Data(target, toRgb32Data(source, target.size() / 4));
-        }
-
-        static std::vector<Pt::uint8_t> toSourceOverRgb32Data(const std::vector<Pt::uint8_t>& target,
-                                                              const std::vector<Pt::uint8_t>& source)
-        {
-            std::vector<Pt::uint8_t> result(target);
-
-            for(std::size_t i = 0; i < result.size(); i += 4)
-            {
-                const Pt::uint32_t alphaInv = 255 - source[i + 3];
-                result[i + 0] = Pt::uint8_t(source[i + 0] + ((alphaInv * result[i + 0]) >> 8));
-                result[i + 1] = Pt::uint8_t(source[i + 1] + ((alphaInv * result[i + 1]) >> 8));
-                result[i + 2] = Pt::uint8_t(source[i + 2] + ((alphaInv * result[i + 2]) >> 8));
-                result[i + 3] = Pt::uint8_t(source[i + 3] + ((alphaInv * result[i + 3]) >> 8));
-            }
-
-            return result;
         }
 
         template <typename ImageT>
